15dc2f864a2e3553bbdba9ff9c388eaee6110e8a
Sanjana Srivastava <sanjana2@stanford.edu>
2020-10-09 14:48:08 -0500

    setup

---
+++ bddl/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# TODO package data 
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+print('FIND PACKAGES OUTPUT:', find_packages())
+
+
+setup(
+    name='tasknet',
+    version='0.0.0',
+    author='Stanford University',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='none of your business',
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[],
+    package_data={},
+    include_package_data=True
+)
+     
+        
+
+
