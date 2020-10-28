a66bda9a6253ad2172814a588c49171e1ef6ea47
Sanjana Srivastava <sanjana2@stanford.edu>
2020-10-28 13:42:34 -0500

    minor

--- bddl/tasknet/config.py
+++ bddl/tasknet/config.py
@@ -1,6 +1,6 @@
 import os 
 
-TASK_CONFIGS_PATH = 'C:\\Users\\igibs\\TaskNet\\tasknet\\task_configs\\'
+TASK_CONFIGS_PATH = 'C:\\Users\\igibs\\TaskNet\\tasknet\\task_conditions\\'
 SCENE_PATH = 'd:\\ig_dataset\\scenes'
 OBJECT_MODEL_PATH = 'D:\\gibson2_assets\\processed'
 
