d064d2dc5d8a5396dfa46c60ab53e6ad3e907afe
Sanjana Srivastava <sanjana2@stanford.edu>
2021-11-08 23:37:00 -0800

    updated to ask for populated scope when making goals
    Former-commit-id: 36a9bdbf776c7845b04a1484ddd73f7f1b0a2d1d [formerly 36a9bdbf776c7845b04a1484ddd73f7f1b0a2d1d [formerly 36a9bdbf776c7845b04a1484ddd73f7f1b0a2d1d [formerly 249c0dbcc48f648e83c7d201081dc5f77ae9502e]]]
    Former-commit-id: 00f8e9507ceb3e783dafd7f38b858ab902e0f4da
    Former-commit-id: 82931b3bc8da5057503a3531fa54121767eedecb
    Former-commit-id: 976a06f3499e2891c6f6ebec0c61e80a8c8e0d82

--- bddl/bddl/activity.py
+++ bddl/bddl/activity.py
@@ -1,9 +1,4 @@
-from ast import parse
-import numpy as np
-import os 
-import random 
-import inspect
-
+import bddl 
 from bddl.parsing import parse_domain, parse_problem, gen_natural_language_conditions
 from bddl.condition_evaluation import create_scope, compile_state, evaluate_state, get_ground_state_options
 from bddl.object_taxonomy import ObjectTaxonomy
@@ -14,6 +9,14 @@ class Conditions(object):
                  behavior_activity, 
                  activity_definition, 
                  simulator_name):
+        """Object to store behavior activity content and compile conditions for checking and 
+            simulator use 
+
+        Args:
+            behavior_activity (str): behavior activity being used 
+            activity_definition (int): specific definition of behavior_activity
+            simulator_name (str): simulator that BEHAVIOR is being used with
+        """
         self.behavior_activity = behavior_activity 
         self.activity_definition = activity_definition 
         domain_name, *__ = parse_domain(simulator_name)
@@ -22,70 +25,80 @@ class Conditions(object):
             self.activity_definition, 
             domain_name
         )
-        self.object_scope = create_scope(self.parsed_objects)
-        self.compile_initial_conditions()
-        self.compile_goal_conditions()
-        self.compile_ground_goal_state_options()
+        bddl.set_backend("iGibson")
     
-    def compile_initial_conditions(self):
+    def get_object_scope(self):
+        """Create unpopulated object scope to populate for generating goal and
+            ground goal conditions. 
+
+        Returns:
+            dict<str: None>: unpopulated scope with string keys to be mapped to 
+                             simulator object values 
+        """
+        return create_scope(self.parsed_objects)
+
+    def get_initial_conditions(self):
+        """Create compiled initial conditions that can be checked and sampled
+
+        Returns:
+            list<bddl.condition_evaluation.HEAD>: compiled conditions if initial 
+                                                  condition definition is not 
+                                                  empty else None 
+        """
         if bool(self.parsed_initial_conditions[0]):
-            print(inspect.getfullargspec(compile_state))
-            self.initial_conditions = compile_state(
+            initial_conditions = compile_state(
                 [cond for cond in self.parsed_initial_conditions if cond[0] not in ["inroom"]],
                 scope=self.object_scope,
                 object_map=self.parsed_objects
             )
-    
-    def compile_goal_conditions(self):
+            return initial_conditions
+
+    def get_goal_conditions(self, populated_object_scope):
+        """Create compiled goal conditions with a populated object scope for checking
+
+        Args:
+            populated_object_scope (dict<str: simulator object>): scope mapping object 
+                                                                  terms in BDDL to 
+                                                                  simulator objects
+
+        Returns:
+            list<bddl.condition_evaluation.HEAD>: compiled conditions if goal condition
+                                                  definition is not empty else None 
+        """
         if bool(self.parsed_goal_conditions[0]):
-            self.goal_conditions = compile_state(
+            goal_conditions = compile_state(
                 self.parsed_goal_conditions,
-                scope=self.object_scope,
+                scope=populated_object_scope,
                 object_map=self.parsed_objects
             )
+            return goal_conditions 
     
-    def compile_ground_goal_state_options(self):
-        self.ground_goal_state_options = get_ground_state_options(
+    def get_ground_goal_state_options(self, populated_object_scope):
+        """Create compiled ground solutions to goal state with a populated object scope
+           for checking progress on specific solutions 
+
+        Args:
+            populated_object_scope (dict<str: simulator object>): scope mapping object 
+                                                                  terms in BDDL to 
+                                                                  simulator objects
+
+        Returns:
+            list<bddl.condition_evaluation.HEAD>: compiled goal solutions
+        
+        Raises:
+            AssertionError if there are no ground solutions
+        """
+        ground_goal_state_options = get_ground_state_options(
             self.goal_conditions,
-            scope=self.object_scope,
+            scope=populated_object_scope,
             object_map=self.parsed_objects
         )
+        assert len(ground_goal_state_options) > 0
+        return ground_goal_state_options
 
 
-
-# def get_conditions(behavior_activity, 
-#                    activity_definition,
-#                    simulator_name, 
-#                    predefined_problem=None):
-#     domain_name, *__ = parse_domain(simulator_name)
-#     __, parsed_objects, parsed_initial_conditions, parsed_goal_conditions = parse_problem(
-#         behavior_activity,
-#         activity_definition,
-#         domain_name,
-#     )
-
-#     object_scope = create_scope(parsed_objects)
-
-#     if bool(parsed_initial_conditions[0]):
-#         initial_conditions = compile_state(
-#             [cond for cond in parsed_initial_conditions if cond[0] not in ["inroom"]],
-#             # task??,
-#             scope=object_scope,
-#             object_map=parsed_objects
-#         )
+class Instructions(object):
+    def __init__(self, behavior_activity, activity_definition, simulator_name):
+        self.conditions = Conditions(behavior_activity, activity_definition, simulator_name)
     
-#     if bool(parsed_goal_conditions[0]):
-#         goal_conditions = compile_state(
-#             parsed_goal_conditions, 
-#             # task??
-#             scope=object_scope, 
-#             object_map=parsed_objects
-#         )
-    
-#     ground_goal_state_options = get_ground_state_options(
-#         goal_conditions,
-#         #task??
-#         scope=object_scope, 
-#         object_map=parsed_objects
-#     )
-#     assert len(ground_goal_state_options) > 0
+    # TODO implement class to serve VR instructions
--- bddl/bddl/activity_example.py
+++ bddl/bddl/activity_example.py
@@ -1,16 +1,25 @@
-from activity import Conditions, ObjectTaxonomy
+import bddl
+from bddl.activity import Conditions, ObjectTaxonomy
 
+
+bddl.set_backend("iGibson")
 behavior_activity = "packing_lunches"
 activity_definition = 0
 simulator_name = "igibson"
 
 conds = Conditions(behavior_activity, activity_definition, simulator_name)
+scope = conds.get_object_scope()
+populated_scope = None      # TODO populate scope in iGibson 
+init = conds.get_initial_conditions()
+goal = conds.get_goal_conditions(populated_scope)
+ground = conds.get_ground_goal_state_options(populated_scope)
+
 
 print("####### Initial #######")
-print(conds.initial_conditions)
+print(init)
 print()
 print("####### Goal #######")
-print(conds.goal_conditions)
+print(goal)
 print()
 print("####### Ground #######")
-print(conds.ground_goal_state_options)
+print(ground)
