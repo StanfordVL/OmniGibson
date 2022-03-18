82932636cd3158f8d691a7fb30908c60a210c4d4
cremebrule <84cremebrule@gmail.com>
2022-03-18 15:50:28 -0700

    refactor rewards

--- igibson/reward_functions/potential_reward.py
+++ igibson/reward_functions/potential_reward.py
@@ -6,11 +6,28 @@ class PotentialReward(BaseRewardFunction):
     Potential reward
     Assume task has get_potential implemented; Low potential is preferred
     (e.g. a common potential for goal-directed task is the distance to goal)
+
+    Args:
+        potential_fcn (method): function for calculating potential. Function signature should be:
+
+                potential = potential_fcn(env)
+
+                where @env is a BaseEnv instance, and @potential is a float value representing the calculated potential
+
+        r_potential (float): Reward weighting to give proportional to the potential difference calculated
+            in between env timesteps
     """
 
-    def __init__(self, config):
-        super(PotentialReward, self).__init__(config)
-        self.potential_reward_weight = self.config.get("potential_reward_weight", 1.0)
+    def __init__(self, potential_fcn, r_potential=1.0):
+        # Store internal vars
+        self._potential_fcn = potential_fcn
+        self._r_potential = r_potential
+
+        # Store internal vars that will be filled in at runtime
+        self._potential = None
+
+        # Run super
+        super().__init__()
 
     def reset(self, task, env):
         """
@@ -19,19 +36,15 @@ class PotentialReward(BaseRewardFunction):
         :param task: task instance
         :param env: environment instance
         """
-        self.potential = task.get_potential(env)
+        # Reset potential
+        self._potential = self._potential_fcn(env)
 
-    def get_reward(self, task, env):
-        """
-        Reward is proportional to the potential difference between
-        the current and previous timestep
+    def _step(self, task, env, action):
+        # Reward is proportional to the potential difference between the current and previous timestep
+        new_potential = self._potential_fcn(env)
+        reward = (self._potential - new_potential) * self._r_potential
 
-        :param task: task instance
-        :param env: environment instance
-        :return: reward
-        """
-        new_potential = task.get_potential(env)
-        reward = self.potential - new_potential
-        reward *= self.potential_reward_weight
-        self.potential = new_potential
-        return reward
+        # Update internal potential
+        self._potential = new_potential
+
+        return reward, {}
