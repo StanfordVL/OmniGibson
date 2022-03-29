08bc3d77dcdc2244b8189111c3d7f80b53e9469c
cremebrule <84cremebrule@gmail.com>
2022-03-29 14:50:33 -0700

    update benchmark interactive scene script

--- tests/benchmark/benchmark_interactive_scene.py
+++ tests/benchmark/benchmark_interactive_scene.py
@@ -7,36 +7,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import igibson
-from igibson.render.mesh_renderer.mesh_renderer_settings import MeshRendererSettings
+from igibson import app, ig_dataset_path
 from igibson.robots.turtlebot import Turtlebot
-from igibson.scenes.interactive_traversable_scene import InteractiveIndoorScene
-from igibson.simulator import Simulator
+from igibson.scenes.interactive_traversable_scene import InteractiveTraversableScene
+from igibson.simulator_omni import Simulator
 from igibson.utils.assets_utils import get_ig_assets_version
 from igibson.utils.utils import parse_config
 
 
-def benchmark_scene(scene_name, optimized=False, import_robot=True):
-    config = parse_config(os.path.join(os.path.dirname(__file__), "..", "test.yaml"))
+def benchmark_scene(sim, scene_name, optimized=False, import_robot=True):
     assets_version = get_ig_assets_version()
     print("assets_version", assets_version)
-    scene = InteractiveIndoorScene(scene_name, texture_randomization=False, object_randomization=False)
-    settings = MeshRendererSettings(msaa=False, enable_shadow=False, optimized=optimized)
-    s = Simulator(
-        mode="headless",
-        image_width=512,
-        image_height=512,
-        device_idx=0,
-        rendering_settings=settings,
-    )
+    scene_path = f"{ig_dataset_path}/scenes/{scene_name}/urdf/{scene_name}_best_template.usd"
+    scene = InteractiveTraversableScene(scene_name, usd_path=scene_path, texture_randomization=False, object_randomization=False)
     start = time.time()
-    s.import_scene(scene)
+    sim.import_scene(scene)
+    sim.play()
     print(time.time() - start)
 
     if import_robot:
-        turtlebot = Turtlebot()
-        s.import_object(turtlebot)
+        turtlebot = Turtlebot(prim_path="/World/robot", name="agent")
+        sim.import_object(turtlebot, auto_initialize=True)
+        sim.step()
 
-    s.renderer.use_pbr(use_pbr=True, use_pbr_mapping=True)
     fps = []
     physics_fps = []
     render_fps = []
@@ -45,26 +38,27 @@ def benchmark_scene(scene_name, optimized=False, import_robot=True):
         # if i % 100 == 0:
         #     scene.randomize_texture()
         start = time.time()
-        s.step()
+        sim.step()
         if import_robot:
             # apply random actions
             turtlebot.apply_action(turtlebot.action_space.sample())
         physics_end = time.time()
-        if import_robot:
-            _ = s.renderer.render_robot_cameras(modes=("rgb"))
-        else:
-            _ = s.renderer.render(modes=("rgb"))
+        
+        # TODO: We need a way to measure rendering time. Currently omni already renders when we call s.step()
+        # if import_robot:
+        #     _ = s.renderer.render_robot_cameras(modes=("rgb"))
+        # else:
+        #     _ = s.renderer.render(modes=("rgb"))
         end = time.time()
 
         # print("Elapsed time: ", end - start)
-        print("Render Frequency: ", 1 / (end - physics_end))
+        # print("Render Frequency: ", 1 / (end - physics_end))
         print("Physics Frequency: ", 1 / (physics_end - start))
         print("Step Frequency: ", 1 / (end - start))
         fps.append(1 / (end - start))
         physics_fps.append(1 / (physics_end - start))
-        render_fps.append(1 / (end - physics_end))
-        obj_awake.append(s.body_links_awake)
-    s.disconnect()
+        # render_fps.append(1 / (end - physics_end))
+        # obj_awake.append(sim.body_links_awake)
     plt.figure(figsize=(7, 25))
 
     ax = plt.subplot(6, 1, 1)
@@ -89,16 +83,22 @@ def benchmark_scene(scene_name, optimized=False, import_robot=True):
     plt.plot(physics_fps)
     ax.set_xlabel("Physics fps with time, converge to {}".format(np.mean(physics_fps[-100:])))
     ax.set_ylabel("fps")
-    ax = plt.subplot(6, 1, 6)
-    plt.plot(obj_awake)
-    ax.set_xlabel("Num object links awake, converge to {}".format(np.mean(obj_awake[-100:])))
+
+    # TODO! Objs awake not implemented yet in simulator_omni.py
+    # ax = plt.subplot(6, 1, 6)
+    # plt.plot(obj_awake)
+    # ax.set_xlabel("Num object links awake, converge to {}".format(np.mean(obj_awake[-100:])))
 
     plt.savefig("scene_benchmark_{}_o_{}_r_{}.pdf".format(scene_name, optimized, import_robot))
 
 
 def main():
-    benchmark_scene("Rs_int", optimized=True, import_robot=True)
-    benchmark_scene("Rs_int", optimized=True, import_robot=False)
+    sim = Simulator(
+        viewer_width=512,
+        viewer_height=512,
+    )
+    benchmark_scene(sim, "Rs_int", optimized=True, import_robot=True)
+    benchmark_scene(sim, "Rs_int", optimized=True, import_robot=False)
 
     # scenes = ["Beechwood_0_int",
     #           "Beechwood_1_int",
