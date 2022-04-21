6ebc40a574733d1b63f8b6c4051d6ca4d6e4c5b8
mj-hwang <mjhwang@berkeley.edu>
2022-04-21 11:39:40 -0700

    add ik controller

--- igibson/controllers/ik_controller.py
+++ igibson/controllers/ik_controller.py
@@ -1,19 +1,18 @@
 import numpy as np
 
-
 import igibson.utils.transform_utils as T
 from igibson.controllers import ControlType, ManipulationController
 from igibson.utils.filters import MovingAverageFilter
+
+from omni.isaac.core.utils.rotations import quat_to_rot_matrix
+import lula
 
 # Different modes
 IK_MODE_COMMAND_DIMS = {
     "pose_absolute_ori": 6,  # 6DOF (dx,dy,dz,ax,ay,az) control over pose, where the orientation is given in absolute axis-angle coordinates
     "pose_delta_ori": 6,  # 6DOF (dx,dy,dz,dax,day,daz) control over pose
-    "position_fixed_ori": 3,  # 3DOF (dx,dy,dz) control over position, with orientation commands being kept as fixed initial absolute orientation
-    "position_compliant_ori": 3,  # 3DOF (dx,dy,dz) control over position, with orientation commands automatically being sent as 0s (so can drift over time)
 }
 IK_MODES = set(IK_MODE_COMMAND_DIMS.keys())
-
 
 class InverseKinematicsController(ManipulationController):
     """
@@ -27,32 +26,29 @@ class InverseKinematicsController(ManipulationController):
 
     def __init__(
         self,
-        base_body_id,
-        task_link_id,
-        task_name,
+        robot_description_yaml_path,
+        robot_urdf_path,
+        eef_name,
         control_freq,
-        default_joint_pos,
-        joint_damping,
         control_limits,
-        joint_idx,
-        command_input_limits="default",
+        dof_idx,
+        task_name="eef_0",  # TODO: maybe unify this w/ eef name?
+        # command_input_limits="default",
+        command_input_limits=None,
         command_output_limits=((-0.2, -0.2, -0.2, -0.5, -0.5, -0.5), (0.2, 0.2, 0.2, 0.5, 0.5, 0.5)),
         kv=2.0,
-        mode="pose_delta_ori",
+        mode="pose_absolute_ori",
         smoothing_filter_size=None,
         workspace_pose_limiter=None,
-        joint_range_tolerance=0.01,
     ):
         """
-        :param base_body_id: int, unique pybullet ID corresponding to the pybullet body being controlled by IK
-        :param task_link_id: int, pybullet link ID corresponding to the link within the body being controlled by IK
         :param task_name: str, name assigned to this task frame for computing IK control. During control calculations,
             the inputted control_dict should include entries named <@task_name>_pos_relative and
             <@task_name>_quat_relative. See self._command_to_control() for what these values should entail.
+        :param robot_description_yaml_path: str, path to robot description yaml file
+        :param robot_urdf_path: str, path to robot urdf file
+        :param eef_name: str, end effector frame name
         :param control_freq: int, controller loop frequency
-        :param default_joint_pos: Array[float], default joint positions, used as part of nullspace controller in IK
-        :param joint_damping: Array[float], joint damping parameters associated with each joint
-            in the body being controlled
         :param control_limits: Dict[str, Tuple[Array[float], Array[float]]]: The min/max limits to the outputted
             control signal. Should specify per-actuator type limits, i.e.:
 
@@ -62,8 +58,6 @@ class InverseKinematicsController(ManipulationController):
             "has_limit": [...bool...]
 
             Values outside of this range will be clipped, if the corresponding joint index in has_limit is True.
-        :param joint_idx: Array[int], specific joint indices controlled by this robot. Used for inferring
-            controller-relevant values during control computations
         :param command_input_limits: None or "default" or Tuple[float, float] or Tuple[Array[float], Array[float]],
             if set, is the min/max acceptable inputted command. Values outside of this range will be clipped.
             If None, no clipping will be used. If "default", range will be set to (-1, 1)
@@ -73,15 +67,6 @@ class InverseKinematicsController(ManipulationController):
             If either is None, no scaling will be used. If "default", then this range will automatically be set
             to the @control_limits entry corresponding to self.control_type
         :param kv: float, Gain applied to error between IK-commanded joint positions and current joint positions
-        :param mode: str, mode to use when computing IK. In all cases, position commands are 3DOF delta (dx,dy,dz)
-            cartesian values, relative to the robot base frame. Valid options are:
-                - "pose_absolute_ori": 6DOF (dx,dy,dz,ax,ay,az) control over pose,
-                    where the orientation is given in absolute axis-angle coordinates
-                - "pose_delta_ori": 6DOF (dx,dy,dz,dax,day,daz) control over pose
-                - "position_fixed_ori": 3DOF (dx,dy,dz) control over position,
-                    with orientation commands being kept as fixed initial absolute orientation
-                - "position_compliant_ori": 3DOF (dx,dy,dz) control over position,
-                    with orientation commands automatically being sent as 0s (so can drift over time)
         :param smoothing_filter_size: None or int, if specified, sets the size of a moving average filter to apply
             on all outputted IK joint positions.
         :param workspace_pose_limiter: None or function, if specified, callback method that should clip absolute
@@ -93,38 +78,42 @@ class InverseKinematicsController(ManipulationController):
 
             where pos_command is (x,y,z) cartesian position values, command_quat is (x,y,z,w) quarternion orientation
             values, and the returned tuple is the processed (pos, quat) command.
-        :param joint_range_tolerance: float, amount to add to each side of the inputted joint range, to improve IK
-            convergence stability (e.g.: for joint_ranges = 0 for no limits, prevents NaNs from occurring)
         """
-        # Store arguments
-        control_dim = len(joint_damping)
+        self.kv = kv
+        self.workspace_pose_limiter = workspace_pose_limiter
+        self.task_name = task_name
+        self.mode = mode
+
+        # Lula specifics
+        self.robot_description_yaml_path = robot_description_yaml_path
+        self.robot_urdf_path = robot_urdf_path
+        self.eef_name = eef_name
+
+        robot_description = lula.load_robot(self.robot_description_yaml_path, self.robot_urdf_path)
+        self.lula_kinematics = robot_description.kinematics()
+        self.lula_config = lula.CyclicCoordDescentIkConfig()
+
+        # Other variables that will be filled in at runtime
+        self._quat_target = None
+
+        control_dim = len(dof_idx)
         self.control_filter = (
             None
             if smoothing_filter_size in {None, 0}
             else MovingAverageFilter(obs_dim=control_dim, filter_width=smoothing_filter_size)
         )
-        assert mode in IK_MODES, "Invalid ik mode specified! Valid options are: {IK_MODES}, got: {mode}"
-        self.mode = mode
-        self.kv = kv
-        self.workspace_pose_limiter = workspace_pose_limiter
-        self.base_body_id = base_body_id
-        self.task_link_id = task_link_id
-        self.task_name = task_name
-        self.default_joint_pos = np.array(default_joint_pos)
-        self.joint_damping = np.array(joint_damping)
-        self.joint_range_tolerance = joint_range_tolerance
-
-        # Other variables that will be filled in at runtime
-        self._quat_target = None
 
         # Run super init
         super().__init__(
             control_freq=control_freq,
             control_limits=control_limits,
-            joint_idx=joint_idx,
+            dof_idx=dof_idx,
             command_input_limits=command_input_limits,
             command_output_limits=command_output_limits,
         )
+
+        print()
+        print("IK Control Limits", command_input_limits)
 
     def reset(self):
         # Reset the filter and clear internal control state
@@ -171,12 +160,9 @@ class InverseKinematicsController(ManipulationController):
 
     def _command_to_control(self, command, control_dict):
         """
-        Converts the (already preprocessed) inputted @command into deployable (non-clipped!) joint control signal.
-        This processes the command based on self.mode, possibly clips the command based on self.workspace_pose_limiter,
-
+        Converts the (already preprocessed) inputted @command into deployable joint control signal.
         :param command: Array[float], desired (already preprocessed) command to convert into control signals
             Is one of:
-                (dx,dy,dz) - desired delta cartesian position
                 (dx,dy,dz,dax,day,daz) - desired delta cartesian position and delta axis-angle orientation
                 (dx,dy,dz,ax,ay,az) - desired delta cartesian position and global axis-angle orientation
         :param control_dict: Dict[str, Any], dictionary that should include any relevant keyword-mapped
@@ -199,16 +185,7 @@ class InverseKinematicsController(ManipulationController):
         dpos = command[:3]
         target_pos = pos_relative + dpos
 
-        # Compute orientation
-        if self.mode == "position_fixed_ori":
-            # We need to grab the current robot orientation as the commanded orientation if there is none saved
-            if self._quat_target is None:
-                self._quat_target = quat_relative
-            target_quat = self._quat_target
-        elif self.mode == "position_compliant_ori":
-            # Target quat is simply the current robot orientation
-            target_quat = quat_relative
-        elif self.mode == "pose_absolute_ori":
+        if self.mode == "pose_absolute_ori":
             # Received "delta" ori is in fact the desired absolute orientation
             target_quat = T.axisangle2quat(command[3:])
         else:  # pose_delta_ori control
@@ -220,27 +197,23 @@ class InverseKinematicsController(ManipulationController):
         if self.workspace_pose_limiter is not None:
             target_pos, target_quat = self.workspace_pose_limiter(target_pos, target_quat, control_dict)
 
-        # Convert to world frame
-        target_pos, target_quat = self._pose_in_base_to_pose_in_world(
-            pose_in_base=(target_pos, target_quat),
-            base_in_world=(np.array(control_dict["base_pos"]), np.array(control_dict["base_quat"])),
-        )
+        current_joint_pos = control_dict["joint_position"][self.dof_idx]
 
-        # Calculate and return IK-backed out joint angles
-        joint_targets = self._calc_joint_angles_from_ik(target_pos=target_pos, target_quat=target_quat)
+        # Calculate and return IK-backed out joint positions (angles)
+        target_joint_pos = self._calc_joint_pos_from_ik(target_pos, target_quat, current_joint_pos=current_joint_pos)
 
         # Grab the resulting error and scale it by the velocity gain
-        u = -self.kv * (control_dict["joint_position"] - joint_targets)
+        u = -self.kv * (current_joint_pos - target_joint_pos)
 
         # Return these commanded velocities, (only the relevant joint idx)
-        return u[self.joint_idx]
+        return u
 
-    def _calc_joint_angles_from_ik(self, target_pos, target_quat):
+    def _calc_joint_pos_from_ik(self, target_pos, target_quat, current_joint_pos=None):
         """
-        Solves for joint angles given the ik target position and orientation
+        Solves for joint positions (angles) given the ik target position and orientation
 
-        Note that this outputs joint angles for the entire pybullet robot body! It is the responsibility of the
-        associated Robot class to filter out the redundant / non-impact joints from the computation
+        Note that this outputs joint positions for the entire pybullet robot body! It is the responsibility of
+        the associated Robot class to filter out the redundant / non-impact joints from the computation
 
         Args:
             target_pos (3-array): absolute (x, y, z) eef position command (in robot base frame)
@@ -250,29 +223,21 @@ class InverseKinematicsController(ManipulationController):
             n-array: corresponding joint positions to match the inputted targets
         """
         # Run IK
-        cmd_joint_pos = np.array(
-            p.calculateInverseKinematics(
-                bodyIndex=self.base_body_id,
-                endEffectorLinkIndex=self.task_link_id,
-                targetPosition=target_pos.tolist(),
-                targetOrientation=target_quat.tolist(),
-                lowerLimits=(self._control_limits[ControlType.POSITION][0] - self.joint_range_tolerance).tolist(),
-                upperLimits=(self._control_limits[ControlType.POSITION][1] + self.joint_range_tolerance).tolist(),
-                jointRanges=(
-                    self._control_limits[ControlType.POSITION][1]
-                    - self._control_limits[ControlType.POSITION][0]
-                    + 2 * self.joint_range_tolerance
-                ).tolist(),
-                restPoses=self.default_joint_pos.tolist(),
-                jointDamping=self.joint_damping.tolist(),
-            )
-        )
+        self.lula_config.cspace_seeds = [current_joint_pos] if current_joint_pos is not None else []
+
+        trans = np.array(target_pos, dtype=np.float64).reshape(3, 1)
+        rot = np.array(quat_to_rot_matrix(target_quat), dtype=np.float64).reshape(3, 3)
+
+        ik_target_pose = lula.Pose3(lula.Rotation3(rot), trans)
+        ik_results = lula.compute_ik_ccd(self.lula_kinematics, ik_target_pose, self.eef_name, self.lula_config)
+
+        target_joint_pos = np.array(ik_results.cspace_position)
 
         # Optionally pass through smoothing filter for better stability
         if self.control_filter is not None:
-            cmd_joint_pos = self.control_filter.estimate(cmd_joint_pos)
+            target_joint_pos = self.control_filter.estimate(target_joint_pos)
 
-        return cmd_joint_pos
+        return target_joint_pos
 
     @property
     def control_type(self):
@@ -280,4 +245,4 @@ class InverseKinematicsController(ManipulationController):
 
     @property
     def command_dim(self):
-        return IK_MODE_COMMAND_DIMS[self.mode]
+        return 6
