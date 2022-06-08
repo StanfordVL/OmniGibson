71c060be4b0e155969b911a70480fae2786d6a1b
cremebrule <84cremebrule@gmail.com>
2022-06-07 17:22:04 -0700

    re-add COM logic in rigid_prim.py

--- igibson/prims/rigid_prim.py
+++ igibson/prims/rigid_prim.py
@@ -111,6 +111,7 @@ class RigidPrim(XFormPrim):
         # and grab any that are presumed to be meshes
         self._collision_meshes, self._visual_meshes = OrderedDict(), OrderedDict()
         prims_to_check = []
+        coms, vols = [], []
         for prim in self._prim.GetChildren():
             prims_to_check.append(prim)
             for child in prim.GetChildren():
@@ -118,12 +119,30 @@ class RigidPrim(XFormPrim):
         for prim in prims_to_check:
             if prim.GetPrimTypeInfo().GetTypeName() in GEOM_TYPES:
                 mesh_name, mesh_path = prim.GetName(), prim.GetPrimPath().__str__()
+                mesh_prim = get_prim_at_path(prim_path=mesh_path)
                 mesh = get_prim_at_path(prim_path=mesh_path)
                 mesh_kwargs = {"prim_path": mesh_path, "name": f"{self._name}:{mesh_name}"}
                 if mesh.HasAPI(UsdPhysics.CollisionAPI):
                     self._collision_meshes[mesh_name] = CollisionGeomPrim(**mesh_kwargs)
+                    # We construct a trimesh object from this mesh in order to infer its center-of-mass and volume
+                    # TODO: Cleaner way to aggregate this information? Right now we just skip if we encounter a primitive
+                    mesh_vertices = mesh_prim.GetAttribute("points").Get()
+                    if mesh_vertices is not None and len(mesh_vertices) > 4:
+                        msh = trimesh.Trimesh(
+                            vertices=np.array(mesh_vertices),
+                            faces=np.array(mesh_prim.GetAttribute("faceVertexIndices").Get()).reshape(-1, 3),
+                            vertex_normals=np.array(mesh_prim.GetAttribute("normals").Get()),
+                        )
+                        coms.append(msh.center_mass)
+                        vols.append(msh.volume)
                 else:
                     self._visual_meshes[mesh_name] = VisualGeomPrim(**mesh_kwargs)
+
+        # If we have any collision meshes, we aggregate their center of mass and volume values to set the center of mass
+        # for this link
+        if len(coms) > 0:
+            com = (np.array(coms) * np.array(vols).reshape(-1, 1)).sum(axis=0) / np.sum(vols)
+            self.set_attribute("physics:centerOfMass", Gf.Vec3f(*com))
 
         # Set the visual-only attribute
         # This automatically handles setting collisions / gravity appropriately
