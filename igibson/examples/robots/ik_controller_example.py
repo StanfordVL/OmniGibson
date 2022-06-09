0e000df9cf151021c1ee00c04ad4ce46840bdead
mj-hwang <mjhwang@berkeley.edu>
2022-06-09 14:51:45 -0700

    Add detailed comments in example files

--- igibson/examples/robots/ik_controller_example.py
+++ igibson/examples/robots/ik_controller_example.py
@@ -13,7 +13,7 @@ USD_TEMPLATE_FILE = f"{ig_dataset_path}/scenes/Rs_int/urdf/Rs_int_best_template.
 
 # Test iGibson IK Controller
 
-## Part 1: Load Scene, Robot, and Marker
+## PART 1: Load Scene, Robot, and Marker
 
 # Create sim
 sim = Simulator()
@@ -37,41 +37,40 @@ sim.import_object(obj=marker)
 
 sim.play()
 
-# set the robot position; if None, put it in the default location
+# Set the robot position.
 robot_pos = np.array([0, 0, 0])
-# robot_pos = np.array([-1.0, 0.4, 0])
-robot_quat = np.array([0.0, 0.0, 1.0, 1.0])
-# robot.set_position_orientation(robot_pos, robot_quat)
 robot.set_position(robot_pos)
 
-# set the target end effector position w.r.t the robot position
+# Alternatively, set the robot position and quaternion as below.
+# robot_pos = np.array([-1.0, 0.4, 0])
+# robot_quat = np.array([0.0, 0.0, 1.0, 1.0])
+# robot.set_position_orientation(robot_pos, robot_quat)
+
+# Set the target end effector position w.r.t the robot position
 target_pos_delta = np.array([0.6, -0.4, 0.5])
 target_pos = robot_pos + target_pos_delta
 target_quat = np.array([1.0, 0.0, 0.0, 0.0])
 marker.set_position(target_pos)
 
-## Part 2: Initialize IK Controller
+# Initialize the zero control
+control = np.zeros(robot.action_dim)
 
-
-control = np.zeros(11)
-
-## Part 3: Compute IK controls and simulate
+## PART 2: Compute IK controls and simulate
 for i in range(100000):
-    # if i == 3200:
-    #     target_pos_delta = np.array([0.8, -0.1, 0.5])
-    #     target_pos = robot_pos + target_pos_delta
-    #     marker.set_position(target_pos)
-
+    
+    # Get the relative position of the end effector 
     control_dict = robot.get_control_dict()
     pos_relative = np.array(control_dict["eef_0_pos_relative"])
-
+    
+    # Convert the target pose in robot's base frame
     target_pose_in_world_mat = T.pose2mat((target_pos, target_quat))
     base_pose_in_world_mat = T.pose2mat((np.array(control_dict["root_pos"]), np.array(control_dict["root_quat"])))
     world_pose_in_base_mat = T.pose_inv(base_pose_in_world_mat)
     target_pose_in_base_mat = T.pose_in_A_to_pose_in_B(pose_A=target_pose_in_world_mat, pose_A_in_B=world_pose_in_base_mat)
-
     target_pos_relative, target_quat_relative = T.mat2pose(target_pose_in_base_mat)
-
+    
+    # Indices 4~6 correspond to the commands for the position of the end effector
+    # Since we use the delta control for position, we input desired delta as control
     control[4:7] = target_pos_relative - pos_relative
 
     robot.apply_action(control)
--- igibson/examples/robots/ik_example.py
+++ igibson/examples/robots/ik_example.py
@@ -13,9 +13,16 @@ SCENE_ID = "Rs_int"
 USD_TEMPLATE_FILE = f"{ig_dataset_path}/scenes/Rs_int/urdf/Rs_int_best_template.usd"
 #####################
 
-# Test omni.isaac.lula inverse kinematics function (works when we have JointController for arm(s))
+# Test omni.isaac.lula inverse kinematics function 
+# works when we have JointController for arm(s))
 
-## Part 1: Load Scene, Robot, and Marker
+"""
+This script contains a sample script for testing omni.isaac.lula inverse kinematics solver.
+Thus, in order to directly test functionalities, it does NOT use igibson's InverseKinematicsContoller. 
+Instead, this script bypasses `apply_action` and directly sets the joint position on every step.
+"""
+
+## PART 1: Load Scene, Robot, and Marker
 
 # Create sim
 sim = Simulator()
@@ -39,49 +46,66 @@ sim.import_object(obj=marker)
 
 sim.play()
 
-# set the robot position; if None, put it in the default location
+# Set the robot position; if None, put it in the default location
 robot_pos = None
 # robot_pos = np.array([-1.0, 0.4, 0])
-
-# set the target end effector position w.r.t the robot position
-target_pos_delta = np.array([0.6, -0.4, 0.5])
-target_quat = np.array([1.0, 0.0, 0.0, 0.0])
-
 if robot_pos is not None:
     robot.set_position(robot_pos)
     marker.set_position(robot_pos + target_pos_delta)
 else:
     marker.set_position(target_pos_delta)
 
-## Part 2: Initialize omni.lula ik params
+# Set the target end effector position & quaternion w.r.t the robot position
+target_pos_delta = np.array([0.6, -0.4, 0.5])
+target_quat = np.array([1.0, 0.0, 0.0, 0.0])
 
+## PART 2: Initialize omni.lula ik params
+
+# Set the required variables for initializing and running IK solver
+
+# Set the descriptor and urdf file path
 robot_descriptor_yaml_path = f"{FETCH_ASSETS_DIR}/fetch_descriptor.yaml"
 robot_urdf_path = f"{FETCH_ASSETS_DIR}/fetch.urdf"
+
+# Set the end effector name
 eef_name = "gripper_link"
 
+# Initialize variables for the robot kinemtaics and IK config in lula
 robot_description = lula.load_robot(robot_descriptor_yaml_path, robot_urdf_path)
 lula_kinematics = robot_description.kinematics()
 lula_config = lula.CyclicCoordDescentIkConfig()
 
-kv = 0.1  # control gain parameter
-ik_control_idx = np.array([2, 5, 6, 7, 8, 9, 10, 11])  # inverse control indices for Fetch Robot
+# Control gain parameter
+kv = 0.1
+
+# Target control indices for Fetch Robot (trunk & arm)
+ik_control_idx = np.array([2, 5, 6, 7, 8, 9, 10, 11])
 
 ## Part 3: Compute IK controls and simulate
 for i in range(100000):
-    # Set up IK Solver
+
+    # Get the current joint position of target joints
     all_joint_position = np.array(robot.get_joint_positions())
     joint_position = all_joint_position[ik_control_idx]
+
+    # Set the current position as the initial position (cspace seeds) for the IK solver
     lula_config.cspace_seeds = [joint_position]
+    
+    # Initialize the target end effector pose (position & quaternion) in lula
     trans = np.array(target_pos_delta, dtype=np.float64).reshape(3, 1)
     rot = np.array(quat_to_rot_matrix(target_quat), dtype=np.float64).reshape(3, 3)
     ik_target_pose = lula.Pose3(lula.Rotation3(rot), trans)
 
-    # Compute the target joint position and control input
+    # Compute the target joint position
     ik_results = lula.compute_ik_ccd(lula_kinematics, ik_target_pose, eef_name, lula_config)
     target_joint_position = np.array(ik_results.cspace_position)
+    
+    # Compute the control by scaling the joint position's difference by the control gain
     u = -kv * (joint_position - target_joint_position)
 
-    # Set the joint position
+    # Apply the control by adding the control and setting the joint position 
     all_joint_position[ik_control_idx] += u
     robot.set_joint_positions(all_joint_position)
+    
+    # Take a simulation step
     sim.step()
