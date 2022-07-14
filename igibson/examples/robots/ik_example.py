d0bfcc95f3cdbc8ab80678fa3c325b047457d448
cremebrule <84cremebrule@gmail.com>
2022-07-13 18:15:26 -0700

    refactor ik example to use new ik interface

--- igibson/examples/robots/ik_example.py
+++ igibson/examples/robots/ik_example.py
@@ -1,24 +1,19 @@
 import argparse
 import logging
-import os
 import time
 
 import numpy as np
 
+import carb
+import omni
 
-import igibson
-# from igibson.external.pybullet_tools.utils import (
-    get_max_limits,
-    get_min_limits,
-    get_sample_fn,
-    joints_from_names,
-    set_joint_positions,
-)
-from igibson.objects.visual_marker import VisualMarker
+from igibson import Simulator, app
+from igibson.objects.primitive_object import PrimitiveObject
 from igibson.robots.fetch import Fetch
 from igibson.scenes.empty_scene import EmptyScene
-from igibson.simulator import Simulator
-from igibson.utils.utils import l2_distance, parse_config, restoreState
+from igibson.sensors.vision_sensor import VisionSensor
+from igibson.utils.control_utils import IKSolver
+from pxr import Gf
 
 
 def main(random_selection=False, headless=False, short_exec=False):
@@ -45,222 +40,164 @@ def main(random_selection=False, headless=False, short_exec=False):
     else:
         programmatic_pos = True
 
-    # Create simulator, scene and robot (Fetch)
-    config = parse_config(os.path.join(igibson.example_config_path, "fetch_reaching.yaml"))
-    s = Simulator(mode="headless", use_pb_gui=True if not headless else False)
-    scene = EmptyScene()
-    s.import_scene(scene)
+    # Create simulator, scene, and robot (Fetch)
+    sim = Simulator()
+    scene = EmptyScene(floor_plane_visible=True)
+    sim.import_scene(scene)
 
-    robot_config = config["robot"]
-    robot_config.pop("name")
-
-    fetch = Fetch(**robot_config)
-    s.import_object(fetch)
-
-    body_ids = fetch.get_body_ids()
-    assert len(body_ids) == 1, "Fetch robot is expected to be single-body."
-    robot_id = body_ids[0]
-
-    arm_default_joint_positions = (
-        0.10322468280792236,
-        -1.414019864768982,
-        1.5178184935241699,
-        0.8189625336474915,
-        2.200358942909668,
-        2.9631312579803466,
-        -1.2862852996643066,
-        0.0008453550418615341,
+    # Create a reference to the default viewer camera that already exists in the simulator
+    cam = VisionSensor(
+        prim_path="/World/viewer_camera",
+        name="camera",
+        modalities="rgb",
+        image_height=720,
+        image_width=1280,
+        viewport_name="Viewport",
+    )
+    # We update its clipping range so that it doesn't clip nearby objects (default min is 1 m)
+    cam.set_attribute("clippingRange", Gf.Vec2f(0.001, 10000000.0))
+    # In order for camera changes to propagate, we must toggle its visibility
+    cam.visible = False
+    # A single step has to happen here before we toggle visibility for changes to propagate
+    sim.step()
+    cam.visible = True
+    # Initialize the camera sensor and update its pose so it points towards the robot
+    cam.initialize()
+    sim.step()
+    cam.set_position_orientation(
+        position=np.array([4.32248, -5.74338, 6.85436]),
+        orientation=np.array([0.39592, 0.13485, 0.29286, 0.85982]),
     )
 
-    robot_default_joint_positions = (
-        [0.0, 0.0]
-        + [arm_default_joint_positions[0]]
-        + [0.0, 0.0]
-        + list(arm_default_joint_positions[1:])
-        + [0.01, 0.01]
+    # Create Fetch robot -- this by default utilizes IK Control for its arm!
+    # Note that since we only care about IK functionality, we turn off physics for its arm (making it "visual_only")
+    # This means that both gravity and collisions are disabled for the robot
+    # Note that any object can also have its visual_only attribute set to True!
+    robot = Fetch(prim_path="/World/robot", name="robot", visual_only=True)
+    sim.import_object(robot)
+
+    # Set robot base at the origin
+    robot.set_position_orientation(np.array([0, 0, 0]), np.array([0, 0, 0, 1]))
+    # At least one simulation step while the simulator is playing must occur for the robot (or in general, any object)
+    # to be fully initialized after it is imported into the simulator
+    sim.play()
+    sim.step()
+    # Make sure none of the joints are moving
+    robot.keep_still()
+
+    # Create the IK solver -- note that we are controlling both the trunk and the arm since both are part of the
+    # controllable kinematic chain for the end-effector!
+    control_idx = np.concatenate([robot.trunk_control_idx, robot.arm_control_idx[robot.default_arm]])
+    ik_solver = IKSolver(
+        robot_description_path=robot.robot_arm_descriptor_yamls[robot.default_arm],
+        robot_urdf_path=robot.robot_urdf,
+        default_joint_pos=robot.get_joint_positions()[control_idx],
+        eef_name=robot.eef_link_names[robot.default_arm],
     )
 
-    robot_joint_names = [
-        "r_wheel_joint",
-        "l_wheel_joint",
-        "torso_lift_joint",
-        "head_pan_joint",
-        "head_tilt_joint",
-        "shoulder_pan_joint",
-        "shoulder_lift_joint",
-        "upperarm_roll_joint",
-        "elbow_flex_joint",
-        "forearm_roll_joint",
-        "wrist_flex_joint",
-        "wrist_roll_joint",
-        "r_gripper_finger_joint",
-        "l_gripper_finger_joint",
-    ]
-    arm_joints_names = [
-        "torso_lift_joint",
-        "shoulder_pan_joint",
-        "shoulder_lift_joint",
-        "upperarm_roll_joint",
-        "elbow_flex_joint",
-        "forearm_roll_joint",
-        "wrist_flex_joint",
-        "wrist_roll_joint",
-    ]
+    # Define a helper function for executing specific end-effector commands using the ik solver
+    def execute_ik(pos, quat=None, max_iter=100):
+        logging.info("Querying joint configuration to current marker position")
+        # Grab the joint positions in order to reach the desired pose target
+        joint_pos = ik_solver.solve(
+            target_pos=pos,
+            target_quat=quat,
+            max_iterations=max_iter,
+        )
+        if joint_pos is not None:
+            logging.info("Solution found. Setting new arm configuration.")
+            robot.set_joint_positions(joint_pos, indices=control_idx)
+        else:
+            logging.info("EE position not reachable.")
+        sim.step()
 
-    # Indices of the joints of the arm in the vectors returned by IK and motion planning (excluding wheels, head, fingers)
-    robot_arm_indices = [robot_joint_names.index(arm_joint_name) for arm_joint_name in arm_joints_names]
-
-    # PyBullet ids of the joints corresponding to the joints of the arm
-    arm_joint_ids = joints_from_names(robot_id, arm_joints_names)
-    all_joint_ids = joints_from_names(robot_id, robot_joint_names)
-
-    set_joint_positions(robot_id, arm_joint_ids, arm_default_joint_positions)
-
-    # Set robot base
-    fetch.set_position_orientation([0, 0, 0], [0, 0, 0, 1])
-    fetch.keep_still()
-
-    # Get initial EE position
-    x, y, z = fetch.get_eef_position()
-
-    # Define the limits (max and min, range), and resting position for the joints, including the two joints of the
-    # wheels of the base (indices 0 and 1), the two joints for the head (indices 3 and 4) and the two joints of the
-    # fingers (indices 12 and 13)
-    max_limits = get_max_limits(robot_id, all_joint_ids)
-    min_limits = get_min_limits(robot_id, all_joint_ids)
-    rest_position = robot_default_joint_positions
-    joint_range = list(np.array(max_limits) - np.array(min_limits))
-    joint_range = [item + 1 for item in joint_range]
-    joint_damping = [0.1 for _ in joint_range]
-
-    def accurate_calculate_inverse_kinematics(robot_id, eef_link_id, target_pos, threshold, max_iter):
-        logging.info("IK solution to end effector position {}".format(target_pos))
-        # Save initial robot pose
-        state_id = p.saveState()
-
-        max_attempts = 5
-        solution_found = False
-        joint_poses = None
-        for attempt in range(1, max_attempts + 1):
-            logging.info("Attempt {} of {}".format(attempt, max_attempts))
-            # Get a random robot pose to start the IK solver iterative process
-            # We attempt from max_attempt different initial random poses
-            sample_fn = get_sample_fn(robot_id, arm_joint_ids)
-            sample = np.array(sample_fn())
-            # Set the pose of the robot there
-            set_joint_positions(robot_id, arm_joint_ids, sample)
-
-            it = 0
-            # Query IK, set the pose to the solution, check if it is good enough repeat if not
-            while it < max_iter:
-
-                joint_poses = p.calculateInverseKinematics(
-                    robot_id,
-                    eef_link_id,
-                    target_pos,
-                    lowerLimits=min_limits,
-                    upperLimits=max_limits,
-                    jointRanges=joint_range,
-                    restPoses=rest_position,
-                    jointDamping=joint_damping,
-                )
-                joint_poses = np.array(joint_poses)[robot_arm_indices]
-
-                set_joint_positions(robot_id, arm_joint_ids, joint_poses)
-
-                dist = l2_distance(fetch.get_eef_position(), target_pos)
-                if dist < threshold:
-                    solution_found = True
-                    break
-                logging.debug("Dist: " + str(dist))
-                it += 1
-
-            if solution_found:
-                logging.info("Solution found at iter: " + str(it) + ", residual: " + str(dist))
-                break
-            else:
-                logging.info("Attempt failed. Retry")
-                joint_poses = None
-
-        restoreState(state_id)
-        p.removeState(state_id)
-        return joint_poses
-
-    threshold = 0.03
-    max_iter = 100
     if programmatic_pos or headless:
+        # Sanity check IK using pre-defined hardcoded positions
         query_positions = [[1, 0, 0.8], [1, 1, 1], [0.5, 0.5, 0], [0.5, 0.5, 0.5]]
-        for pos in query_positions:
-            logging.info("Querying joint configuration to current marker position")
-            joint_pos = accurate_calculate_inverse_kinematics(
-                robot_id, fetch.eef_links[fetch.default_arm].link_id, pos, threshold, max_iter
-            )
-            if joint_pos is not None and len(joint_pos) > 0:
-                logging.info("Solution found. Setting new arm configuration.")
-                set_joint_positions(robot_id, arm_joint_ids, joint_pos)
-            else:
-                logging.info("EE position not reachable.")
-            fetch.set_position_orientation([0, 0, 0], [0, 0, 0, 1])
-            fetch.keep_still()
-            time.sleep(10)
+        for query_pos in query_positions:
+            execute_ik(query_pos)
+            time.sleep(2)
     else:
-        marker = VisualMarker(visual_shape=p.GEOM_SPHERE, radius=0.06)
-        s.import_object(marker)
-        marker.set_position([x, y, z])
+        # Create a visual marker to be moved by the user, representing desired end-effector position
+        marker = PrimitiveObject(
+            prim_path=f"/World/marker",
+            name="marker",
+            primitive_type="Sphere",
+            radius=0.03,
+            visual_only=True,
+            rgba=[1.0, 0, 0, 1.0],
+        )
+        sim.import_object(marker)
 
-        print_message()
-        quit_now = False
-        while True:
-            keys = p.getKeyboardEvents()
-            for k, v in keys.items():
-                if k == p.B3G_RIGHT_ARROW and (v & p.KEY_IS_DOWN):
-                    y -= 0.01
-                if k == p.B3G_LEFT_ARROW and (v & p.KEY_IS_DOWN):
-                    y += 0.01
-                if k == p.B3G_UP_ARROW and (v & p.KEY_IS_DOWN):
-                    x += 0.01
-                if k == p.B3G_DOWN_ARROW and (v & p.KEY_IS_DOWN):
-                    x -= 0.01
-                if k == ord("z") and (v & p.KEY_IS_DOWN):
-                    z += 0.01
-                if k == ord("x") and (v & p.KEY_IS_DOWN):
-                    z -= 0.01
-                if k == ord(" "):
-                    logging.info("Querying joint configuration to current marker position")
-                    joint_pos = accurate_calculate_inverse_kinematics(
-                        robot_id, fetch.eef_links[fetch.default_arm].link_id, [x, y, z], threshold, max_iter
-                    )
-                    if joint_pos is not None and len(joint_pos) > 0:
-                        logging.info("Solution found. Setting new arm configuration.")
-                        set_joint_positions(robot_id, arm_joint_ids, joint_pos)
-                        print_message()
-                    else:
-                        logging.info(
-                            "No configuration to reach that point. Move the marker to a different configuration and try again."
-                        )
-                if k == ord("q"):
+        # Get initial EE position and set marker to that location
+        command = robot.get_eef_position()
+        marker.set_position(command)
+        sim.step()
+
+        # Setup callbacks for grabbing keyboard inputs from omni
+        exit_now = False
+
+        def keyboard_event_handler(event, *args, **kwargs):
+            nonlocal command, exit_now
+            # Check if we've received a key press or repeat
+            if event.type == carb.input.KeyboardEventType.KEY_PRESS \
+                    or event.type == carb.input.KeyboardEventType.KEY_REPEAT:
+                if event.input == carb.input.KeyboardInput.ENTER:
+                    # Execute the command
+                    execute_ik(pos=command)
+                elif event.input == carb.input.KeyboardInput.ESCAPE:
+                    # Quit
                     logging.info("Quit.")
-                    quit_now = True
-                    break
+                    exit_now = True
+                else:
+                    # We see if we received a valid delta command, and if so, we update our command and visualized
+                    # marker position
+                    delta_cmd = input_to_xyz_delta_command(inp=event.input)
+                    if delta_cmd is not None:
+                        command = command + delta_cmd
+                        marker.set_position(command)
+                        sim.step()
 
-            if quit_now:
-                break
+            # Callback must return True if valid
+            return True
 
-            marker.set_position([x, y, z])
-            fetch.set_position_orientation([0, 0, 0], [0, 0, 0, 1])
-            fetch.keep_still()
-            s.step()
+        # Hook up the callback function with omni's user interface
+        appwindow = omni.appwindow.get_default_app_window()
+        input_interface = carb.input.acquire_input_interface()
+        keyboard = appwindow.get_keyboard()
+        sub_keyboard = input_interface.subscribe_to_keyboard_events(keyboard, keyboard_event_handler)
 
-    s.disconnect()
+        # Print out helpful information to the user
+        print_message()
+
+        # Loop until the user requests an exit
+        while not exit_now:
+            sim.step()
+
+    # Always shut the simulation down cleanly at the end
+    app.close()
+
+
+def input_to_xyz_delta_command(inp, delta=0.01):
+    mapping = {
+        carb.input.KeyboardInput.W: np.array([delta, 0, 0]),
+        carb.input.KeyboardInput.S: np.array([-delta, 0, 0]),
+        carb.input.KeyboardInput.DOWN: np.array([0, 0, -delta]),
+        carb.input.KeyboardInput.UP: np.array([0, 0, delta]),
+        carb.input.KeyboardInput.A: np.array([0, delta, 0]),
+        carb.input.KeyboardInput.D: np.array([0, -delta, 0]),
+    }
+
+    return mapping.get(inp)
 
 
 def print_message():
     print("*" * 80)
-    print("Move the marker to a desired position to query IK and press SPACE")
-    print("Up/Down arrows: move marker further away or closer to the robot")
-    print("Left/Right arrows: move marker to the left or the right of the robot")
-    print("z/x: move marker up and down")
-    print("q: quit")
+    print("Move the marker to a desired position to query IK and press ENTER")
+    print("W/S: move marker further away or closer to the robot")
+    print("A/D: move marker to the left or the right of the robot")
+    print("T/G: move marker up and down")
+    print("ESC: quit")
 
 
 if __name__ == "__main__":
