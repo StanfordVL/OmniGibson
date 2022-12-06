8514ba494ae2e70980dadd98967431116cf7e725
cremebrule <84cremebrule@gmail.com>
2022-12-06 12:07:38 -0800

    prune and refactor learning examples

--- omnigibson/examples/learning/stable_baselines3_ppo_example.py
+++ omnigibson/examples/learning/stable_baselines3_ppo_example.py
@@ -1,11 +1,8 @@
+import argparse
 import logging
 import os, time, cv2
-from typing import Callable
-import pdb
-import bddl
 
-import omnigibson as ig
-from omnigibson.wrappers import ActionPrimitiveWrapper
+import omnigibson as og
 from omnigibson import example_config_path
 
 log = logging.getLogger(__name__)
@@ -101,14 +98,44 @@ class CustomCombinedExtractor(BaseFeaturesExtractor):
 
 
 def main():
-    config_file = f"{example_config_path}/behavior_mp_tiago.yaml"
+    # Parse args
+    parser = argparse.ArgumentParser(description="Train or evaluate a PPO agent in BEHAVIOR")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default=f"{example_config_path}/fetch_behavior.yaml",
+        help="Absolute path to desired OmniGibson environment config to load",
+    )
+
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default=None,
+        help="Absolute path to desired PPO checkpoint to load for evaluation",
+    )
+
+    parser.add_argument(
+        "--eval",
+        action="store_true",
+        help="If set, will evaluate the PPO agent found from --checkpoint",
+    )
+
+    args = parser.parse_args()
     tensorboard_log_dir = os.path.join("log_dir", time.strftime("%Y%m%d-%H%M%S"))
     os.makedirs(tensorboard_log_dir, exist_ok=True)
     prefix = ''
     seed = 0
 
-    env = ig.Environment(configs=config_file, action_timestep=1 / 60., physics_timestep=1 / 60.)
-    env = ActionPrimitiveWrapper(env=env, action_generator="BehaviorActionPrimitives", mode='baseline')
+    env = og.Environment(configs=args.config, action_timestep=1 / 60., physics_timestep=1 / 60.)
+
+    # If we're evaluating, hide the ceilings and enable camera teleoperation so the user can easily
+    # visualize the rollouts dynamically
+    if args.eval:
+        ceiling = env.scene.object_registry("name", "ceilings")
+        ceiling.visible = False
+        og.sim.enable_viewer_camera_teleoperation()
+
+    # Set the set
     set_random_seed(seed)
     env.reset()
 
@@ -118,36 +145,34 @@ def main():
 
     os.makedirs(tensorboard_log_dir, exist_ok=True)
 
-    model = PPO(
-        "MultiInputPolicy",
-        env,
-        verbose=1,
-        tensorboard_log=tensorboard_log_dir,
-        policy_kwargs=policy_kwargs,
-        n_steps=20 * 10,
-        batch_size=8,
-        device='cuda',
-    )
-    '''
-    load_path = 'log_dir/20221016-193306/_5000_steps'
-    model = PPO.load(load_path)
-    print(model.policy)
-    for name, param in model.policy.named_parameters():
-        print(name, param)
-    model.set_env(env)
-    print('Successfully RESUME from {}'.format(load_path))
-    '''
-    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=tensorboard_log_dir, name_prefix=prefix)
-    log.debug(model.policy)
-    print(model)
+    if args.eval:
+        assert args.checkpoint is not None, "If evaluating a PPO policy, @checkpoint argument must be specified!"
+        model = PPO.load(args.checkpoint)
+        print("Starting evaluation...")
+        mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=50)
+        print("Finished evaluation!")
+        log.info(f"Mean reward: {mean_reward} +/- {std_reward:.2f}")
 
-    model.learn(total_timesteps=10000000, callback=checkpoint_callback,
-                eval_env=env, eval_freq=1000,
-                n_eval_episodes=20)
+    else:
+        model = PPO(
+            "MultiInputPolicy",
+            env,
+            verbose=1,
+            tensorboard_log=tensorboard_log_dir,
+            policy_kwargs=policy_kwargs,
+            n_steps=20 * 10,
+            batch_size=8,
+            device='cuda',
+        )
+        checkpoint_callback = CheckpointCallback(save_freq=1000, save_path=tensorboard_log_dir, name_prefix=prefix)
+        log.debug(model.policy)
+        print(model)
 
-    model = PPO.load(os.path.join(tensorboard_log_dir, "ckpt_{}".format(prefix)))
-    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=20)
-    log.info(f"After Loading: Mean reward: {mean_reward} +/- {std_reward:.2f}")
+        print("Starting training...")
+        model.learn(total_timesteps=10000000, callback=checkpoint_callback,
+                    eval_env=env, eval_freq=1000,
+                    n_eval_episodes=20)
+        print("Finished training!")
 
 
 if __name__ == "__main__":
