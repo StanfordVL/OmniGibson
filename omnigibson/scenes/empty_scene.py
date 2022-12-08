4cce78333cca6b0f8a673ac0acc190775f128ab5
cremebrule <84cremebrule@gmail.com>
2022-12-08 02:06:50 -0800

    revert to using old ground plane logic for better stability

--- omnigibson/scenes/empty_scene.py
+++ omnigibson/scenes/empty_scene.py
@@ -3,9 +3,7 @@ import logging
 import numpy as np
 
 from omnigibson.scenes.scene_base import Scene
-from omnigibson.objects.primitive_object import PrimitiveObject
 import omnigibson.utils.transform_utils as T
-from pxr import PhysxSchema, UsdPhysics
 
 
 class EmptyScene(Scene):
@@ -29,27 +27,7 @@ class EmptyScene(Scene):
     def _load(self, simulator):
         # Initialize systems
         self.initialize_systems(simulator)
-
-        # Load ground plane
-        self.ground_plane = PrimitiveObject(
-            prim_path="/World/ground_plane",
-            name="ground_plane",
-            primitive_type="Cube",
-            category="floors",
-            model="ground_plane",
-            scale=[2500, 2500, 1e-5],
-            visible=self.floor_plane_visible,
-            fixed_base=True,
-            rgba=[*self.floor_plane_color, 1.0],
-        )
-
-        simulator.import_object(self.ground_plane)
-        self.ground_plane.set_position([0, 0, -self.ground_plane.scale[2] / 2.0])
-
-        # We also add an extra ground plane geom for more stability
-        plane_prim = PhysxSchema.Plane.Define(simulator.stage, f"{self.ground_plane.root_link.prim_path}/collisionPlane").GetPrim()
-        UsdPhysics.CollisionAPI.Apply(plane_prim)
-        PhysxSchema.PhysxCollisionAPI.Apply(plane_prim)
+        self.add_ground_plane(color=self.floor_plane_color, visible=self.floor_plane_visible)
 
     def get_random_point(self, floor=None):
         """
