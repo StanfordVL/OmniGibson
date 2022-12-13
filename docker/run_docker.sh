8cb224b9f2f6212db49698783fc612433425f9d2
Cem Gökmen <cgokmen@stanford.edu>
2022-12-13 23:38:21 +1300

    Add Omniverse EULA stuff

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,7 +2,19 @@
 
 DATA_PATH=${1:-'~/omnigibson-data'}
 
-docker login nvcr.io
+echo "The NVIDIA Omniverse License Agreement (EULA) must be accepted before"
+echo "Omniverse Kit can start. The license terms for this product can be viewed at"
+echo "https://docs.omniverse.nvidia.com/app_isaacsim/common/NVIDIA_Omniverse_License_Agreement.html"
+
+while true; do
+    read -p "Do you accept the Omniverse EULA? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 docker run \
     --gpus all \
     --privileged \
