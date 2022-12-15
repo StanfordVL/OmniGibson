f5bba0cf474bd5f1b2788c012386d81230468551
wensi-ai <va0817@g.ucla.edu>
2022-12-14 23:24:56 -0800

    Fix bugs in setup_conda_env

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -12,16 +12,23 @@ rm -r "${ISAAC_SIM_PATH}/exts/omni.isaac.ml_archive/pip_prebundle/gym*"
 # Create a conda environment with python 3.7
 conda create -y -n omnigibson python=3.7
 
+# Now activate the omnigibson environment
+conda activate omnigibson
+
+mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
+mkdir -p ${CONDA_PREFIX}/etc/conda/deactivate.d
+touch ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
+touch ${CONDA_PREFIX}/etc/conda/deactivate.d/env_vars.sh
 # We add some preprocessing information so that the Isaac Sim paths are linked to this environment upon startup
 # See https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#macos-and-linux for reference
-CONDA_ACT_FILE="${CONDA_PREFIX}/etc/conda/activate.d"
-echo "#!/bin/sh" > ${CONDA_ACT_FILE}
+CONDA_ACT_FILE="${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh"
+echo '#!/bin/sh' > ${CONDA_ACT_FILE}
 echo "export LD_LIBRARY_PATH_OLD=\$LD_LIBRARY_PATH" >> ${CONDA_ACT_FILE}
 echo "export PYTHONPATH_OLD=\$PYTHONPATH" >> ${CONDA_ACT_FILE}
 echo "source ${ISAAC_SIM_PATH}/setup_conda_env.sh" >> ${CONDA_ACT_FILE}
 
-CONDA_DEACT_FILE="${CONDA_PREFIX}/etc/conda/deactivate.d"
-echo "#!/bin/sh" > ${CONDA_DEACT_FILE}
+CONDA_DEACT_FILE="${CONDA_PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+echo '#!/bin/sh' > ${CONDA_DEACT_FILE}
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH_OLD" >> ${CONDA_DEACT_FILE}
 echo "export PYTHONPATH=\$PYTHONPATH_OLD" >> ${CONDA_DEACT_FILE}
 echo "unset ISAAC_PATH" >> ${CONDA_DEACT_FILE}
@@ -29,6 +36,6 @@ echo "unset CARB_APP_PATH" >> ${CONDA_DEACT_FILE}
 echo "unset LD_LIBRARY_PATH_OLD" >> ${CONDA_DEACT_FILE}
 echo "unset PYTHONPATH_OLD" >> ${CONDA_DEACT_FILE}
 
-# Now activate the omnigibson environment and install omnigibson!
-conda activate omnigibson
-pip install -e .
+# Install omnigibson!
+pip install -e . 
+
