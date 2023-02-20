7abcfba749651ce9681704da51dd46740b5b9b61
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-02-20 02:08:38 -0800

    Fixes to some stages

--- asset_pipeline/b1k_pipeline/export_scene.py
+++ asset_pipeline/b1k_pipeline/export_scene.py
@@ -6,8 +6,8 @@ import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
 from b1k_pipeline.mesh_tree import build_mesh_tree
-from b1k_pipeline.export_objs import compute_object_bounding_box
-
+from b1k_pipeline.export_objs_global import compute_object_bounding_box
+from b1k_pipeline.utils import PIPELINE_ROOT
 
 OFFSETS = {
     "restaurant_hotel": np.array([-13697.9,-14270.8,-3387.5]) / 1000.0,
@@ -17,10 +17,10 @@ OFFSETS = {
 def main():
     target = sys.argv[1]
     scene_name = os.path.split(target)[-1]
-    object_list_filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cad", target, "artifacts/object_list.json")
-    mesh_root_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cad", target, "artifacts/meshes")
-    objects_root_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "artifacts/aggregate/objects")
-    output_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cad", target, "artifacts/scene/urdf")
+    object_list_filename = PIPELINE_ROOT / "cad" / target / "artifacts/object_list.json"
+    mesh_root_dir = PIPELINE_ROOT / "cad" / target / "artifacts/meshes"
+    objects_root_dir = PIPELINE_ROOT / "artifacts/aggregate/objects"
+    output_dir = PIPELINE_ROOT / "cad" / target / "artifacts/scene/urdf"
 
     # Load the mesh list from the object list json.
     with open(object_list_filename, "r") as f:
@@ -48,7 +48,8 @@ def main():
             obj_rooms = ""
 
         # Assert that this object is available in the objects folder.
-        assert os.path.exists(os.path.join(objects_root_dir, obj_name.replace("-", "/"))), f"Could not find object {obj_name} in objects directory."
+        obj_path = objects_root_dir / obj_name.replace("-", "/")
+        assert obj_path.exists(), f"Could not find object {obj_name} in objects directory."
 
         # Get the relevant bbox info.
         bbox_size, _, bbox_world_center, bbox_world_rot = compute_object_bounding_box(G.nodes[root_node])
@@ -82,8 +83,8 @@ def main():
         joint_child = ET.SubElement(joint, "child")
         joint_child.attrib = {"link": obj_name_in_scene}
 
-    scene_urdf_file = os.path.join(output_dir, f"{scene_name}_best.urdf")
-    os.makedirs(output_dir, exist_ok=True)
+    scene_urdf_file = output_dir / f"{scene_name}_best.urdf"
+    scene_urdf_file.mkdir(parents=True, exist_ok=True)
     xmlstr = minidom.parseString(ET.tostring(scene_tree_root)).toprettyxml(indent="   ")
     with open(scene_urdf_file, "w") as f:
         f.write(xmlstr)
@@ -92,7 +93,7 @@ def main():
     tree.write(scene_urdf_file, xml_declaration=True)
 
     # If we got here, we were successful. Let's create the success file.
-    success_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cad", target, "artifacts/export_scene.success")
+    success_file = PIPELINE_ROOT / "cad" / target / "artifacts/export_scene.success"
     with open(success_file, "w"):
         pass
 
