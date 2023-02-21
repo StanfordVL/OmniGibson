c4e7ff2f63dbcb4f47d41e4c8e50fa954231caf2
Cem Gökmen <cem@cemgokmen.com>
2023-02-21 02:43:01 -0800

    Show progress in USDify

--- asset_pipeline/b1k_pipeline/usd_conversion/usdify_dataset.py
+++ asset_pipeline/b1k_pipeline/usd_conversion/usdify_dataset.py
@@ -3,6 +3,7 @@ Script to import scene and objects
 """
 import os
 
+import tqdm
 from omnigibson.macros import gm
 
 # Set some macros. Is this kosher?
@@ -12,11 +13,14 @@ gm.USE_GPU_DYNAMICS = False
 gm.USE_ENCRYPTED_ASSETS = True
 
 from omnigibson import app
+
 from b1k_pipeline.usd_conversion.import_metadata import (
     import_models_metadata_from_scene,
     import_obj_metadata,
 )
-from b1k_pipeline.usd_conversion.import_scene_template import import_models_template_from_scene
+from b1k_pipeline.usd_conversion.import_scene_template import (
+    import_models_template_from_scene,
+)
 from b1k_pipeline.usd_conversion.import_urdfs_from_scene import (
     import_obj_urdf,
     import_objects_from_scene_urdf,
@@ -27,21 +31,25 @@ IMPORT_RENDER_CHANNELS = True
 
 
 if __name__ == "__main__":
-    for obj_category in os.listdir(os.path.join(DATASET_ROOT, "objects")):
-        for obj_model in os.listdir(
-            os.path.join(DATASET_ROOT, "objects", obj_category)
-        ):
-            print(f"IMPORTING CATEGORY/MODEL {obj_category}/{obj_model}...")
-            import_obj_urdf(
-                obj_category=obj_category, obj_model=obj_model, skip_if_exist=False
-            )
-            import_obj_metadata(
-                obj_category=obj_category,
-                obj_model=obj_model,
-                import_render_channels=IMPORT_RENDER_CHANNELS,
-            )
+    obj_cats = os.listdir(os.path.join(DATASET_ROOT, "objects"))
+    obj_items = [
+        (obj_category, obj_model)
+        for obj_category in obj_cats
+        for obj_model in os.listdir(os.path.join(DATASET_ROOT, "objects", obj_category))
+    ]
+    for obj_category, obj_model in tqdm.tqdm(obj_items):
+        print(f"IMPORTING CATEGORY/MODEL {obj_category}/{obj_model}...")
+        import_obj_urdf(
+            obj_category=obj_category, obj_model=obj_model, skip_if_exist=False
+        )
+        import_obj_metadata(
+            obj_category=obj_category,
+            obj_model=obj_model,
+            import_render_channels=IMPORT_RENDER_CHANNELS,
+        )
 
-    for scene in os.listdir(os.path.join(DATASET_ROOT, "scenes")):
+    scenes = list(os.listdir(os.path.join(DATASET_ROOT, "scenes")))
+    for scene in tqdm.tqdm(scenes):
         urdf = f"{DATASET_ROOT}/scenes/{scene}/urdf/{scene}_best.urdf"
         usd_out = f"{DATASET_ROOT}/scenes/{scene}/usd/{scene}_best_template.usd"
 
