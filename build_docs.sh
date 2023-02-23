efa2d2c08e0ec91780d01c1a2f2653df02fbdfff
Cem Gökmen <cem@cemgokmen.com>
2023-02-22 19:42:32 -0800

    Force docs rm

--- build_docs.sh
+++ build_docs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Remove source directory and copy over source files to docs folder
-rm -r docs/src
+rm -rf docs/src
 mkdir docs/src
 cp -r omnigibson/* docs/src
 
 # Update code source references
-rm -r docs/reference
+rm -rf docs/reference
 python docs/gen_ref_pages.py
 
 # Build the docs (written to ./site)
