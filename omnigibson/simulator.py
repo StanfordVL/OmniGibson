a6ef41e3cf98d56af305e68dc6a29be909b70172
cremebrule <84cremebrule@gmail.com>
2023-04-20 15:43:42 -0700

    create physics sim view in simulator.py

--- omnigibson/simulator.py
+++ omnigibson/simulator.py
@@ -470,6 +470,10 @@ class Simulator(SimulationContext, Serializable):
 
                 self.step_physics()
 
+                # Initialize physics view
+                self._physics_sim_view = omni.physics.tensors.create_simulation_view(self.backend)
+                self._physics_sim_view.set_subspace_roots("/")
+
             # Additionally run non physics things
             self._non_physics_step()
 
