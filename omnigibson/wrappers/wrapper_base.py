279a56433fb80ab53df860ff3362c67308dce972
cremebrule <84cremebrule@gmail.com>
2023-05-22 14:26:50 -0700

    wip refactoring wrapper classes

--- omnigibson/wrappers/wrapper_base.py
+++ omnigibson/wrappers/wrapper_base.py
@@ -9,11 +9,12 @@ class BaseWrapper:
     Base class for all wrappers in OmniGibson
 
     Args:
-        env (OmniGibsonEnv): The environment to wrap.
+        obj (any): Arbitrary python object instance to wrap
     """
 
-    def __init__(self, env):
-        self.env = env
+    def __init__(self, obj):
+        # Set the internal attributes -- store wrapped obj
+        self.wrapped_obj = obj
 
     @classmethod
     def class_name(cls):
@@ -25,67 +26,35 @@ class BaseWrapper:
         Raises:
             Exception: [Double wrapping env]
         """
-        env = self.env
+        obj = self.wrapped_obj
         while True:
-            if isinstance(env, BaseWrapper):
-                if env.class_name() == self.class_name():
+            if isinstance(obj, BaseWrapper):
+                if obj.class_name() == self.class_name():
                     raise Exception("Attempted to double wrap with Wrapper: {}".format(self.__class__.__name__))
-                env = env.env
+                obj = obj.wrapped_obj
             else:
                 break
-
-    def step(self, action):
-        """
-        By default, run the normal environment step() function
-        Args:
-            action (np.array): action to take in environment
-        Returns:
-            4-tuple:
-                - (dict) observations from the environment
-                - (float) reward from the environment
-                - (bool) whether the current episode is completed or not
-                - (dict) misc information
-        """
-        return self.env.step(action)
-
-    def reset(self):
-        """
-        By default, run the normal environment reset() function
-        Returns:
-            dict: Environment observation space after reset occurs
-        """
-        return self.env.reset()
-
-    def observation_spec(self):
-        """
-        By default, grabs the normal environment observation_spec
-        Returns:
-            dict: Observations from the environment
-        """
-        return self.env.observation_spec()
 
     @property
     def unwrapped(self):
         """
-        Grabs unwrapped environment
+        Grabs unwrapped object
+
         Returns:
-            env (OmniGibsonEnv): The unwrapped environment
+            any: The unwrapped object instance
         """
-        if hasattr(self.env, "unwrapped"):
-            return self.env.unwrapped
-        else:
-            return self.env
+        return self.wrapped_obj.unwrapped if hasattr(self.wrapped_obj, "unwrapped") else self.wrapped_obj
 
     # this method is a fallback option on any methods the original env might support
     def __getattr__(self, attr):
         # using getattr ensures that both __getattribute__ and __getattr__ (fallback) get called
         # (see https://stackoverflow.com/questions/3278077/difference-between-getattr-vs-getattribute)
-        orig_attr = getattr(self.env, attr)
+        orig_attr = getattr(self.wrapped_obj, attr)
         if callable(orig_attr):
             def hooked(*args, **kwargs):
                 result = orig_attr(*args, **kwargs)
                 # prevent wrapped_class from becoming unwrapped
-                if result == self.env:
+                if result == self.wrapped_obj:
                     return self
                 return result
             return hooked
