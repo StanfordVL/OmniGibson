d2d8f0b4a572c2dbf0c2807f0cc0eb748fc02709
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-05-22 17:06:41 -0700

    Fix NLTK dependency

--- bddl/bddl/data_generation/run_everything.py
+++ bddl/bddl/data_generation/run_everything.py
@@ -5,6 +5,7 @@ from bddl.data_generation.propagate_by_intersection import create_get_save_propa
 from bddl.data_generation.process_prop_param_annots import create_get_save_propagated_annots_params
 import pandas as pd
 import csv
+import nltk
 
 '''
 Inputs:
@@ -18,30 +19,36 @@ SYN_PROP_DATA_FN = pathlib.Path(__file__).parents[1] / "generated_data" / "synse
 # owned_models = syns_mast["synset"].rename("Synset")
 # owned_models.to_csv("owned_models.csv", index=False)
 
-# Get full hierarchy (it's created and saved on import)
-with open(SYN_PROP_DATA_FN, "r") as f:
-    syn_prop_dict = {}
-    for row in csv.DictReader(f):
-        syn_prop_dict[row.pop("synset")] = row
-hierarchy = get_hierarchy(syn_prop_dict)
-syn_prop_df = pd.read_csv(SYN_PROP_DATA_FN)
+def main():
+    nltk.download('wordnet')
 
-# Create, get, and save master canonical 
-annots_canonical = create_get_save_annots_canonical(syn_prop_dict)
-# Create, get, and save final canonical from hierarchy, master_canonical
-propagated_canonical = create_get_save_propagated_canonical(hierarchy, annots_canonical)
+    # Get full hierarchy (it's created and saved on import)
+    with open(SYN_PROP_DATA_FN, "r") as f:
+        syn_prop_dict = {}
+        for row in csv.DictReader(f):
+            syn_prop_dict[row.pop("synset")] = row
+    hierarchy = get_hierarchy(syn_prop_dict)
+    syn_prop_df = pd.read_csv(SYN_PROP_DATA_FN)
 
-# Create and save properties_to_synsets from propagated canonical 
-props_to_syns = create_get_save_properties_to_synsets(propagated_canonical)
+    # Create, get, and save master canonical 
+    annots_canonical = create_get_save_annots_canonical(syn_prop_dict)
+    # Create, get, and save final canonical from hierarchy, master_canonical
+    propagated_canonical = create_get_save_propagated_canonical(hierarchy, annots_canonical)
 
-# Create and save synsets_to_descriptors from propagated canonical
-create_get_save_synsets_to_descriptors(propagated_canonical)
+    # Create and save properties_to_synsets from propagated canonical 
+    props_to_syns = create_get_save_properties_to_synsets(propagated_canonical)
 
-# Add parameter info to syns-to-props
-create_get_save_propagated_annots_params(propagated_canonical, props_to_syns)
+    # Create and save synsets_to_descriptors from propagated canonical
+    create_get_save_synsets_to_descriptors(propagated_canonical)
 
-# # Create and save activity-specific hierarchies (no getting because that will get complicated)
-# create_save_activity_specific_hierarchies()
+    # Add parameter info to syns-to-props
+    create_get_save_propagated_annots_params(propagated_canonical, props_to_syns)
 
-# # Create and save single dict of all activity-specific hierarchies. Since the other script is run before it, the files will be updated
-# create_save_all_activity_hierarchy_dict()
+    # # Create and save activity-specific hierarchies (no getting because that will get complicated)
+    # create_save_activity_specific_hierarchies()
+
+    # # Create and save single dict of all activity-specific hierarchies. Since the other script is run before it, the files will be updated
+    # create_save_all_activity_hierarchy_dict()
+
+if __name__ == '__main__':
+    main()
