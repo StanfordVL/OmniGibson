49a51e1597d3d074a7d93edf7e24e119bd5250f4
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-06-02 01:26:21 -0700

    Some nice cleanup

--- asset_pipeline/b1k_pipeline/usd_conversion/usdify_objects.py
+++ asset_pipeline/b1k_pipeline/usd_conversion/usdify_objects.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 import fs.copy
 from fs.multifs import MultiFS
+import fs.path
 from fs.osfs import OSFS
 from fs.tempfs import TempFS
+import tqdm
 
 from b1k_pipeline.utils import ParallelZipFS, PipelineFS
 
@@ -15,15 +17,31 @@ def main():
          ParallelZipFS("metadata.zip") as metadata_fs, \
          ParallelZipFS("objects_usd.zip", write=True) as out_fs, \
          PipelineFS() as pipeline_fs, \
-         OSFS(r"D:\test") as tmp_fs:
-         # TempFS(temp_dir=r"E:\tmp") as tmp_fs:
+         TempFS(temp_dir=r"E:\tmp") as tmp_fs:
         # Create a multifs containing both ZIPs
         multi_fs = MultiFS()
         multi_fs.add_fs("objects", objects_fs, priority=0)
         multi_fs.add_fs("metadata", metadata_fs, priority=1)
 
         # Copy everything over to the tmpfs
-        # fs.copy.copy_fs(multi_fs, tmp_fs)
+        print("Copying input to TempFS...")
+        fs.copy.copy_dir(multi_fs, "/metadata", tmp_fs, "/metadata")
+        objdir_glob = tmp_fs.glob("objects/*/*")
+        for item in tqdm.tqdm(objdir_glob, total=objdir_glob.count().directories):
+            if tmp_fs.opendir(item.path).glob("urdf/*.urdf").count().files == 0:
+                continue
+            if "acetone" not in item.path:
+                continue
+            fs.copy.copy_dir(tmp_fs, item.path, out_fs, item.path)
+
+        # Temporarily move URDF into root of objects
+        print("Moving URDF files...")
+        urdf_glob = tmp_fs.glob("objects/*/*/urdf/*.urdf")
+        for item in tqdm.tqdm(urdf_glob, total=urdf_glob.count().files):
+            orig_path = item.path
+            obj_root_dir = fs.path.dirname(fs.path.dirname(orig_path))
+            new_path = fs.path.join(obj_root_dir, fs.path.basename(orig_path))
+            tmp_fs.move(orig_path, new_path)
 
         sub_env = os.environ.copy()
         sub_env["OMNIGIBSON_HEADLESS"] = "True"
@@ -39,6 +57,12 @@ def main():
                 tmp_fs.getsyspath("/"), str(start), str(end)]
             print("Running batch from", start, "to", end)
             subprocess.run(cmd, shell=True, check=True, env=sub_env)
+
+        # Move the USDs to the output FS
+        print("Copying USDs to output FS...")
+        usd_glob = tmp_fs.glob("objects/*/*/usd")
+        for item in tqdm.tqdm(usd_glob, total=usd_glob.count().directories):
+            fs.copy.copy_dir(tmp_fs, item.path, out_fs, item.path)
 
         # Save the success file.
         pipeline_fs.touch("usdify_objects.success")
