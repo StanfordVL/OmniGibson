30a372169bf033ebd6d2384a3c9216973e6a200d
cremebrule <84cremebrule@gmail.com>
2023-06-09 13:30:51 -0700

    add BEHAVIOR-relevant globals to bddl_utils.py

--- omnigibson/utils/bddl_utils.py
+++ omnigibson/utils/bddl_utils.py
@@ -1,9 +1,19 @@
+import json
+import bddl
+import os
 from bddl.backend_abc import BDDLBackend
 from bddl.condition_evaluation import Negation
 from bddl.logic_base import BinaryAtomicFormula, UnaryAtomicFormula, AtomicFormula
+from bddl.object_taxonomy import ObjectTaxonomy
+import omnigibson as og
+from omnigibson.macros import gm
 from omnigibson.utils.ui_utils import create_module_logger
-
+from omnigibson.utils.python_utils import Wrapper
+from omnigibson.objects.dataset_object import DatasetObject
+from omnigibson.robots import BaseRobot
 from omnigibson import object_states
+from omnigibson.object_states.factory import _KINEMATIC_STATE_SET
+from omnigibson.systems.system_base import REGISTERED_SYSTEMS, is_system_active, get_system
 
 # Create module logger
 log = create_module_logger(module_name=__name__)
@@ -112,6 +122,22 @@ SUPPORTED_PREDICATES = {
     "future": ObjectStateFuturePredicate,
     "real": ObjectStateRealPredicate,
 }
+
+KINEMATIC_STATES_BDDL = frozenset([state.__name__.lower() for state in _KINEMATIC_STATE_SET])
+
+# Load substance and object mapping
+with open(f"{bddl.__path__[0]}/../substance_synset_mapping.json", "r") as f:
+    SUBSTANCE_SYNSET_MAPPING = json.load(f)
+
+
+# BEHAVIOR-related
+OBJECT_TAXONOMY = ObjectTaxonomy() #hierarchy_type="b1k")
+FLOOR_SYNSET = "floor.n.01"
+NON_SAMPLEABLE_SYNSETS = set()
+non_sampleable_category_txt = os.path.join(gm.DATASET_PATH, "metadata/non_sampleable_categories.txt")
+if os.path.isfile(non_sampleable_category_txt):
+    with open(non_sampleable_category_txt) as f:
+        NON_SAMPLEABLE_SYNSETS = set([FLOOR_SYNSET] + [line.strip() for line in f.readlines()])
 
 
 class OmniGibsonBDDLBackend(BDDLBackend):
