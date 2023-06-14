d01619961df7c0fd7a03db422a736e4773f06af4
Sanjana Srivastava <sanjana2@stanford.edu>
2023-06-14 10:36:51 -0700

    reformatted json according to josiahs specs for states

--- bddl/bddl/generated_data/transition_map/tm_submap_params.py
+++ bddl/bddl/generated_data/transition_map/tm_submap_params.py
@@ -1,76 +1,98 @@
 TM_SUBMAPS_TO_PARAMS = {
     "electric_mixer": {
         "input_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "input_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         },
         "output_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "output_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         }
     },
     "heat_cook": {
         "input_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "input_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         },
         "container": {
-            "optional"
+            "required": False,
+            "type": "synset"
         },
-        "container_input_relation": {
-            "required"
-        },
+        # "container_input_relation": {
+        #     "required"
+        # },
         "heat_source": {
-            "optional"
+            "required": False,
+            "type": "synset"
         },
         "output_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "output_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         },
-        "container_output_relation": {
-            "required"
-        },
+        # "container_output_relation": {
+        #     "required"
+        # },
         "timesteps": {
-            "optional"
+            "required": False,
+            "type": "integer"
         }
     },
     "mixing_stick": {
         "input_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "input_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         },
         "output_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "output_states": {
-            "required"
+            "required": True,
+            "type": "atom"
+
         }
     },
     "single_toggleable_machine": {
         "input_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "input_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         },
         "machine": {
-            "required"
+            "required": True,
+            "type": "synset"
+
         },
         "output_objects": {
-            "required"
+            "required": True,
+            "type": "synset"
         },
         "output_states": {
-            "required"
+            "required": True,
+            "type": "atom"
         }
     }
 }
