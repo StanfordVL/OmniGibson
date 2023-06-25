a93e6dc7e57489ff6caa958e4177d7636c48ca46
Cem Gökmen <cem@cemgokmen.com>
2023-06-25 02:56:51 -0700

    Add generated substances file

--- asset_pipeline/b1k_pipeline/max/generate_particles.py
+++ asset_pipeline/b1k_pipeline/max/generate_particles.py
@@ -5,36 +5,55 @@ import csv
 import string
 import random
 
+def get_shape(shape, size):
+    if shape == "cube":
+        obj = rt.Box()
+        obj.width = float(size)
+        obj.height = float(size)
+        obj.length = float(size)
+    elif shape == "sphere":
+        obj = rt.Sphere()
+        obj.radius = float(size) / 2
+    else:
+        raise ValueError("Unknown shape " + shape) 
+    
+    return obj
+
 def generate_particle(name, shape, colors, size):
+    objs = []
     for color in colors.split(";"):
-        if shape == "cube":
-            obj = rt.Box()
-            obj.width = float(size)
-            obj.height = float(size)
-            obj.length = float(size)
-        elif shape == "sphere":
-            obj = rt.Sphere()
-            obj.radius = float(size) / 2
-        else:
-            raise ValueError("Unknown shape " + shape) 
+        # Generate the object
+        obj = get_shape(shape, size)
             
         # Assign a name
         model_id = "".join(random.choices(string.ascii_lowercase, k=6))
         obj.name = f"{name}-{model_id}-0"
+
+        # Create the collision version
+        col_obj = get_shape(shape, size)
+        col_obj.name = obj.name + "-Mcollision"
+        col_obj.parent = obj
+        col_obj.isHidden = True
         
         # Create the material
-        r, g, b = [int(float(x) * 255) for x in color.split(",")]
+        r, g, b = [int(x) for x in color.split(",")]
         mat = rt.VRayMtl()
-        mat.diffuseColor = rt.Color(r, g, b)
+        mat.diffuse = rt.Color(r, g, b)
+        obj.material = mat
+
+        objs.append(obj)
         print("Created", obj.name)
-        return obj
+    return objs
         
 def main():
     with open(r"D:\ig_pipeline\metadata\substance_generation.csv") as f:
-        for i, row in enumerate(csv.DictReader(f)):
+        particles = []
+        for row in csv.DictReader(f):
             if row["Request"] != "GENERATE" or row["Shape"] not in ("cube", "sphere"):
                 continue
-            particle = generate_particle(row["Name"], row["Shape"], row["Color"], row["Diameter (mm)"])
+            particles.extend(generate_particle(row["Name"], row["Shape"], row["Color"], row["Diameter (mm)"]))
+
+        for i, particle in enumerate(particles):
             particle.position = rt.Point3(i * 100, 0, 0)
             
 if __name__ == "__main__":
