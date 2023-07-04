f2f95581176e73855da5600448a5d136146226e8
cremebrule <84cremebrule@gmail.com>
2023-07-03 17:22:27 -0700

    wip metadata

---
+++ omnigibson/utils/object_utils.py
@@ -0,0 +1,19 @@
+"""
+Helper utility functions for computing relevant object information
+"""
+
+
+def sample_stable_orientations(obj):
+    pass
+
+
+def compute_base_link_offset(obj):
+    pass
+
+
+def compute_native_bbox(obj):
+    pass
+
+
+def compute_base_aligned_bboxes(obj):
+    pass
