94480c32c8202afd429be80bd7a826f36b562b3f
Chengshu Li <chengshu@stanford.edu>
2023-07-11 15:35:11 -0700

    fix bddl tests

--- bddl/bddl/data_generation/prop_config.py
+++ bddl/bddl/data_generation/prop_config.py
@@ -10,7 +10,7 @@ GPT_PROP_NAMES = [
     "cookable", 
     "heatSource",
     "coldSource",
-    "fireSource",
+    # "fireSource",       # deprecated
     "sliceable",
     "slicer",
     # "soakable",         # deprecated
