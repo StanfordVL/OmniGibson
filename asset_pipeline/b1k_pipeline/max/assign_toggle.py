fd52de0b097f4d426f92d930bc61821169f11b10
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-07-31 09:57:29 -0700

    Revert "Revert "Merge pull request #76 from StanfordVL/benjamin-scene_completion""
    This reverts commit 16cdd4574b5d76a884c352a0b1d54b09e8a0b6a7.

---
+++ asset_pipeline/b1k_pipeline/max/assign_toggle.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append(r"D:\ig_pipeline")
+
+import pymxs
+
+from b1k_pipeline.utils import parse_name
+
+rt = pymxs.runtime
+
+
+def assign_toggle():
+    target = rt.pickobject()
+
+    assert (
+        len(rt.selection) == 1
+    ), "You have to assign toggle button one at a time - Select toggle button first, then click this button, then select the target object."
+
+    button = rt.selection[0]
+
+    # Parent the collision object to the target object
+    button.parent = target
+
+    # Rename the first object to match the selected object
+    button.name = target.name + "-Mtogglebutton"
+
+    # Validate that the object name is valid
+    assert parse_name(button.name) is not None, f"Done, but please fix invalid name {button.name} for collision object"
+
+
+def assign_toggle_button():
+    try:
+        assign_toggle()
+    except AssertionError as e:
+        # Print message
+        rt.messageBox(str(e))
+        return
+
+
+if __name__ == "__main__":
+    assign_toggle_button()
