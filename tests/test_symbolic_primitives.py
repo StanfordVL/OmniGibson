3dd9665fa88ab9339d485c8d2c346dc173b4dcf0
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-08-24 15:04:21 -0700

    Add more unit tests

--- tests/test_symbolic_primitives.py
+++ tests/test_symbolic_primitives.py
@@ -6,8 +6,7 @@ import omnigibson as og
 from omnigibson import object_states
 from omnigibson.action_primitives.symbolic_semantic_action_primitives import SymbolicSemanticActionPrimitiveSet, SymbolicSemanticActionPrimitives
 
-@pytest.fixture
-def env():
+def start_env():
   config = {
     "env": {
       "initial_pos_z_offset": 0.1
@@ -19,20 +18,8 @@ def env():
     "scene": {
       "type": "InteractiveTraversableScene",
       "scene_model": "Wainscott_0_int",
-      "trav_map_resolution": 0.1,
-      "trav_map_erosion": 2,
-      "trav_map_with_objects": True,
-      "build_graph": True,
-      "num_waypoints": 1,
-      "waypoint_resolution": 0.2,
-      "load_object_categories": ["floors", "walls", "fridge", "sink"],
-      "not_load_object_categories": None,
-      "load_room_types": None,
-      "load_room_instances": None,
-      "load_task_relevant_only": False,
-      "seg_map_resolution": 0.1,
+      "load_object_categories": ["floors", "walls", "countertop", "fridge", "sink", "stove"],
       "scene_source": "OG",
-      "include_robots": True
     },
     "robots": [
       {
@@ -80,13 +67,38 @@ def env():
         }
       }
     ],
-    "objects": []
+    "objects": [
+      {
+          "type": "DatasetObject",
+          "name": "pan",
+          "category": "frying_pan",
+          "model": "mhndon",
+          "position": [5.31, 10.75, 1.],
+      },
+      {
+          "type": "DatasetObject",
+          "name": "steak",
+          "category": "steak",
+          "model": "ppykkp",
+          "position": [5.31, 10.28, 1.],
+      },
+      {
+          "type": "DatasetObject",
+          "name": "sponge",
+          "category": "sponge",
+          "model": "qewotb",
+          "position": [4.75, 10.75, 1.],
+      },
+    ]
   }
 
   env = og.Environment(configs=config)
-
-  # TODO: Add some new objects
+  
   return env
+
+@pytest.fixture
+def env():
+  return start_env()
 
 @pytest.fixture
 def prim_gen(env):
@@ -95,12 +107,28 @@ def prim_gen(env):
   return SymbolicSemanticActionPrimitives(None, scene, robot)
 
 @pytest.fixture
+def countertop(env):
+  return next(iter(env.scene.object_registry("category", "countertop")))
+
+@pytest.fixture
 def fridge(env):
   return next(iter(env.scene.object_registry("category", "fridge")))
 
 @pytest.fixture
 def sink(env):
   return next(iter(env.scene.object_registry("category", "sink")))
+
+@pytest.fixture
+def pan(env):
+  return next(iter(env.scene.object_registry("category", "pan")))
+
+@pytest.fixture
+def steak(env):
+  return next(iter(env.scene.object_registry("category", "steak")))
+
+@pytest.fixture
+def sponge(env):
+  return next(iter(env.scene.object_registry("category", "sponge")))
 
 # def test_navigate():
 #    pass
@@ -111,18 +139,28 @@ def test_open(env, prim_gen, fridge):
     env.step(action)
   assert fridge.states[object_states.Open].get_value()
 
-def test_close():
+def test_close(env, prim_gen, fridge):
   fridge.states[object_states.Open].set_value(True)
   assert fridge.states[object_states.Open].get_value()
   for action in prim_gen.apply_ref(SymbolicSemanticActionPrimitiveSet.CLOSE, fridge):
     env.step(action)
   assert not fridge.states[object_states.Open].get_value()
 
-# def test_place_inside():
-#    pass
+def test_place_inside(env, prim_gen, steak, fridge):
+  assert not steak.states[object_states.Inside].get_value(fridge)
+  for action in prim_gen.apply_ref(SymbolicSemanticActionPrimitiveSet.GRASP, steak):
+    env.step(action)
+  for action in prim_gen.apply_ref(SymbolicSemanticActionPrimitiveSet.PLACE_INSIDE, fridge):
+    env.step(action)
+  assert steak.states[object_states.Inside].get_value(fridge)
 
-# def test_place_ontop():
-#    pass
+def test_place_ontop(env, prim_gen, steak, pan):
+  assert not steak.states[object_states.OnTop].get_value(pan)
+  for action in prim_gen.apply_ref(SymbolicSemanticActionPrimitiveSet.GRASP, steak):
+    env.step(action)
+  for action in prim_gen.apply_ref(SymbolicSemanticActionPrimitiveSet.PLACE_ON_TOP, pan):
+    env.step(action)
+  assert steak.states[object_states.OnTop].get_value(pan)
 
 # def test_toggle_on():
 #    pass
@@ -144,3 +182,8 @@ def test_close():
 
 # def test_wait_for_cooked():
 #    pass
+
+if __name__ == "__main__":
+  env = start_env()
+  while True:
+    env.step(env.action_space.sample())
