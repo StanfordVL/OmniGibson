c0ddc2126583825f40d8b4d1065e9987690cbf7c
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-10-11 13:45:51 -0700

    Remove some util stuff

--- omnigibson/object_states/particle_modifier.py
+++ omnigibson/object_states/particle_modifier.py
@@ -16,10 +16,10 @@ from omnigibson.object_states.update_state_mixin import UpdateStateMixin
 from omnigibson.systems.system_base import VisualParticleSystem, PhysicalParticleSystem, get_system, \
     is_visual_particle_system, is_physical_particle_system, is_system_active, REGISTERED_SYSTEMS
 from omnigibson.utils.constants import ParticleModifyMethod, ParticleModifyCondition, PrimType
+from omnigibson.utils.deprecated_utils import Core
 from omnigibson.utils.geometry_utils import generate_points_in_volume_checker_function, \
     get_particle_positions_in_frame, get_particle_positions_from_frame
 from omnigibson.utils.python_utils import classproperty
-from omnigibson.utils.sim_utils import meets_minimum_isaac_version
 from omnigibson.utils.ui_utils import suppress_omni_log
 from omnigibson.utils.usd_utils import create_primitive_mesh, FlatcacheAPI
 import omnigibson.utils.transform_utils as T
@@ -117,10 +117,6 @@ def create_projection_visualization(
     UsdGeom.Imageable(source.GetPrim()).MakeInvisible()
 
     # Generate the ComputeGraph nodes to render the projection
-    if meets_minimum_isaac_version("2023.0.0"):
-        from omni.particle.system.core.scripts.core import Core
-    else:
-        from omnigibson.utils.deprecated_utils import Core
     core = Core(lambda val: None, particle_system_name=projection_name)
 
     # Scale radius and height by the parent scale -- projection always points in the negative-z direction of the
--- omnigibson/utils/deprecated_utils.py
+++ omnigibson/utils/deprecated_utils.py
@@ -65,54 +65,13 @@ class CreateMeshPrimWithDefaultXformCommand(CMPWDXC):
         assert isinstance(self._evaluator_class, type)
 
 
-class Utils(OmniUtils):
-    """
-    Subclass that overrides a specific function within Omni's Utils class to fix a bug
-    """
-    def create_material(self, name):
-        # TODO: THIS IS THE ONLY LINE WE CHANGE! "/" SHOULD BE ""
-        material_path = ""
-        default_prim = self.stage.GetDefaultPrim()
-        if default_prim:
-            material_path = default_prim.GetPath().pathString
-
-        if not self.stage.GetPrimAtPath(material_path + "/Looks"):
-            self.stage.DefinePrim(material_path + "/Looks", "Scope")
-        material_path += "/Looks/" + name
-        material_path = ou.get_stage_next_free_path(
-            self.stage, material_path, False
-        )
-        material = UsdShade.Material.Define(self.stage, material_path)
-
-        shader_path = material_path + "/Shader"
-        shader = UsdShade.Shader.Define(self.stage, shader_path)
-
-        # Update Neuraylib MDL search paths
-        import omni.particle.system.core as core
-        core.update_mdl_search_paths()
-
-        shader.SetSourceAsset(name + ".mdl", "mdl")
-        shader.SetSourceAssetSubIdentifier(name, "mdl")
-        shader.GetImplementationSourceAttr().Set(UsdShade.Tokens.sourceAsset)
-        shader.CreateOutput("out", Sdf.ValueTypeNames.Token)
-        material.CreateSurfaceOutput().ConnectToSource(shader, "out")
-
-        return [material_path]
-
-
 class Core(OmniCore):
     """
     Subclass that overrides a specific function within Omni's Core class to fix a bug
     """
     def __init__(self, popup_callback: Callable[[str], None], particle_system_name: str):
-        self._popup_callback = popup_callback
-        self.utils = Utils()    # TODO: THIS IS THE ONLY LINE THAT WE CHANGE! ONCE FIXED, REMOVE THIS
-        self.context = ou.get_context()
-        self.stage = self.context.get_stage()
-        self.selection = self.context.get_selection()
+        super().__init__(popup_callback=popup_callback)
         self.particle_system_name = particle_system_name
-        self.sub_stage_update = self.context.get_stage_event_stream().create_subscription_to_pop(self.on_stage_update)
-        self.on_stage_update()
 
     def get_compute_graph(self, selected_paths, create_new_graph=True, created_paths=None):
         """
@@ -122,7 +81,7 @@ class Core(OmniCore):
         """
         graph = None
         graph_paths = [path for path in selected_paths
-                       if self.stage.GetPrimAtPath(path).GetTypeName() == "ComputeGraph"]
+                       if self.stage.GetPrimAtPath(path).GetTypeName() in ["ComputeGraph", "OmniGraph"] ]
 
         if len(graph_paths) > 0:
             graph = ogc.get_graph_by_path(graph_paths[0])
@@ -130,6 +89,7 @@ class Core(OmniCore):
                 carb.log_warn(f"Multiple ComputeGraph prims selected. Only the first will be used: {graph.get_path_to_graph()}")
         elif create_new_graph:
             # If no graph was found in the selected prims, we'll make a new graph.
+            # TODO: THIS IS THE ONLY LINE THAT WE CHANGE! ONCE FIXED, REMOVE THIS
             graph_path = Sdf.Path(f"/OmniGraph/{self.particle_system_name}").MakeAbsolutePath(Sdf.Path.absoluteRootPath)
             graph_path = ou.get_stage_next_free_path(self.stage, graph_path, True)
 
