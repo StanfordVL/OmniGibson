9e3f6705287802a740663cf24f2d35ce63420d36
Cem Gökmen <cem@cemgokmen.com>
2023-11-15 01:00:39 -0800

    Add new reward and termination back into their registries

--- omnigibson/reward_functions/__init__.py
+++ omnigibson/reward_functions/__init__.py
@@ -1,5 +1,6 @@
 from omnigibson.reward_functions.reward_function_base import BaseRewardFunction, REGISTERED_REWARD_FUNCTIONS
 from omnigibson.reward_functions.collision_reward import CollisionReward
+from omnigibson.reward_functions.grasp_reward import GraspReward
 from omnigibson.reward_functions.point_goal_reward import PointGoalReward
 from omnigibson.reward_functions.potential_reward import PotentialReward
 from omnigibson.reward_functions.reaching_goal_reward import ReachingGoalReward
--- omnigibson/termination_conditions/__init__.py
+++ omnigibson/termination_conditions/__init__.py
@@ -1,6 +1,7 @@
 from omnigibson.termination_conditions.termination_condition_base import REGISTERED_TERMINATION_CONDITIONS, \
     REGISTERED_SUCCESS_CONDITIONS, REGISTERED_FAILURE_CONDITIONS, BaseTerminationCondition
 from omnigibson.termination_conditions.falling import Falling
+from omnigibson.termination_conditions.grasp_goal import GraspGoal
 from omnigibson.termination_conditions.max_collision import MaxCollision
 from omnigibson.termination_conditions.point_goal import PointGoal
 from omnigibson.termination_conditions.predicate_goal import PredicateGoal
