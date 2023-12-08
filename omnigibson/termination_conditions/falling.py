8497affd9a2794143149b7933d43b6825dbcc969
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-12-08 11:17:39 -0800

    Add topple detection functionality

--- omnigibson/termination_conditions/falling.py
+++ omnigibson/termination_conditions/falling.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
 from omnigibson.termination_conditions.termination_condition_base import FailureCondition
 
 
@@ -12,12 +14,15 @@ class Falling(FailureCondition):
             robot added to the scene
         fall_height (float): distance (m) > 0 below the scene's floor height under which the the robot is considered
             to be falling out of the world
+        topple (bool): whether to also consider the robot to be falling if it is toppling over (i.e.: if it is
+            no longer upright
     """
 
-    def __init__(self, robot_idn=0, fall_height=0.03):
+    def __init__(self, robot_idn=0, fall_height=0.03, topple=True):
         # Store internal vars
         self._robot_idn = robot_idn
         self._fall_height = fall_height
+        self._topple = topple
 
         # Run super init
         super().__init__()
@@ -25,4 +30,12 @@ class Falling(FailureCondition):
     def _step(self, task, env, action):
         # Terminate if the specified robot is falling out of the scene
         robot_z = env.scene.robots[self._robot_idn].get_position()[2]
-        return robot_z < (env.scene.get_floor_height() - self._fall_height)
+        if robot_z < (env.scene.get_floor_height() - self._fall_height):
+            return True
+        
+        # Terminate if the robot has toppled over
+        if self._topple:
+            rotation = R.from_quat(env.scene.robots[self._robot_idn].get_orientation())
+            robot_up = rotation.apply(np.array([0, 0, 1]))
+            if robot_up[2] < 0.75:
+                return True
