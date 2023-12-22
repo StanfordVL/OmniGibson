1b04722ebbc79e199f58cae139262ac3fe31bd4a
Cem Gökmen <cem@cemgokmen.com>
2023-12-21 21:45:03 -0800

    What happens if we increase rendering dt?

--- omnigibson/envs/env_base.py
+++ omnigibson/envs/env_base.py
@@ -186,7 +186,7 @@ class Environment(gym.Env, GymObservable, Recreatable):
 
         # Set the simulator settings
         # NOTE: This must be done BEFORE the scene is loaded, or else all vision sensors can't retrieve observations
-        og.sim.set_simulation_dt(physics_dt=self.physics_timestep, rendering_dt=self.action_timestep)
+        og.sim.set_simulation_dt(physics_dt=self.physics_timestep, rendering_dt=self.physics_timestep)
 
         # Create the scene from our scene config
         scene = create_class_from_registry_and_config(
