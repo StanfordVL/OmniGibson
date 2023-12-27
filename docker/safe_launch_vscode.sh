ba92a3bbe8110cb0bdbefbd6832ab62d91c4e971
Cem Gökmen <cem@cemgokmen.com>
2023-12-26 21:09:42 -0800

    Update safe_launch_vscode.sh

--- docker/safe_launch_vscode.sh
+++ docker/safe_launch_vscode.sh
@@ -16,21 +16,22 @@ fi
 sbatch /cvgl/group/Gibson/og-docker/launch_vscode.sh
 
 # Wait for the file to show up
-JOBS_AFTER_LAUNCH=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode || true)
-JOBS_AFTER_LAUNCH_COUNT=$(echo -n "$JOBS_AFTER_LAUNCH" | wc -l)
+JOBS_AFTER_LAUNCH=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode | wc -l || true)
+JOBS_AFTER_LAUNCH_COUNT=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode | wc -l || true)
 
-if [ "$JOBS_AFTER_LAUNCH_COUNT" -eq 1 ]; then
-    echo "Job could not be queued. Please contact the OmniGibson team."
-    exit 1
+while [ ! "$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode | wc -l || true)" -eq 1 ]; do
+    echo "Waiting for the job to launch."
+    sleep 3
 fi
 
 # Get the job id
-LAUNCHED_JOB_ID=$(echo -n "$JOBS_AFTER_LAUNCH" | sed "s/.*://g" | tr -d '\n')
+LAUNCHED_JOB_ID=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode | sed "s/.*://g" | tr -d '\n')
+echo "Job queued successfully. Job ID: $LAUNCHED_JOB_ID"
 
 # Check that the output file exists
 OUTPUT_FILE="~/slurm-${LAUNCHED_JOB_ID}.out"
 while [ ! -f "$OUTPUT_FILE" ]; do
-    echo "Waiting for the job to launch."
+    echo "Waiting for the job to start outputting."
     sleep 3
 done
 echo "Job launched successfully."
