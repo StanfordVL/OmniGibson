1bd6f4f4e7b27bb9f1e5ed442955c26f1acc4dec
Cem Gökmen <cem@cemgokmen.com>
2023-12-27 01:19:42 -0800

    Update safe_launch_vscode.sh

--- docker/safe_launch_vscode.sh
+++ docker/safe_launch_vscode.sh
@@ -12,14 +12,40 @@ if [ ! -z "$CURRENTLY_RUNNING_JOBS" ]; then
 fi
 
 # Queue a new job for the user
-srun \
-    --job-name=omnigibson-vscode \
-    --account=cvgl \
-    --partition=svl-interactive --qos=normal \
-    --nodes=1 \
-    --cpus-per-task=8 \
-    --mem=30G \
-    --gres=gpu:2080ti:1 \
-    /cvgl/group/Gibson/og-docker/launch_vscode.sh 2>&1 | grep -m 3 --line-buffered "OMNIGIBSON-VSCODE"
+sbatch --parsable /cvgl/group/Gibson/og-docker/launch_vscode.sh
 
-echo "You may need to wait up to 1 minute for vscode to launch. The password is your username."
+# Wait for the file to show up
+while ! (squeue -u $USERNAME -o "%j:%i" | grep -q omnigibson-vscode); do
+    echo "Waiting for the job to launch."
+    sleep 3
+done
+
+# Get the job id
+LAUNCHED_JOB_ID=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode | sed "s/.*://g" | tr -d '\n')
+echo "Job queued successfully. Job ID: $LAUNCHED_JOB_ID"
+
+# Check that the output file exists
+OUTPUT_FILE="slurm-${LAUNCHED_JOB_ID}.out"
+while [ ! -f "$OUTPUT_FILE" ]; do
+    echo "Waiting for the job to start outputting."
+    sleep 3
+done
+echo "Job launched successfully."
+
+# Wait for the output file to contain the string OMNIGIBSON-VSCODE exactly 3 times
+while [ "$(grep -c "OMNIGIBSON-VSCODE" "$OUTPUT_FILE" || true)" -lt 3 ]; do
+    echo "Waiting for the job to allocate ports."
+    sleep 3
+done
+echo "Ports allocated successfully."
+
+# Wait for the output file to contain the string "HTTP server listening"
+while ! grep -q "HTTP server listening" "$OUTPUT_FILE"; do
+    echo "Waiting for the job to start the HTTP server."
+    sleep 3
+done
+echo "HTTP server started successfully."
+echo ""
+
+# Echo the OMNIGIBSON-VSCODE lines
+grep "OMNIGIBSON-VSCODE" "$OUTPUT_FILE"
