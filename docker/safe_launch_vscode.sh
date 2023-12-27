32b874079b4e765ab8dfef209b062a5414b95bb7
Cem Gökmen <cem@cemgokmen.com>
2023-12-26 19:20:26 -0800

    Update safe_launch_vscode.sh

--- docker/safe_launch_vscode.sh
+++ docker/safe_launch_vscode.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
 # Get the user's currently running vscode job count
-CURRENTLY_RUNNING_JOBS = $(squeue -u cgokmen -o "%j:%i" | grep vscode)  # TODO: omnigibson-vscode
-CURRENTLY_RUNNING=$(squeue -u cgokmen -o "%j" | grep vscode | wc -l)
+USERNAME=$(whoami)
+CURRENTLY_RUNNING_JOBS=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode)
+CURRENTLY_RUNNING_JOBS_COUNT=$(echo -n $CURRENTLY_RUNNING_JOBS | wc -l)
 
-if [ "$CURRENTLY_RUNNING" -gt 0 ]; then
-    echo "You already have a job running."
+if [ "$CURRENTLY_RUNNING_JOBS_COUNT" -gt 0 ]; then
+    CURRENT_JOB_IDS=$(echo -n "$CURRENTLY_RUNNING_JOBS" | sed "s/.*://g" | tr '\n' ',') 
+    echo "You already have omnigibson-vscode running. First cancel those jobs by running: scancel ${CURRENT_JOB_IDS}"
     exit 1
 fi
+
+# Queue a new job for the user
+sbatch /cvgl/group/Gibson/og-docker/launch_vscode.sh
+
+# Wait for the file to show up
+JOBS_AFTER_LAUNCH=$(squeue -u $USERNAME -o "%j:%i" | grep omnigibson-vscode)
+JOBS_AFTER_LAUNCH_COUNT=$(echo -n $JOBS_AFTER_LAUNCH | wc -l)
+
+if [ "$JOBS_AFTER_LAUNCH_COUNT" -eq 1 ]; then
+    echo "Job could not be queued. Please contact the OmniGibson team."
+    exit 1
+fi
+
+# Get the job id
+LAUNCHED_JOB_ID=$(echo -n "$CURRENTLY_RUNNING_JOBS" | sed "s/.*://g" | tr '\n' '')
+
+# Check that the output file exists
+OUTPUT_FILE="~/slurm-${LAUNCHED_JOB_ID}.out"
+while [ ! -f "$OUTPUT_FILE" ]; do
+    echo "Waiting for the job to launch."
+    sleep 3
+done
+
+# Use tail to output the first 3 lines of the file that contain the string OMNIGIBSON-VSCODE
+tail -f "$OUTPUT_FILE" | grep -m 3 "OMNIGIBSON-VSCODE"
