20e506c06772908a18812658f3a406fab63d3912
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-01-25 13:47:14 -0800

    Update test_rotation.py

--- omnigibson/examples/test_rotation.py
+++ omnigibson/examples/test_rotation.py
@@ -1,4 +1,5 @@
 import omnigibson as og
+import omnigibson.lazy as lazy
 from omnigibson.objects.dataset_object import DatasetObject
 from scipy.spatial.transform import Rotation as R
 import numpy as np
@@ -19,16 +20,26 @@ def main():
   z_coordinate = obj.aabb_extent[2]/2 + offset + 0.5
   obj.set_position([0, 0, z_coordinate])
 
+  dc = lazy.omni.isaac.dynamic_control._dynamic_control.acquire_dynamic_control_interface()
+  handle = dc.get_rigid_body(obj.root_link.prim_path)
+
   step = 0
   while True:
     og.sim.step()
     step += 1
 
     if step % 100 == 0:
-      current_rot = R.from_quat(obj.get_orientation())
+      pose = dc.get_rigid_body_pose(handle)
+      current_orn_quat = list(pose.r)
+      current_rot = R.from_quat(current_orn_quat)
       new_rot = R.from_euler('z', np.pi / 4) * current_rot
-      obj.set_orientation(new_rot.as_quat())
-      print("rotate object from", current_rot.as_quat(), "to", new_rot.as_quat())
+      new_orn_quat = new_rot.as_quat()
+      
+      new_pos = pose.p
+      # new_pos = np.array(pose.p) + np.array([0, 0, 0.1])
+      new_pose = lazy.omni.isaac.dynamic_control._dynamic_control.Transform(new_pos, new_orn_quat)
+      dc.set_rigid_body_pose(handle, new_pose)
+      print("rotate object from", current_orn_quat, "to", new_orn_quat)
 
 if __name__ == "__main__":
   main()
