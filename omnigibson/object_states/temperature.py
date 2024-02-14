d91eef8f6680d4d72cc2c263b6394c6dd3750d4f
cremebrule <84cremebrule@gmail.com>
2024-02-13 17:56:48 -0800

    revert temperature changes

--- omnigibson/object_states/temperature.py
+++ omnigibson/object_states/temperature.py
@@ -3,7 +3,7 @@ from omnigibson.macros import create_module_macros
 from omnigibson.object_states.heat_source_or_sink import HeatSourceOrSink
 from omnigibson.object_states.object_state_base import AbsoluteObjectState
 from omnigibson.object_states.aabb import AABB
-from omnigibson.object_states.update_state_mixin import GlobalUpdateStateMixin
+from omnigibson.object_states.update_state_mixin import UpdateStateMixin
 import omnigibson as og
 
 
@@ -18,42 +18,13 @@ m.DEFAULT_TEMPERATURE = 23.0  # degrees Celsius
 m.TEMPERATURE_DECAY_SPEED = 0.02  # per second. We'll do the conversion to steps later.
 
 
-class Temperature(AbsoluteObjectState, GlobalUpdateStateMixin):
-
-    # Numpy array of raw temperature values
-    VALUES = np.array([])
-
-    # Maps object name to index in VALUES
-    OBJ_IDXS = dict()
+class Temperature(AbsoluteObjectState, UpdateStateMixin):
 
     def __init__(self, obj):
         # Run super first
         super(Temperature, self).__init__(obj)
 
-        # Add this object to the tracked global state
-        self.VALUES = np.concatenate([self.VALUES, np.array([m.DEFAULT_TEMPERATURE])])
-        self.OBJ_IDXS[self.obj.name] = len(self.VALUES) - 1
-
-    @classmethod
-    def remove_object(cls, obj):
-        """
-        Removes a tracked object from the array
-
-        Args:
-            obj (BaseObject): Object to potentially remove from internal global tracked state
-        """
-        if obj in cls.OBJ_IDXS:
-            deleted_idx = cls.OBJ_IDXS.pop(obj.name)
-            new_idxs = dict()
-            for name, old_idx in cls.OBJ_IDXS.items():
-                new_idxs[name] = old_idx - (0 if old_idx < new_idxs else 1)
-            cls.OBJ_IDXS = new_idxs
-            cls.VALUES = np.delete(cls.VALUES, [deleted_idx])
-
-    @classmethod
-    def global_update(cls):
-        # This will globally decay all tracked temperatures
-        cls.VALUES += (m.DEFAULT_TEMPERATURE - cls.VALUES) * m.TEMPERATURE_DECAY_SPEED * og.sim.get_rendering_dt()
+        self.value = m.DEFAULT_TEMPERATURE
 
     @classmethod
     def get_dependencies(cls):
@@ -68,11 +39,15 @@ class Temperature(AbsoluteObjectState, GlobalUpdateStateMixin):
         return deps
 
     def _get_value(self):
-        return self.VALUES[self.OBJ_IDXS[self.obj.name]]
+        return self.value
 
     def _set_value(self, new_value):
-        self.VALUES[self.OBJ_IDXS[self.obj.name]] = new_value
+        self.value = new_value
         return True
+
+    def _update(self):
+        # Apply temperature decay
+        self.value += (m.DEFAULT_TEMPERATURE - self.value) * m.TEMPERATURE_DECAY_SPEED * og.sim.get_rendering_dt()
 
     def update_temperature_from_heatsource_or_sink(self, temperature, rate):
         """
@@ -82,7 +57,7 @@ class Temperature(AbsoluteObjectState, GlobalUpdateStateMixin):
             temperature (float): Heat source / sink temperature
             rate (float): Heating rate of the source / sink
         """
-        self.VALUES[self.OBJ_IDXS[self.obj.name]] += (temperature - self.VALUES[self.OBJ_IDXS[self.obj.name]]) * rate * og.sim.get_rendering_dt()
+        self.value += (temperature - self.value) * rate * og.sim.get_rendering_dt()
 
     @property
     def state_size(self):
@@ -90,10 +65,10 @@ class Temperature(AbsoluteObjectState, GlobalUpdateStateMixin):
 
     # For this state, we simply store its value.
     def _dump_state(self):
-        return dict(temperature=self.VALUES[self.OBJ_IDXS[self.obj.name]])
+        return dict(temperature=self.value)
 
     def _load_state(self, state):
-        self.VALUES[self.OBJ_IDXS[self.obj.name]] = state["temperature"]
+        self.value = state["temperature"]
 
     def _serialize(self, state):
         return np.array([state["temperature"]], dtype=float)
