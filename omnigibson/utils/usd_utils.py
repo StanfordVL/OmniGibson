5039fcb6d78c2bb61a3ed8c30ee0756d59d3f5f6
Hang Yin <yinhang0226@gmail.com>
2024-02-21 15:02:04 -0800

    Update docstring

--- omnigibson/utils/usd_utils.py
+++ omnigibson/utils/usd_utils.py
@@ -724,6 +724,9 @@ class PoseAPI:
     
     @classmethod
     def get_world_pose_with_scale(cls, prim_path):
+        """
+        This is needed only when accessing the parent transform of a prim.
+        """
         cls._refresh()
         return np.array(lazy.omni.isaac.core.utils.xforms._get_world_pose_transform_w_scale(prim_path))
 
