8fc105764e0ca1e8252649c93c5e831d4663aa6f
Chengshu Li <chengshu@stanford.edu>
2024-03-08 14:10:59 -0800

    fix current task

--- omnigibson/sampling/sample_b1k_scenes.py
+++ omnigibson/sampling/sample_b1k_scenes.py
@@ -81,9 +81,7 @@ def main(random_selection=False, headless=False, short_exec=False):
     # Potentially update start_at based on current task observed
     # Current task is either an empty list [] or a filled list [['<ACTIVITY>']]
     current_task = worksheet.get(f"Y{scene_row}")
-    if args.start_at is None and current_task:
-        print("-" * 50)
-        print(current_task)
+    if args.start_at is None and current_task and current_task[0]:
         args.start_at = current_task[0][0]
         # Also clear the in_progress bar in case this is from a failed run
         worksheet.update_acell(f"B{ACTIVITY_TO_ROW[args.start_at]}", "")
