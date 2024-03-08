36439e27516e906f48909f1d8f88cbae52897950
cremebrule <84cremebrule@gmail.com>
2024-03-08 11:24:18 -0800

    update cremebrule sbatch script

--- omnigibson/sampling/cremebrule_sampling_sc.sh
+++ omnigibson/sampling/cremebrule_sampling_sc.sh
@@ -1,16 +1,16 @@
-#sbatch sample_tasks_sbatch.sh -m Ihlen_0_int
-sbatch sample_tasks_sbatch.sh -m Beechwood_0_garden
-sbatch sample_tasks_sbatch.sh -m Beechwood_1_int
-#sbatch sample_tasks_sbatch.sh -m Benevolence_0_int
-#sbatch sample_tasks_sbatch.sh -m Benevolence_1_int
-#sbatch sample_tasks_sbatch.sh -m Benevolence_2_int
-sbatch sample_tasks_sbatch.sh -m Merom_0_garden
-sbatch sample_tasks_sbatch.sh -m Merom_0_int
-sbatch sample_tasks_sbatch.sh -m Pomaria_0_garden
-#sbatch sample_tasks_sbatch.sh -m Pomaria_0_int
-#sbatch sample_tasks_sbatch.sh -m Pomaria_1_int
-#sbatch sample_tasks_sbatch.sh -m Pomaria_2_int
-sbatch sample_tasks_sbatch.sh -m Rs_garden
-sbatch sample_tasks_sbatch.sh -m Wainscott_0_garden
-#sbatch sample_tasks_sbatch.sh -m Wainscott_1_int
-#sbatch sample_tasks_sbatch.sh -m grocery_store_asian
+#sbatch sample_tasks_sbatch.sh -m Ihlen_0_int -s clean_an_electric_razor           # R # C
+#sbatch sample_tasks_sbatch.sh -m Beechwood_0_garden    # C # U
+#sbatch sample_tasks_sbatch.sh -m Beechwood_1_int       # C # R
+#sbatch sample_tasks_sbatch.sh -m Benevolence_0_int     # R # D
+#sbatch sample_tasks_sbatch.sh -m Benevolence_1_int     # R # C
+#sbatch sample_tasks_sbatch.sh -m Benevolence_2_int     # R # D
+#sbatch sample_tasks_sbatch.sh -m Merom_0_garden        # C # U
+#sbatch sample_tasks_sbatch.sh -m Merom_0_int           # C # R
+#sbatch sample_tasks_sbatch.sh -m Pomaria_0_garden      # C # C
+#sbatch sample_tasks_sbatch.sh -m Pomaria_0_int         # R # D
+#sbatch sample_tasks_sbatch.sh -m Pomaria_1_int         # R # C
+#sbatch sample_tasks_sbatch.sh -m Pomaria_2_int         # R # R
+#sbatch sample_tasks_sbatch.sh -m Rs_garden             # C # U
+#sbatch sample_tasks_sbatch.sh -m Wainscott_0_garden    # C # C
+#sbatch sample_tasks_sbatch.sh -m Wainscott_1_int       # R # C
+#sbatch sample_tasks_sbatch.sh -m grocery_store_asian   # R # R
