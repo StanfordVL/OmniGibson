66a0d48f04cc97557c78adc9269fa9a104facfad
Sujay Garlanka <sujay.garlanka@gmail.com>
2024-03-13 21:38:42 -0700

    rs_int example

---
+++ omnigibson/examples/action_primitives/rs_int_example.py
@@ -0,0 +1,62 @@
+"""
+Example script demo'ing robot primitive to solve a task
+"""
+import os
+import yaml
+import numpy as np
+
+import omnigibson as og
+from omnigibson.macros import gm
+from omnigibson.action_primitives.starter_semantic_action_primitives import StarterSemanticActionPrimitives, StarterSemanticActionPrimitiveSet
+
+# Don't use GPU dynamics and use flatcache for performance boost
+# gm.USE_GPU_DYNAMICS = True
+# gm.ENABLE_FLATCACHE = True
+
+def execute_controller(ctrl_gen, env):
+    for action in ctrl_gen:
+        env.step(action)
+
+def main():
+    # Load the config
+    config_filename = os.path.join(og.example_config_path, "fetch_primitives.yaml")
+    config = yaml.load(open(config_filename, "r"), Loader=yaml.FullLoader)
+
+    # Update it to run a grocery shopping task
+    config["scene"]["scene_model"] = "Rs_int"
+    config["scene"]["not_load_object_categories"] = ["ceilings"]
+    config["objects"] = [
+        {
+            "type": "DatasetObject",
+            "name": "apple",
+            "category": "apple",
+            "model": "agveuv",
+            "position": [-0.3, -1.1, 0.5],
+            "orientation": [0, 0, 0, 1]
+        },
+    ]
+
+    # Load the environment
+    env = og.Environment(configs=config)
+    scene = env.scene
+    robot = env.robots[0]
+
+    # Allow user to move camera more easily
+    og.sim.enable_viewer_camera_teleoperation()
+
+    controller = StarterSemanticActionPrimitives(env, enable_head_tracking=False)
+    cabinet = scene.object_registry("name", "bottom_cabinet_slgzfc_0")
+    apple = scene.object_registry("name", "apple")
+    # laptop = scene.object_registry("name", "laptop_nvulcs_0")
+    # Grasp apple
+    print("Executing controller")
+    execute_controller(controller.apply_ref(StarterSemanticActionPrimitiveSet.GRASP, apple), env)
+    print("Finished executing grasp")
+
+    # Place on cabinet
+    print("Executing controller")
+    execute_controller(controller.apply_ref(StarterSemanticActionPrimitiveSet.PLACE_ON_TOP, cabinet), env)
+    print("Finished executing place")
+
+if __name__ == "__main__":
+    main()
