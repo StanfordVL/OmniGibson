15c72e7ad532d2c4a339e9ba6ef1b3467ec16f4c
cremebrule <84cremebrule@gmail.com>
2024-03-15 14:12:53 -0700

    update cremebrule sampling

--- omnigibson/sampling/cremebrule_sampling_sc.sh
+++ omnigibson/sampling/cremebrule_sampling_sc.sh
@@ -1,28 +1,28 @@
-sbatch sample_tasks_sbatch.sh -m Beechwood_0_garden # A
+#sbatch sample_tasks_sbatch.sh -m Beechwood_0_garden # A
 sbatch sample_tasks_sbatch.sh -m Beechwood_0_int # A
-sbatch sample_tasks_sbatch.sh -m Beechwood_1_int
-sbatch sample_tasks_sbatch.sh -m Benevolence_0_int
+#sbatch sample_tasks_sbatch.sh -m Beechwood_1_int
+#sbatch sample_tasks_sbatch.sh -m Benevolence_0_int
 sbatch sample_tasks_sbatch.sh -m Benevolence_1_int # A
-sbatch sample_tasks_sbatch.sh -m Benevolence_2_int
-sbatch sample_tasks_sbatch.sh -m Ihlen_0_int # A
-sbatch sample_tasks_sbatch.sh -m Ihlen_1_int # A
-sbatch sample_tasks_sbatch.sh -m Merom_0_garden # A
-sbatch sample_tasks_sbatch.sh -m Merom_0_int
-sbatch sample_tasks_sbatch.sh -m Merom_1_int # A
-sbatch sample_tasks_sbatch.sh -m Pomaria_0_garden
+#sbatch sample_tasks_sbatch.sh -m Benevolence_2_int
+#sbatch sample_tasks_sbatch.sh -m Ihlen_0_int # A
+#sbatch sample_tasks_sbatch.sh -m Ihlen_1_int # A
+#sbatch sample_tasks_sbatch.sh -m Merom_0_garden # A
+#sbatch sample_tasks_sbatch.sh -m Merom_0_int
+#sbatch sample_tasks_sbatch.sh -m Merom_1_int # A
+#sbatch sample_tasks_sbatch.sh -m Pomaria_0_garden
 sbatch sample_tasks_sbatch.sh -m Pomaria_0_int
-sbatch sample_tasks_sbatch.sh -m Pomaria_1_int # A
-sbatch sample_tasks_sbatch.sh -m Pomaria_2_int
-sbatch sample_tasks_sbatch.sh -m Rs_garden # A
+#sbatch sample_tasks_sbatch.sh -m Pomaria_1_int # A
+#sbatch sample_tasks_sbatch.sh -m Pomaria_2_int
+#sbatch sample_tasks_sbatch.sh -m Rs_garden # A
 sbatch sample_tasks_sbatch.sh -m Rs_int # A
 sbatch sample_tasks_sbatch.sh -m Wainscott_0_garden # A
-sbatch sample_tasks_sbatch.sh -m Wainscott_0_int # A
+#sbatch sample_tasks_sbatch.sh -m Wainscott_0_int # A
 sbatch sample_tasks_sbatch.sh -m Wainscott_1_int # A
-sbatch sample_tasks_sbatch.sh -m grocery_store_asian
-sbatch sample_tasks_sbatch.sh -m grocery_store_cafe # A
+#sbatch sample_tasks_sbatch.sh -m grocery_store_asian
+#sbatch sample_tasks_sbatch.sh -m grocery_store_cafe # A
 sbatch sample_tasks_sbatch.sh -m grocery_store_convenience # A
-sbatch sample_tasks_sbatch.sh -m grocery_store_half_stocked # A
-sbatch sample_tasks_sbatch.sh -m hall_arch_wood
+#sbatch sample_tasks_sbatch.sh -m grocery_store_half_stocked # A
+#sbatch sample_tasks_sbatch.sh -m hall_arch_wood
 #sbatch sample_tasks_sbatch.sh -m hall_conference_large
 #sbatch sample_tasks_sbatch.sh -m hall_glass_ceiling
 #sbatch sample_tasks_sbatch.sh -m hall_train_station
@@ -31,12 +31,12 @@ sbatch sample_tasks_sbatch.sh -m hall_arch_wood
 #sbatch sample_tasks_sbatch.sh -m hotel_suite_small
 #sbatch sample_tasks_sbatch.sh -m house_double_floor_lower
 #sbatch sample_tasks_sbatch.sh -m house_double_floor_upper
-sbatch sample_tasks_sbatch.sh -m house_single_floor
+#sbatch sample_tasks_sbatch.sh -m house_single_floor
 #sbatch sample_tasks_sbatch.sh -m office_bike
 #sbatch sample_tasks_sbatch.sh -m office_cubicles_left
 #sbatch sample_tasks_sbatch.sh -m office_cubicles_right
 #sbatch sample_tasks_sbatch.sh -m office_large
-sbatch sample_tasks_sbatch.sh -m office_vendor_machine
+#sbatch sample_tasks_sbatch.sh -m office_vendor_machine
 #sbatch sample_tasks_sbatch.sh -m restaurant_asian
 #sbatch sample_tasks_sbatch.sh -m restaurant_brunch
 #sbatch sample_tasks_sbatch.sh -m restaurant_cafeteria
