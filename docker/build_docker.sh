5344a56741132ea87a07bf4d8f400dc6a24e0e6c
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-03-19 13:52:29 -0700

    Unify dev and prod docker images

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,11 +2,6 @@
 set -e -o pipefail
 
 docker build \
-    -t stanfordvl/omnigibson-dev:latest \
-    -f docker/dev.Dockerfile \
-    .
-
-docker build \
     -t stanfordvl/omnigibson:latest \
     -t stanfordvl/omnigibson:$(sed -ne "s/.*version= *['\"]\([^'\"]*\)['\"] *.*/\1/p" setup.py) \
     -f docker/prod.Dockerfile \
