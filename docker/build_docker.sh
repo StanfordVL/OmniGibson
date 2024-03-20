f6f907b5bf6245b97aefa4a5b0ba3f355cd03fc4
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-03-19 22:14:48 -0700

    Fix build docker file

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,11 +2,22 @@
 set -e -o pipefail
 
 docker build \
-    -t stanfordvl/omnigibson:colab-docker \
+    -t stanfordvl/omnigibson-dev:latest \
+    -f docker/dev.Dockerfile \
+    .
+
+docker build \
+    -t stanfordvl/omnigibson:latest \
+    -t stanfordvl/omnigibson:$(sed -ne "s/.*version= *['\"]\([^'\"]*\)['\"] *.*/\1/p" setup.py) \
     -f docker/prod.Dockerfile \
     .
 
 docker build \
-    -t stanfordvl/omnigibson-colab \
+    -t stanfordvl/omnigibson-vscode:latest \
+    -f docker/vscode.Dockerfile \
+    .
+
+docker build \
+    -t stanfordvl/omnigibson-colab:latest \
     -f docker/colab.Dockerfile \
     .
