e44572a4632763cc084f1fa811a8a32d57b243e2
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-04-26 15:03:00 -0700

    More TODOs for Hang

---
+++ tests/test_multiple_envs.py
@@ -0,0 +1,11 @@
+# TODO(parallel-hang): Add unit tests for parallel execution of multiple environments.
+
+# Test that you can load multiple scenes, save 3rd scene, clear, load the saved 3rd scene as the first and only scene,
+# and make sure that the position is correct
+
+# Test that you can load multiple scenes, and all the global poses are different by the same amount
+
+# Test that you can load multiple scenes and turn on a particle source in all three and nothing breaks
+
+# Load a scene with a robot and everything, then save the state, then move the scene prim far away, then load the state, and make sure nothing goes
+# to the original position.
