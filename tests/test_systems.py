bad2d41e553d0b06d74ace467cbdd91975b13cb5
Hang Yin <yinhang0226@gmail.com>
2024-05-08 16:34:34 -0700

    small fix for system test

--- tests/test_systems.py
+++ tests/test_systems.py
@@ -11,9 +11,9 @@ def test_system_clear(env):
     breakfast_table = env.scene.object_registry("name", "breakfast_table")
     for system_name, system_class in SYSTEM_EXAMPLES.items():
         for _ in range(3):
-            system = env.scene.system_registry("name", system_name)
+            system = env.scene.get_system(system_name)
             assert isinstance(system, system_class)
-            if isinstance(system_class, VisualParticleSystem):
+            if issubclass(system_class, VisualParticleSystem):
                 assert breakfast_table.states[Covered].set_value(system, True)
             else:
                 system.generate_particles(scene=env.scene, positions=[[0, 0, 1]])
