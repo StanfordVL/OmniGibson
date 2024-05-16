365581ad5203cb4db3128c4a7bc7881037431cb1
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-05-16 14:17:31 -0700

    Comment out the per-scene settling in favor of the more generic settling.

--- omnigibson/tasks/grasp_task.py
+++ omnigibson/tasks/grasp_task.py
@@ -132,26 +132,27 @@ class GraspTask(BaseTask):
             robot_pose = self._primitive_controller._get_robot_pose_from_2d_pose(sampled_pose_2d)
             robot.set_position_orientation(*robot_pose)
 
-        # Settle robot
-        for _ in range(10):
-            og.sim.step()
+        # We now do the settling after the fact, in the SB3 Vector Env class
+        # # Settle robot
+        # for _ in range(10):
+        #     og.sim.step()
 
-        # Wait for the robot to fully stabilize.
-        for _ in range(100):
-            og.sim.step()
-            if np.linalg.norm(robot.get_linear_velocity()) > 1e-2:
-                continue
-            if np.linalg.norm(robot.get_angular_velocity()) > 1e-2:
-                continue
-            break
-        else:
-            raise ValueError("Robot could not settle")
+        # # Wait for the robot to fully stabilize.
+        # for _ in range(100):
+        #     og.sim.step()
+        #     if np.linalg.norm(robot.get_linear_velocity()) > 1e-2:
+        #         continue
+        #     if np.linalg.norm(robot.get_angular_velocity()) > 1e-2:
+        #         continue
+        #     break
+        # else:
+        #     raise ValueError("Robot could not settle")
 
-        # Check if the robot has toppled
-        rotation = R.from_quat(robot.get_orientation())
-        robot_up = rotation.apply(np.array([0, 0, 1]))
-        if robot_up[2] < 0.75:
-            raise ValueError("Robot has toppled over")
+        # # Check if the robot has toppled
+        # rotation = R.from_quat(robot.get_orientation())
+        # robot_up = rotation.apply(np.array([0, 0, 1]))
+        # if robot_up[2] < 0.75:
+        #     raise ValueError("Robot has toppled over")
 
         # print("Reset robot pose to: ", robot_pose)
 
