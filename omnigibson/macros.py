7848b41f4b8e234311965541a3e7b16350c823e8
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-06-26 16:53:34 -0700

    Update Docker images to Isaac Sim 4.0.0

--- omnigibson/macros.py
+++ omnigibson/macros.py
@@ -123,7 +123,7 @@ gm.GPU_MAX_RIGID_PATCH_COUNT = 81920 * 4
 gm.ENABLE_OBJECT_STATES = True
 
 # Whether to enable transition rules or not
-gm.ENABLE_TRANSITION_RULES = True
+gm.ENABLE_TRANSITION_RULES = False
 
 # Default settings for the omni UI viewer
 gm.DEFAULT_VIEWER_WIDTH = 1280
