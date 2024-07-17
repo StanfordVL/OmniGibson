ee67c51c698e5a9c6e0086bb6a65c0c973f8f612
hang-yin <yinhang0226@gmail.com>
2024-07-17 12:22:15 -0700

    Run examples as tests

--- tests/create_tests_of_examples.py
+++ tests/create_tests_of_examples.py
@@ -10,22 +10,34 @@ from omnigibson.utils.asset_utils import download_assets
 
 download_assets()
 
+EXAMPLES_TO_SKIP = [
+    "action_primitives.rs_int_example",
+    "action_primitives.solve_simple_task",
+    "action_primitives.wip_solve_behavior_task",
+    "learning.navigation_policy_demo",
+    "teleoperation.robot_teleoperate_demo",
+]
+
 
 def main():
     examples_list = []
-    for package in pkgutil.walk_packages(examples.__path__, examples.__name__ + "."):
+    prefix = examples.__name__ + "."
+    for package in pkgutil.walk_packages(examples.__path__, prefix):
         if (
             not package.ispkg
-            and package.name[17:] != "example_selector"
-            and "web_ui" not in package.name[17:]  # The WebUI examples require additional server setup
-            and "vr_" not in package.name[17:]  # The VR examples require additional dependencies
-            and "ray_" not in package.name[17:]  # The Ray/RLLib example does not run in a subprocess
-        ):  # Consider removing the last condition if we have runnable VR tests
-            examples_list += [package.name[17:]]
+            and package.name[len(prefix) :] != "example_selector"
+            and "web_ui" not in package.name[len(prefix) :]
+            and "vr_" not in package.name[len(prefix) :]
+            and "ray_" not in package.name[len(prefix) :]
+        ):
+            examples_list.append(package.name[len(prefix) :])
 
-    temp_folder_of_test = os.path.join("/", "tmp", "tests_of_examples")
-    shutil.rmtree(temp_folder_of_test, ignore_errors=True)
-    os.makedirs(temp_folder_of_test, exist_ok=True)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    tests_of_examples_dir = os.path.join(current_dir, "tests_of_examples")
+    shutil.rmtree(tests_of_examples_dir, ignore_errors=True)
+    os.makedirs(tests_of_examples_dir, exist_ok=True)
+
+    examples_list = [example for example in examples_list if example not in EXAMPLES_TO_SKIP]
 
     for example in examples_list:
         template_file_name = os.path.join(omnigibson.__path__[0], "..", "tests", "test_of_example_template.txt")
@@ -36,7 +48,7 @@ def main():
             substitutes["name"] = name
             src = Template(f.read())
             dst = src.substitute(substitutes)
-            test_file = open(os.path.join(temp_folder_of_test, name + "_test.py"), "w")
+            test_file = open(os.path.join(tests_of_examples_dir, name + "_test.py"), "w")
             n = test_file.write(dst)
             test_file.close()
 
