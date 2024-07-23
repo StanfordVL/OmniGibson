1d59737dd4996f0e84f57d7e740b7360e54f58e2
Frank Yang <fyx5811@capri32.stanford.edu>
2024-07-23 13:53:10 -0700

    local api coding style fix

--- tests/test_robot_states_flatcache.py
+++ tests/test_robot_states_flatcache.py
@@ -65,10 +65,10 @@ def camera_pose_test(flatcache):
     assert np.allclose(sensor_world_ori, sensor_world_ori_gt, atol=1e-3)
 
     # Now, we want to move the robot and check if the sensor pose has been updated
-    old_camera_local_pose = vision_sensor.get_position_orientation("parent")
+    old_camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
 
     robot.set_position_orientation(position=[100, 100, 100])
-    new_camera_local_pose = vision_sensor.get_position_orientation("parent")
+    new_camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
     new_camera_world_pose = vision_sensor.get_position_orientation()
     robot_pose_mat = pose2mat(robot.get_position_orientation())
     expected_camera_world_pos, expected_camera_world_ori = mat2pose(robot_pose_mat @ robot_to_sensor_mat)
@@ -78,7 +78,7 @@ def camera_pose_test(flatcache):
 
     # Then, we want to move the local pose of the camera and check
     # 1) if the world pose is updated 2) if the robot stays in the same position
-    old_camera_local_pose = vision_sensor.get_position_orientation("parent")
+    old_camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
     vision_sensor.set_position_orientation(position=[10, 10, 10], orientation=[0, 0, 0, 1], frame="parent")
     new_camera_world_pose = vision_sensor.get_position_orientation()
     camera_parent_prim = lazy.omni.isaac.core.utils.prims.get_prim_parent(vision_sensor.prim)
@@ -94,9 +94,9 @@ def camera_pose_test(flatcache):
     # Finally, we want to move the world pose of the camera and check
     # 1) if the local pose is updated 2) if the robot stays in the same position
     robot.set_position_orientation(position=[150, 150, 100])
-    old_camera_local_pose = vision_sensor.get_position_orientation("parent")
+    old_camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
     vision_sensor.set_position_orientation([150, 150, 101.36912537], [-0.29444987, 0.29444981, 0.64288363, -0.64288352])
-    new_camera_local_pose = vision_sensor.get_position_orientation("parent")
+    new_camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
     assert not np.allclose(old_camera_local_pose[0], new_camera_local_pose[0], atol=1e-3)
     assert not np.allclose(old_camera_local_pose[1], new_camera_local_pose[1], atol=1e-3)
     assert np.allclose(robot.get_position_orientation()[0], [150, 150, 100], atol=1e-3)
@@ -104,7 +104,7 @@ def camera_pose_test(flatcache):
     # Another test we want to try is setting the camera's parent scale and check if the world pose is updated
     camera_parent_prim.GetAttribute("xformOp:scale").Set(lazy.pxr.Gf.Vec3d([2.0, 2.0, 2.0]))
     camera_parent_world_transform = PoseAPI.get_world_pose_with_scale(camera_parent_path)
-    camera_local_pose = vision_sensor.get_position_orientation("parent")
+    camera_local_pose = vision_sensor.get_position_orientation(frame="parent")
     expected_new_camera_world_pos, _ = mat2pose(camera_parent_world_transform @ pose2mat(camera_local_pose))
     new_camera_world_pose = vision_sensor.get_position_orientation()
     assert np.allclose(new_camera_world_pose[0], expected_new_camera_world_pos, atol=1e-3)
