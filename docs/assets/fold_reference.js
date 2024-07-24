992ee7f01112042426ece64d9361bad7b8829ba4
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-07-24 15:05:13 -0700

    Start the API reference folded

---
+++ docs/assets/fold_reference.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const reference_link = document.querySelector('a[href$="reference/SUMMARY.html"]');
+  const inp = reference_link.parentElement.parentElement.querySelector('input');
+  inp.classList.remove('md-toggle--indeterminate');
+  const nav = document.querySelector(`[aria-labelledby="${inp.id}_label"]`);
+  nav.setAttribute('aria-expanded', 'false');
+});
