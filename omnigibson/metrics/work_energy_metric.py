corrected rotational inertia calculation

omnigibson/metrics/work_energy_metric.py
@@ -19,20 +19,27 @@ class WorkEnergyMetric(BaseMetric):
         self._energy_metric = 0
         self.state_cache = None
         self.prev_state_cache = None
-        self.link_masses = {}
+
+        # stores object mass and rotational inertia
+        self.link_info = {}
         self.metric_config = metric_config
 
     def _step(self, task, env, action):
 
         # calculate the current pose of all object links
         new_state_cache = {}
+        self.link_info = {}
         for obj in env.scene.objects:
             for link_name, link in obj._links.items():
                 pos, rot = link.get_position_orientation()
                 new_state_cache[link_name] = (pos, rot)
 
                 # compute this every step to account for object addition and removal
-                self.link_masses[link_name] = link.mass
+                # compute link aabb and rototaional inertia, assuming ellipsoid shape and uniform density
+                aabb = link.aabb[1] - link.aabb[0]
+                mass = link.mass
+                inertia = 1 / 5 * mass * np.array([aabb[0] ** 2 + aabb[1] ** 2, aabb[1] ** 2 + aabb[2] ** 2, aabb[2] ** 2 + aabb[0] ** 2])
+                self.link_info[link_name] = (mass, inertia)
 
         # if the state cache is empty, set it to the current state and return 0
         if not self.state_cache:
@@ -53,11 +60,13 @@ class WorkEnergyMetric(BaseMetric):
                 continue
 
             init_posrot = self.state_cache[linkname]
+            mass, inertia = self.link_info[linkname]
             position, orientation = T.relative_pose_transform(posrot[0], posrot[1], init_posrot[0], init_posrot[1])
-            work_metric += np.linalg.norm(position) * self.link_masses[linkname] * self.metric_config["translation"]
-
-            # calculate the energy spent in rotation. TODO: this is a very rough approximation, consider using a more accurate method
-            work_metric += np.linalg.norm(orientation) * self.link_masses[linkname] * self.metric_config["rotation"]
+            orientation = T.quat2axisangle(orientation)
+            work_metric += np.linalg.norm(position) * mass * self.metric_config["translation"]
+            
+            # calculate the energy spent in rotation
+            work_metric += 0.5 * np.dot(inertia, orientation ** 2) * self.metric_config["rotation"]
 
             # check if the link is in the prev_state_cache, if not, skip it to account for object addition and removal
             if linkname not in self.prev_state_cache:
@@ -66,10 +75,11 @@ class WorkEnergyMetric(BaseMetric):
             if self.prev_state_cache is not None:
                 prev_posrot = self.prev_state_cache[linkname]
                 position, orientation = T.relative_pose_transform(posrot[0], posrot[1], prev_posrot[0], prev_posrot[1])
-                energy_metric += np.linalg.norm(position) * self.link_masses[linkname] * self.metric_config["translation"]
+                orientation = T.quat2axisangle(orientation)
+                energy_metric += np.linalg.norm(position) * mass * self.metric_config["translation"]
 
-                # calculate the energy spent in rotation. TODO: this is a very rough approximation, consider using a more accurate method
-                energy_metric += np.linalg.norm(orientation) * self.link_masses[linkname] * self.metric_config["rotation"]
+                # calculate the energy spent in rotation
+                energy_metric += 0.5 * np.dot(inertia, orientation ** 2) * self.metric_config["rotation"]
 
         # update the prev_state cache for energy measurement
         self.prev_state_cache = new_state_cache
@@ -86,3 +96,4 @@ class WorkEnergyMetric(BaseMetric):
         self._energy_metric = 0
         self.state_cache = None
         self.prev_state_cache = None
+        self.link_info = {}

tests/test_metric_functions.py
@@ -102,7 +102,7 @@ def test_behavior_task_energy_metric():
         robot_mass += link.mass
 
     # assert that the total energy spent is twice as much as the energy spent applying the shift
-    assert np.allclose((2 * energy - new_energy) / robot_mass, 0, atol=1e-3)
+    assert np.allclose((2 * energy - new_energy) / robot_mass, 0, atol=1e-2)
     env.reset()
 
 