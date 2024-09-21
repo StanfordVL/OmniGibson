4a202303f77d7cde56a97a51b88ccc6053290b27
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2024-09-20 18:37:54 -0700

    add newline

--- omnigibson/robots/holonomic_base_robot.py
+++ omnigibson/robots/holonomic_base_robot.py
@@ -146,6 +146,7 @@ class HolonomicBaseRobot(LocomotionRobot):
 
         # Force the recomputation of this cached property
         del self.control_limits
+
         # Reload the controllers to update command_output_limits, which read the updated control limits
         self.reload_controllers(self._controller_config)
 
