3627bd6ea29239c64473c05fc8c99acff1259350
Cem Gökmen <cem@cemgokmen.com>
2024-12-05 11:51:08 -0800

    Add script to generate fillable volumes

---
+++ asset_pipeline/b1k_pipeline/usd_conversion/get_bboxes_for_fillable_volumes.py
@@ -0,0 +1,126 @@
+import hashlib
+import os
+import random
+import sys
+import glob
+import pathlib
+import numpy as np
+import omnigibson as og
+from omnigibson.objects import DatasetObject
+from omnigibson.macros import gm
+from omnigibson.prims import XFormPrim
+from omnigibson.utils.ui_utils import KeyboardEventHandler
+from omnigibson.utils.usd_utils import mesh_prim_to_trimesh_mesh
+import omnigibson.utils.transform_utils as T
+import omnigibson.lazy as lazy
+import trimesh
+import json
+import tqdm
+from scipy.spatial.transform import Rotation as R
+from scipy.spatial import ConvexHull
+import torch as th
+import networkx as nx
+from collections import defaultdict
+from fs.zipfs import ZipFS
+import fs.path
+
+gm.HEADLESS = False
+gm.USE_ENCRYPTED_ASSETS = True
+gm.ENABLE_TRANSITION_RULES = False
+gm.ENABLE_FLATCACHE = False
+gm.DATASET_PATH = r"D:\fillable-10-21"
+
+ASSIGNMENT_FILE = os.path.join(gm.DATASET_PATH, "fillable_assignments_2.json")
+ORIENTATION_EDITS_FILE = "orientation_edits.zip"
+
+MAX_BBOX = 0.3
+
+DRAWING_MESHES = []
+
+def get_orientation_edits():
+    orientation_edits = {}
+    zip_path = ORIENTATION_EDITS_FILE
+    with ZipFS(zip_path) as orientation_zip_fs:
+        for item in orientation_zip_fs.glob("recorded_orientation/*/*.json"):
+            model = fs.path.splitext(fs.path.basename(item.path))[0]
+            orientation = json.loads(orientation_zip_fs.readtext(item.path))[0]
+            if np.allclose(orientation, [0, 0, 0, 1], atol=1e-3):
+                continue
+            orientation_edits[model] = orientation
+
+    return orientation_edits
+
+ORIENTATION_EDITS = get_orientation_edits()
+
+def view_object(cat, mdl):
+    if og.sim:
+        og.clear()
+    else:
+        og.launch()
+
+    clear_meshes()
+
+    if og.sim.is_playing():
+        og.sim.stop()
+
+    # Find out if there's an orientation edit for this object
+    orn = [0, 0, 0, 1] if mdl not in ORIENTATION_EDITS else R.from_quat(ORIENTATION_EDITS[mdl]).inv().as_quat().tolist()
+
+    cfg = {
+        "scene": {
+            "type": "Scene",
+            "use_floor_plane": False,
+        },
+        "objects": [
+            {
+                "type": "DatasetObject",
+                "name": "fillable",
+                "category": cat,
+                "model": mdl,
+                "orientation": orn,
+                "kinematic_only": False,
+                "fixed_base": True,
+            },
+        ]
+    }
+
+    env = og.Environment(configs=cfg)
+    og.sim.step()
+
+    fillable = env.scene.object_registry("name", "fillable")
+    fillable.set_position([0, 0, fillable.aabb_extent[2]])
+    og.sim.step()
+
+    bbox_extents = fillable.aabb_extent.tolist()
+    bbox_center = fillable.aabb_center.tolist()
+    base_pos = fillable.get_position_orientation()[0].numpy().tolist()
+    base_orn = fillable.get_position_orientation()[1].numpy().tolist()
+
+    data = {"bbox_center": bbox_center, "bbox_extents": bbox_extents, "base_pos": base_pos, "base_orn": base_orn}
+    target_path = pathlib.Path(DatasetObject.get_usd_path(cat, mdl)).parent.parent / "bbox.json"
+    with open(target_path, "w") as f:
+        json.dump(data, f)
+
+def main():
+    print("Fillable annotator version 11.6.0")
+
+    fillables = [
+        tuple(pathlib.Path(x).parts[-4:-2])
+        for x in glob.glob(f"{gm.DATASET_PATH}/objects/*/*/usd/*.usd")
+        if not (pathlib.Path(x).parent.parent / "bbox.json").exists()
+    ]
+    random.shuffle(fillables)
+
+    for cat, mdl in tqdm.tqdm(fillables[:32]):
+        if not os.path.exists(DatasetObject.get_usd_path(cat, mdl).replace(".usd", ".encrypted.usd")):
+            print(f"Skipping {cat}/{mdl} because it does not exist")
+            continue
+        view_object(cat, mdl)
+
+    if len(fillables) == 0:
+        with open(os.path.join(gm.DATASET_PATH, "done.txt"), "w") as f:
+            pass
+
+
+if __name__ == "__main__":
+    main()
