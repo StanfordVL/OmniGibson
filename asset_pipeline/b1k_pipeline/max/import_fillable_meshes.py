48a3fb6815673de2575e00d3dcf33687adaca737
Cem Gökmen <cem@cemgokmen.com>
2024-12-04 23:58:17 -0800

    Fix and finalize fillable mesh importer

--- asset_pipeline/b1k_pipeline/max/import_fillable_meshes.py
+++ asset_pipeline/b1k_pipeline/max/import_fillable_meshes.py
@@ -3,7 +3,7 @@ import pathlib
 import tempfile
 import numpy as np
 import json
-import pxr
+import pxr.Usd
 from fs.osfs import OSFS
 from cryptography.fernet import Fernet
 
@@ -14,12 +14,19 @@ import sys
 
 sys.path.append(r"D:\ig_pipeline")
 
-from b1k_pipeline.utils import PipelineFS, get_targets, parse_name, load_mesh, PIPELINE_ROOT
+from b1k_pipeline.utils import parse_name, load_mesh
 from b1k_pipeline.max.replace_bad_object import node_bounding_box_incl_children, rotation_only_transform
 
 FILLABLE_DIR = pathlib.Path(r"D:\fillable-10-21")
+FILLABLE_ASSIGNMENTS = {
+    k: v
+    for k, v in
+    json.loads((FILLABLE_DIR / "fillable_assignments_2.json").read_text()).items()
+    if v in {"dip", "ray", "combined", "generated"}
+}
+BOUNDING_BOX_DATA_PATH = FILLABLE_DIR / "fillable_bboxes.json"
+BOUNDING_BOX_DATA = json.loads(BOUNDING_BOX_DATA_PATH.read_text())
 KEY_PATH = pathlib.Path(r"C:\Users\cgokmen\research\OmniGibson\omnigibson\data\omnigibson.key")
-JUST_THIS_FILE = True
 REMOVE_EXISTING = True
 
 
@@ -38,18 +45,8 @@ def decrypt_file(encrypted_filename, decrypted_filename):
 
 
 def get_bounding_box_from_usd(obj_dir):
-    with tempfile.TemporaryDirectory() as tempdir:
-        tempdir = pathlib.Path(tempdir)
-        encrypted_filename = obj_dir / "usd" / f"{obj_dir.name}.encrypted.usd"
-        decrypted_filename = tempdir / "obj.usd"
-        decrypt_file(encrypted_filename, decrypted_filename)
-        stage = pxr.Usd.Stage.Open(str(decrypted_filename))
-        prim = stage.GetDefaultPrim()
-
-        base_link_size = np.array(prim.GetAttribute("ig:nativeBB").Get()) * 1000
-        base_link_offset = np.array(prim.GetAttribute("ig:offsetBaseLink").Get()) * 1000
-
-        return base_link_offset, base_link_size
+    offset, size = BOUNDING_BOX_DATA[obj_dir.parts[-1]]
+    return np.array(offset), np.array(size)
 
 
 def import_fillable_volumes(model_id, object_links):
@@ -84,18 +81,21 @@ def import_fillable_volumes(model_id, object_links):
     # Get information about the base link of the object
     base_link = object_links["base_link"]
     base_rot = base_link.rotation
-    bbox_min, bbox_max = node_bounding_box_incl_children(base_link, rotation_only_transform(base_link.transform), only_canonical=True)
-    bbox_ctr = (bbox_min + bbox_max) / 2
+    base_link_rotation_transform = rotation_only_transform(base_link.transform)
+
+    # Compute the full object bbox by looking at all the links
+    bbox_points = []
+    for link_obj in object_links.values():
+        bbox_points.extend(node_bounding_box_incl_children(link_obj, base_link_rotation_transform, only_canonical=True))
+    bbox_min = np.min(bbox_points, axis=0)
+    bbox_max = np.max(bbox_points, axis=0)
+    bbox_ctr_rotated = (bbox_min + bbox_max) / 2
+    base_link_rotated = (bbox_ctr_rotated - usd_bbox_offset)
+    base_link_world = (base_link_rotation_transform @ np.concatenate([base_link_rotated, [1]]))[:3]
     bbox_size = bbox_max - bbox_min
 
     # Compare the bounding box sizes
-    assert np.allclose(bbox_size, usd_bbox_size), f"Bounding box sizes do not match for {model_id}. USD has {usd_bbox_size}, max has {bbox_size}"
-
-    # Compute the theoretical base link params using the current bbox center and the offset from the USD
-    bbox_offset = rt.Point3(*usd_bbox_offset)
-    rotated_bbox_offset = base_rot * bbox_offset
-    theoretical_base_link_pos = bbox_ctr - rotated_bbox_offset
-    theoretical_base_link_rot = base_rot
+    assert np.allclose(bbox_size, usd_bbox_size, atol=10), f"Bounding box sizes do not match for {model_id}. USD has {usd_bbox_size}, max has {bbox_size}"
 
     for link_name, kind_files in by_link_and_kind.items():
         link_obj = object_links[link_name]
@@ -113,8 +113,8 @@ def import_fillable_volumes(model_id, object_links):
             # Get a flattened list of vertices and faces
             all_vertices = []
             all_faces = []
-            for split in fillable_mesh:
-                vertices = [rt.Point3(*v.tolist()) for v in split.vertices]
+            for split in fillable_meshes:
+                vertices = [rt.Point3(*(v * 1000).tolist()) for v in split.vertices]
                 # Offsetting here by the past vertex count
                 faces = [[v + len(all_vertices) + 1 for v in f.tolist()] for f in split.faces]
                 all_vertices.extend(vertices)
@@ -137,8 +137,8 @@ def import_fillable_volumes(model_id, object_links):
             # So far, everything is in the local coordinate system, e.g. where they would go if the
             # object base link was at the origin and had no rotation. We need to move it to where we
             # think the base link is (this is unknown in 3ds Max but we read it from the USD).
-            fillable_obj.position = theoretical_base_link_pos
-            fillable_obj.rotation = theoretical_base_link_rot
+            fillable_obj.rotation = base_rot
+            fillable_obj.position = rt.Point3(*base_link_world.tolist())
 
             # Optionally set its wire color
             fillable_obj.wirecolor = rt.yellow
@@ -159,13 +159,7 @@ def import_fillable_volumes(model_id, object_links):
             fillable_obj.isHidden = True
 
 
-def process_target(pipeline_fs, target, fillable_assignments):
-    print("Processing", target)
-    
-    if not JUST_THIS_FILE:
-        filename = str(pipeline_fs.target(target).getsyspath("processed.max"))
-        assert rt.loadMaxFile(filename, useFileUnits=False, quiet=True)
-
+def process_current_file():
     # Iterate through the objects in the file and build the link lists
     object_links = defaultdict(dict)
     for obj in rt.objects:
@@ -189,34 +183,9 @@ def process_target(pipeline_fs, target, fillable_assignments):
         object_links[match.group("model_id")][link_name] = obj
 
     # For each object, try to import the fillable volumes
-    availables = set(fillable_assignments.keys()) & set(object_links.keys())
+    availables = set(FILLABLE_ASSIGNMENTS.keys()) & set(object_links.keys())
     for model_id in sorted(availables):
         import_fillable_volumes(model_id, object_links[model_id])
 
-    if not JUST_THIS_FILE:
-        rt.saveMaxFile(filename)
-
-
-def main():
-    # Load the fillable selection
-    with open(FILLABLE_DIR / "fillable_assignments_2.json", "r") as f:
-        fillable_assignments = json.load(f)
-
-    # Filter it down to just the fillable objects
-    fillable_assignments = {k: v for k, v in fillable_assignments.items() if v in {"dip", "ray", "combined", "generated"}}
-
-    with PipelineFS() as pipeline_fs:
-        current_target_dir = pathlib.Path(rt.maxFilePath)
-        current_target_rel = current_target_dir.relative_to(PIPELINE_ROOT)
-        cad, target_type, target_name = current_target_rel.parts
-        assert cad == "cad", f"Current file is not in the cad directory: {current_target_rel}"
-        assert target_type in ("scenes", "objects"), f"Current file is not in the scenes or objects directory: {current_target_rel}"
-        current_target = target_type + "/" + target_name
-
-        targets = get_targets("combined_unfiltered") if not JUST_THIS_FILE else [current_target]
-
-        for target in targets:
-            process_target(pipeline_fs, target, fillable_assignments)
-
 if __name__ == "__main__":
-    main()
+    process_current_file()
