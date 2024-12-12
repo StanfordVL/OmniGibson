863209b16b815592764c83ce49687b858e83c38d
cremebrule <84cremebrule@gmail.com>
2024-12-11 16:00:04 -0800

    fix grasping state inference for gripper controller

--- omnigibson/controllers/multi_finger_gripper_controller.py
+++ omnigibson/controllers/multi_finger_gripper_controller.py
@@ -17,7 +17,7 @@ m = create_module_macros(module_path=__file__)
 
 # is_grasping heuristics parameters
 m.POS_TOLERANCE = 0.002  # arbitrary heuristic
-m.VEL_TOLERANCE = 0.01  # arbitrary heuristic
+m.VEL_TOLERANCE = 0.02  # arbitrary heuristic
 
 
 class MultiFingerGripperController(GripperController):
@@ -251,11 +251,8 @@ class MultiFingerGripperController(GripperController):
                 dist_from_lower_limit = finger_pos - min_pos
                 dist_from_upper_limit = max_pos - finger_pos
 
-                # If the joint positions are not near the joint limits with some tolerance (m.POS_TOLERANCE)
-                valid_grasp_pos = (
-                    th.mean(dist_from_lower_limit) > m.POS_TOLERANCE
-                    and th.mean(dist_from_upper_limit) > m.POS_TOLERANCE
-                )
+                # If either of the joint positions are not near the joint limits with some tolerance (m.POS_TOLERANCE)
+                valid_grasp_pos = th.mean(dist_from_lower_limit) > m.POS_TOLERANCE or th.mean(dist_from_upper_limit) > m.POS_TOLERANCE
 
                 # And the joint velocities are close to zero with some tolerance (m.VEL_TOLERANCE)
                 valid_grasp_vel = th.all(th.abs(finger_vel) < m.VEL_TOLERANCE)
