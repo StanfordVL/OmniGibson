5121aa587a1cccf2919858a7ca3fbb78c9b4a7e7
cremebrule <84cremebrule@gmail.com>
2024-12-11 19:08:26 -0800

    ensure actions and control dict is handled properly externally

--- omnigibson/controllers/controller_base.py
+++ omnigibson/controllers/controller_base.py
@@ -367,7 +367,7 @@ class BaseController(Serializable, Registerable, Recreatable):
         if self._goal is None:
             self._goal = self.compute_no_op_goal(control_dict=control_dict)
         command = self._compute_no_op_action(control_dict=control_dict)
-        return self._reverse_preprocess_command(command)
+        return cb.to_torch(self._reverse_preprocess_command(command))
 
     def _compute_no_op_action(self, control_dict):
         """
