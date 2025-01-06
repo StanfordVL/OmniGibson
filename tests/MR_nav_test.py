253bb0b6b5ad24fb58ad106037b025592e35038e
Joonkyung Kim <0901joonkyung@gmail.com>
2025-01-06 16:40:04 -0500

    [save]

--- tests/MR_nav_test.py
+++ tests/MR_nav_test.py
@@ -15,7 +15,8 @@ def main(random_selection=False, headless=False, short_exec=False):
     og.log.info(f"Demo {__file__}\n    " + "*" * 80 + "\n    Description:\n" + (main.__doc__ or "") + "*" * 80)
 
     # Load the config
-    config_filename = os.path.join(og.example_config_path, "turtlebot_multi_nav.yaml")
+    # config_filename = os.path.join(og.example_config_path, "turtlebot_multi_nav.yaml")
+    config_filename = os.path.join(og.example_config_path, "turtlebot_nav.yaml")
     config = yaml.load(open(config_filename, "r"), Loader=yaml.FullLoader)
 
     # check if we want to quick load or full load the scene
@@ -41,6 +42,9 @@ def main(random_selection=False, headless=False, short_exec=False):
     robots = env.robots
     og.log.info(f"Loaded robots: {[robot.name for robot in robots]}")
 
+    # Save robot name
+    robot_name = robots[0].name
+
     # Run a simple loop and reset periodically
     max_iterations = 10 if not short_exec else 1
     for j in range(max_iterations):
@@ -50,6 +54,24 @@ def main(random_selection=False, headless=False, short_exec=False):
             actions = {robot.name: robot.action_space.sample() for robot in robots}
 
             states, rewards, terminated, truncated, infos = env.step(actions)
+            # return observations, rewards, dones, infos
+            # print(f"states: {states}")
+            robot_state = states[robot_name]
+            print(f"states[robot_name]: {robot_state}")
+
+            # Get the format of the state (dict[Any, Any])
+            # print(f"states: {states}")
+            print(f"states.keys: {robot_state.keys()}")
+            print(f"states.values: {robot_state.values()}")
+            print(f"states.items: {states.items()}")
+
+            camera_key = f"{robot_name}:eyes:Camera:0"
+
+            camera_output = robot_state[camera_key]
+            print(f"camera output: {camera_output}")
+            print(f"camera: {camera_output['rgb'].shape}")
+
+            # print(f"state_shape: {states[robots[0].name].shape}")
 
             if terminated or truncated:
                 og.log.info("Episode finished after {} timesteps".format(i + 1))
