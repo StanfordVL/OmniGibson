8c82c738bdd1e08cd0b4f977e7de55400eb1c423
Joonkyung Kim <0901joonkyung@gmail.com>
2025-02-07 15:20:31 -0500

    [fix] organize code

--- test_NoMaD/train/vint_train/models/nomad/nomad_vint.py
+++ test_NoMaD/train/vint_train/models/nomad/nomad_vint.py
@@ -27,7 +27,9 @@ class NoMaD_ViNT(nn.Module):
 
         # Initialize the observation encoder
         if obs_encoder.split("-")[0] == "efficientnet":
-            self.obs_encoder = EfficientNet.from_name(obs_encoder, in_channels=3)  # context
+            self.obs_encoder = EfficientNet.from_name(
+                obs_encoder, in_channels=3
+            )  # context
             self.obs_encoder = replace_bn_with_gn(self.obs_encoder)
             self.num_obs_features = self.obs_encoder._fc.in_features
             self.obs_encoder_type = "efficientnet"
@@ -35,23 +37,31 @@ class NoMaD_ViNT(nn.Module):
             raise NotImplementedError
 
         # Initialize the goal encoder
-        self.goal_encoder = EfficientNet.from_name("efficientnet-b0", in_channels=6)  # obs+goal
+        self.goal_encoder = EfficientNet.from_name(
+            "efficientnet-b0", in_channels=6
+        )  # obs+goal
         self.goal_encoder = replace_bn_with_gn(self.goal_encoder)
         self.num_goal_features = self.goal_encoder._fc.in_features
 
         # Initialize compression layers if necessary
         if self.num_obs_features != self.obs_encoding_size:
-            self.compress_obs_enc = nn.Linear(self.num_obs_features, self.obs_encoding_size)
+            self.compress_obs_enc = nn.Linear(
+                self.num_obs_features, self.obs_encoding_size
+            )
         else:
             self.compress_obs_enc = nn.Identity()
 
         if self.num_goal_features != self.goal_encoding_size:
-            self.compress_goal_enc = nn.Linear(self.num_goal_features, self.goal_encoding_size)
+            self.compress_goal_enc = nn.Linear(
+                self.num_goal_features, self.goal_encoding_size
+            )
         else:
             self.compress_goal_enc = nn.Identity()
 
         # Initialize positional encoding and self-attention layers
-        self.positional_encoding = PositionalEncoding(self.obs_encoding_size, max_seq_len=self.context_size + 2)
+        self.positional_encoding = PositionalEncoding(
+            self.obs_encoding_size, max_seq_len=self.context_size + 2
+        )
         self.sa_layer = nn.TransformerEncoderLayer(
             d_model=self.obs_encoding_size,
             nhead=mha_num_attention_heads,
@@ -60,7 +70,9 @@ class NoMaD_ViNT(nn.Module):
             batch_first=True,
             norm_first=True,
         )
-        self.sa_encoder = nn.TransformerEncoder(self.sa_layer, num_layers=mha_num_attention_layers)
+        self.sa_encoder = nn.TransformerEncoder(
+            self.sa_layer, num_layers=mha_num_attention_layers
+        )
 
         # Definition of the goal mask (convention: 0 = no mask, 1 = mask)
         self.goal_mask = torch.zeros((1, self.context_size + 2), dtype=torch.bool)
@@ -70,19 +82,25 @@ class NoMaD_ViNT(nn.Module):
         self.avg_pool_mask = torch.cat(
             [
                 1 - self.no_mask.float(),
-                (1 - self.goal_mask.float()) * ((self.context_size + 2) / (self.context_size + 1)),
+                (1 - self.goal_mask.float())
+                * ((self.context_size + 2) / (self.context_size + 1)),
             ],
             dim=0,
         )
 
     def forward(
-        self, obs_img: torch.tensor, goal_img: torch.tensor, input_goal_mask: torch.tensor = None
+        self,
+        obs_img: torch.tensor,
+        goal_img: torch.tensor,
+        input_goal_mask: torch.tensor = None,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
 
         device = obs_img.device
 
         # Initialize the goal encoding
-        goal_encoding = torch.zeros((obs_img.size()[0], 1, self.goal_encoding_size)).to(device)
+        goal_encoding = torch.zeros((obs_img.size()[0], 1, self.goal_encoding_size)).to(
+            device
+        )
 
         # Get the input goal mask
         if input_goal_mask is not None:
@@ -92,7 +110,9 @@ class NoMaD_ViNT(nn.Module):
         obsgoal_img = torch.cat(
             [obs_img[:, 3 * self.context_size :, :, :], goal_img], dim=1
         )  # concatenate the obs image/context and goal image --> non image goal?
-        obsgoal_encoding = self.goal_encoder.extract_features(obsgoal_img)  # get encoding of this img
+        obsgoal_encoding = self.goal_encoder.extract_features(
+            obsgoal_img
+        )  # get encoding of this img
         obsgoal_encoding = self.goal_encoder._avg_pooling(obsgoal_encoding)  # avg pooling
 
         if self.goal_encoder._global_params.include_top:
@@ -116,14 +136,18 @@ class NoMaD_ViNT(nn.Module):
             obs_encoding = self.obs_encoder._dropout(obs_encoding)
         obs_encoding = self.compress_obs_enc(obs_encoding)
         obs_encoding = obs_encoding.unsqueeze(1)
-        obs_encoding = obs_encoding.reshape((self.context_size + 1, -1, self.obs_encoding_size))
+        obs_encoding = obs_encoding.reshape(
+            (self.context_size + 1, -1, self.obs_encoding_size)
+        )
         obs_encoding = torch.transpose(obs_encoding, 0, 1)
         obs_encoding = torch.cat((obs_encoding, goal_encoding), dim=1)
 
         # If a goal mask is provided, mask some of the goal tokens
         if goal_mask is not None:
             no_goal_mask = goal_mask.long()
-            src_key_padding_mask = torch.index_select(self.all_masks.to(device), 0, no_goal_mask)
+            src_key_padding_mask = torch.index_select(
+                self.all_masks.to(device), 0, no_goal_mask
+            )
         else:
             src_key_padding_mask = None
 
@@ -131,9 +155,13 @@ class NoMaD_ViNT(nn.Module):
         if self.positional_encoding:
             obs_encoding = self.positional_encoding(obs_encoding)
 
-        obs_encoding_tokens = self.sa_encoder(obs_encoding, src_key_padding_mask=src_key_padding_mask)
+        obs_encoding_tokens = self.sa_encoder(
+            obs_encoding, src_key_padding_mask=src_key_padding_mask
+        )
         if src_key_padding_mask is not None:
-            avg_mask = torch.index_select(self.avg_pool_mask.to(device), 0, no_goal_mask).unsqueeze(-1)
+            avg_mask = torch.index_select(
+                self.avg_pool_mask.to(device), 0, no_goal_mask
+            ).unsqueeze(-1)
             obs_encoding_tokens = obs_encoding_tokens * avg_mask
         obs_encoding_tokens = torch.mean(obs_encoding_tokens, dim=1)
 
@@ -148,13 +176,17 @@ def replace_bn_with_gn(root_module: nn.Module, features_per_group: int = 16) -> 
     replace_submodules(
         root_module=root_module,
         predicate=lambda x: isinstance(x, nn.BatchNorm2d),
-        func=lambda x: nn.GroupNorm(num_groups=x.num_features // features_per_group, num_channels=x.num_features),
+        func=lambda x: nn.GroupNorm(
+            num_groups=x.num_features // features_per_group, num_channels=x.num_features
+        ),
     )
     return root_module
 
 
 def replace_submodules(
-    root_module: nn.Module, predicate: Callable[[nn.Module], bool], func: Callable[[nn.Module], nn.Module]
+    root_module: nn.Module,
+    predicate: Callable[[nn.Module], bool],
+    func: Callable[[nn.Module], nn.Module],
 ) -> nn.Module:
     """
     Replace all submodules selected by the predicate with
@@ -166,7 +198,11 @@ def replace_submodules(
     if predicate(root_module):
         return func(root_module)
 
-    bn_list = [k.split(".") for k, m in root_module.named_modules(remove_duplicate=True) if predicate(m)]
+    bn_list = [
+        k.split(".")
+        for k, m in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)
+    ]
     for *parent, k in bn_list:
         parent_module = root_module
         if len(parent) > 0:
@@ -181,6 +217,10 @@ def replace_submodules(
         else:
             setattr(parent_module, k, tgt_module)
     # verify that all modules are replaced
-    bn_list = [k.split(".") for k, m in root_module.named_modules(remove_duplicate=True) if predicate(m)]
+    bn_list = [
+        k.split(".")
+        for k, m in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)
+    ]
     assert len(bn_list) == 0
     return root_module
