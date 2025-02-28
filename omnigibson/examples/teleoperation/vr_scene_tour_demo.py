a6b5e72c836eb0fa727cc79c6454fef698417e15
hang-yin <yinhang0226@gmail.com>
2025-02-28 14:47:12 -0800

    Update VR demos

---
+++ omnigibson/examples/teleoperation/vr_scene_tour_demo.py
@@ -0,0 +1,63 @@
+"""
+Example script for interacting with OmniGibson scenes with VR.
+"""
+
+import torch as th
+import torch._dynamo
+
+import omnigibson as og
+from omnigibson.macros import gm
+from omnigibson.utils.asset_utils import get_available_og_scenes
+from omnigibson.utils.teleop_utils import OVXRSystem
+from omnigibson.utils.ui_utils import choose_from_options
+
+torch._dynamo.config.suppress_errors = True
+
+gm.ENABLE_FLATCACHE = True
+gm.ENABLE_OBJECT_STATES = False
+gm.ENABLE_TRANSITION_RULES = False
+
+
+def main():
+    """
+    Users can navigate around and interact with a selected scene using VR.
+    """
+
+    # Choose the scene model to load
+    scenes = get_available_og_scenes()
+    scene_model = choose_from_options(options=scenes, name="scene model")
+
+    # Create the config for generating the environment we want
+    scene_cfg = {"type": "InteractiveTraversableScene", "scene_model": scene_model}
+    cfg = dict(scene=scene_cfg)
+
+    # Create the environment
+    env = og.Environment(configs=cfg)
+    env.reset()
+    # start vrsys
+    vrsys = OVXRSystem(
+        robot=None,
+        show_control_marker=False,
+        system="SteamVR",
+        eef_tracking_mode="disabled",
+        align_anchor_to="touchpad",
+    )
+    vrsys.start()
+    # set headset position to be 1m above ground and facing +x
+    vrsys.vr_profile.set_physical_world_to_world_anchor_transform_to_match_xr_device(
+        vrsys.og2xr(th.tensor([0.0, 0.0, 1.0]), th.tensor([-0.5, 0.5, 0.5, -0.5])).numpy(), vrsys.hmd
+    )
+
+    # main simulation loop
+    while True:
+        # step the VR system to get the latest data from VR runtime
+        vrsys.update(optimized_for_tour=True)
+        og.sim.render()
+
+    # Shut down the environment cleanly at the end
+    vrsys.stop()
+    og.clear()
+
+
+if __name__ == "__main__":
+    main()
