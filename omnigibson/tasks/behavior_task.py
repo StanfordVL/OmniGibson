48fb2e771ae9e74ebd3cc764fc8a5c480c0e82b7
hang-yin <yinhang0226@gmail.com>
2025-03-28 12:03:41 -0700

    Update comment to include future todo

--- omnigibson/tasks/behavior_task.py
+++ omnigibson/tasks/behavior_task.py
@@ -383,6 +383,9 @@ class BehaviorTask(BaseTask):
                 )
                 name = inst_to_name[obj_inst]
                 is_system = name in env.scene.available_systems.keys()
+                # TODO: If we load a robot with a different set of configs, we will not be able to match with the
+                # original object_scope. This is a temporary fix to handle this case. A proper fix involves
+                # storing the robot (potentially only base pose) in the task metadata instead of as a regular object
                 if "agent.n." in obj_inst:
                     idx = int(obj_inst.split("_")[-1].lstrip("0")) - 1
                     entity = env.robots[idx]
