37dd631075cb7859653fc44eeeeb3446e5b760d1
hang-yin <yinhang0226@gmail.com>
2025-04-22 13:32:53 -0700

    Update R1 pro camera pose

--- joylo/gello/robots/sim_robot/og_teleop_cfg.py
+++ joylo/gello/robots/sim_robot/og_teleop_cfg.py
@@ -29,7 +29,7 @@ R1_WRIST_CAMERA_LOCAL_POS = th.tensor([0.1, 0.0, -0.1], dtype=th.float32)  # Loc
 R1_WRIST_CAMERA_LOCAL_ORI = th.tensor([0.6830127018922194, 0.6830127018922193, 0.18301270189221927, 0.18301270189221946], dtype=th.float32)  # Local orientation of the wrist camera relative to eef
 
 # R1 Pro robot-specific configurations
-R1PRO_HEAD_CAMERA_LOCAL_POS = th.tensor([0.06, 0.0, 0.045], dtype=th.float32)  # Local position of the head camera relative to head link
+R1PRO_HEAD_CAMERA_LOCAL_POS = th.tensor([0.06, 0.0, 0.01], dtype=th.float32)  # Local position of the head camera relative to head link
 
 # Default parameters
 DEFAULT_TRUNK_TRANSLATE = 0.5
