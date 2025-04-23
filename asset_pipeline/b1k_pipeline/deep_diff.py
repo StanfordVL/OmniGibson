906edd4d3cd9eea1ab034fd62637802e98744b5d
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2025-04-23 13:14:25 -0700

    More info for the diffing script

--- asset_pipeline/b1k_pipeline/deep_diff.py
+++ asset_pipeline/b1k_pipeline/deep_diff.py
@@ -2,6 +2,18 @@ import json
 import pathlib
 from deepdiff import DeepDiff
 
+from b1k_pipelinee.utils import parse_name
+
+def model_ids_from_objects(objs):
+    model_ids = set()
+    for obj in objs:
+        pn = parse_name(obj)
+        if not pn:
+            continue
+        model_ids.add(pn.group("model_id"))
+
+    return model_ids
+
 def main():
     deep_glob = "cad/*/*/artifacts/file_manifest.json"  # maybe use _deep ?
     base_files = pathlib.Path("base").glob(deep_glob)
@@ -10,6 +22,8 @@ def main():
     pr_manifest_by_target = {"/".join(f.parts[-4:-2]): f for f in pr_files}
     all_targets = set(base_manifest_by_target.keys()) | set(pr_manifest_by_target.keys())
 
+    full_diffs = {}
+    object_diffs = {}
     for target in sorted(all_targets):
         print(f"\n\n-------------------------\n{target}")
         
@@ -28,12 +42,36 @@ def main():
             del val["mtl_hash"]
         
         diff = DeepDiff(base_manifest_dict, pr_manifest_dict)
-        print("  Affected objects:")
-        for root in sorted(diff.affected_root_keys):
+        object_diffs[target] = sorted(diff.affected_root_keys)
+        full_diffs[target] = diff.pretty()
+
+    print("-------------------------------------------------")
+    print("OBJECT DIFFS")
+    print("Unique edited obj names:", sum(len(vals) for vals in object_diffs.values()))
+    print("Unique edited model IDs:", len({mid for objs in object_diffs.values() for mid in model_ids_from_objects(objs)}))
+    print("-------------------------------------------------")
+    for target in sorted(all_targets):
+        target_objs = sorted(object_diffs[target])
+        target_mids = sorted(model_ids_from_objects(target_mids))
+
+        print(f"\n\n-------------------------\n{target}")
+        print(f"Objects ({len(target_objs)})")
+        for root in target_objs:
             print("    " + str(root))
 
+        print(f"Models ({len(target_mids)})")
+        for mid in target_mids:
+            print("    " + str(mid))
+
         print()
-        print(diff.pretty())
+
+    print("-------------------------------------------------")
+    print("FULL DIFFS")
+    print("-------------------------------------------------")
+    for target in sorted(all_targets):
+        print(f"\n\n-------------------------\n{target}")
+        print("    " + full_diffs[target].replace("\n", "\n    "))
+        print()
 
 if __name__ == "__main__":
     main()
