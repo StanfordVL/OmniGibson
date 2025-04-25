2f626770e8f937f19afbdf95a508b7e88664d9b7
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2025-04-25 04:10:09 -0700

    Change channel names in prebake, switch to vray mtl

--- asset_pipeline/b1k_pipeline/max/prebake_textures.py
+++ asset_pipeline/b1k_pipeline/max/prebake_textures.py
@@ -45,13 +45,13 @@ NEW_UV_CHANNEL = 99
 
 # PhysicalMaterial
 CHANNEL_MAPPING = {
-    "VRayRawDiffuseFilterMap": "Base Color Map",  # or VRayDiffuseFilter
-    "VRayNormalsMap": "Bump Map",  # or VRayBumpNormals
-    "VRayMtlReflectGlossinessBake": "Roughness Map",  # iGibson/Omniverse renderer expects we flip the glossiness map
-    "VRayRawReflectionFilterMap": "Reflectivity Map",
-    "VRayRawRefractionFilterMap": "Transparency Map",
-    "VRayMetalnessMap": "Metalness Map",  # requires V-ray 5, update 2.3
-    "VRayMtlReflectIORBake": "IOR Map",
+    "VRayRawDiffuseFilterMap": "Diffuse map",  # or VRayDiffuseFilter
+    "VRayNormalsMap": "Bump map",  # or VRayBumpNormals
+    "VRayMtlReflectGlossinessBake": "Refl. gloss.",  # iGibson/Omniverse renderer expects we flip the glossiness map
+    "VRayRawReflectionFilterMap": "Reflect map",
+    "VRayRawRefractionFilterMap": "Refract map",
+    "VRayMetalnessMap": "Metalness",  # requires V-ray 5, update 2.3
+    "VRayMtlReflectIORBake": "Fresnel IOR",
 }
 
 CHANNEL_DATA_FORMAT_OVERRIDES = {
@@ -296,7 +296,7 @@ class TextureBaker:
             # Only the first channel requires creating the new PhysicalMaterial
             if i == 0:
                 btt.setOutputTo(
-                    obj, "CreateNewMaterial", material=rt.PhysicalMaterial()
+                    obj, "CreateNewMaterial", material=rt.VrayMtl()
                 )
                 # btt.setOutputTo(obj, "CreateNewMaterial", material=rt.PBRMetalRough())
 
@@ -325,6 +325,15 @@ class TextureBaker:
     def postprocess_texture_baking(self, obj, siblings):
         # Set the object to render the baked material
         obj.material.renderMtlIndex = 1
+
+        new_mtl = obj.material.bakedMaterial
+        new_mtl.name = obj.name + "__baked"
+        new_mtl.reflection_lockIOR = False
+        for map_idx in range(rt.getNumSubTexmaps(new_mtl)):
+            channel_name = rt.getSubTexmapSlotName(new_mtl, map_idx + 1)
+            texmap = rt.getSubTexmap(new_mtl, map_idx + 1)
+            if texmap:
+                texmap.name = f"{obj.name}__baked__{channel_name}"
 
         # Update everything that has the same baseobject to use the same material
         for sibling in siblings:
