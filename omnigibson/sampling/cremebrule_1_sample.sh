039d2c76babe2cdb524128eccc5ec3bc1f4fc038
cremebrule <84cremebrule@gmail.com>
2025-05-15 17:32:13 -0700

    update tasks

--- omnigibson/sampling/cremebrule_1_sample.sh
+++ omnigibson/sampling/cremebrule_1_sample.sh
@@ -20,4 +20,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #python ${DIR}/sample_b1k_tasks.py --offline --scene_model house_single_floor --activities canning_food
 #python ${DIR}/sample_b1k_tasks.py --offline --scene_model house_single_floor --activities cook_cabbage
 python ${DIR}/sample_b1k_tasks.py --offline --scene_model house_single_floor --activities picking_up_toys
+python ${DIR}/sample_b1k_tasks.py --offline --scene_model house_single_floor --activities put_together_a_basic_pruning_kit
 
