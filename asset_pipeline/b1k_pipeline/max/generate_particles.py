9a07194a74af80353f7a04ebf1839e186f62b33c
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2025-05-28 14:50:54 -0700

    New particle generation

--- asset_pipeline/b1k_pipeline/max/generate_particles.py
+++ asset_pipeline/b1k_pipeline/max/generate_particles.py
@@ -1,9 +1,11 @@
+import json
 import pymxs
 rt = pymxs.runtime
 
 import csv
 import string
 import random
+# from b1k_pipeline.max.convex_decomposition import generate_convex_decompositions
 
 def get_shape(shape, size):
     if shape == "cube":
@@ -45,7 +47,7 @@ def generate_particle(name, shape, colors, size):
         print("Created", obj.name)
     return objs
         
-def main():
+def substance_generation_round_one():
     with open(r"D:\ig_pipeline\metadata\substance_generation.csv") as f:
         particles = []
         for row in csv.DictReader(f):
@@ -55,6 +57,24 @@ def main():
 
         for i, particle in enumerate(particles):
             particle.position = rt.Point3(i * 100, 0, 0)
+
+def substance_generation_round_two():
+    with open(r"D:/ig_pipeline/metadata/diced_particle_systems_colors.json") as f:
+        data = json.load(f)
+
+    particles = []
+    for particle_name, particle_info in data.items():
+        hex_color = particle_info["color"]
+        rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
+        rgb_color_str = f"{rgb_color[0]},{rgb_color[1]},{rgb_color[2]}"
+        size_cm = particle_info["llm_size_cm"]
+        size_mm = size_cm * 10
+        particle, = generate_particle(particle_name, "cube", rgb_color_str, size_mm)
+        # generate_convex_decompositions(particle, preferred_method="chull")
+        particles.append(particle)
+
+    for i, particle in enumerate(particles):
+        particle.position = rt.Point3(i * 100, 3000, 0)
             
 if __name__ == "__main__":
-    main()
+    substance_generation_round_two()
