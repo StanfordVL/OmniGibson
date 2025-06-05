e456b53e2f986813a89e9f28a141211c3f538ca8
Wensi (Vince) Ai <vinceai@outlook.com>
2025-06-04 17:35:30 -0700

    Update wbvima training

---
+++ omnigibson/learning/scripts/train_policy.sbatch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name="train_xattn"
+#SBATCH --account=viscam
+#SBATCH --partition=viscam
+#SBATCH --exclude=viscam1
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:3090:8
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=12
+#SBATCH --time=1-00:00:00
+#SBATCH --output=outputs/sc/train_xattn_%j.out
+#SBATCH --error=outputs/sc/train_xattn_%j.err
+# notifications for job done & fail
+##SBATCH --mail-type=END,FAIL
+##SBATCH --mail-user=wsai@stanford.edu
+
+# list out some useful information
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NAME="$SLURM_JOB_NAME
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+mkdir -p /vision/u/wsai/OmniGibson/outputs/sc
+source /vision/u/wsai/miniconda3/bin/activate omnigibson
+
+OMNIGIBSON_HEADLESS=1 python omnigibson/learning/train.py policy=wbvima task=picking_up_trash data_dir=/vision/u/wsai/behavior/picking_up_trash/pcd
+
+echo "Job finished."
+exit 0
