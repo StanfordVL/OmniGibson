8f7abaa90788b877a40be806587fb23e28c2b7bb
Cem Gokmen <cgokmen@meta.com>
2025-07-08 15:27:19 -0700

    Fix camera frame convention

--- hdf5_to_colmap.py
+++ hdf5_to_colmap.py
@@ -14,6 +14,8 @@ def hdf5_to_colmap(scene_dir):
   if not os.path.exists(images_dir):
       raise FileNotFoundError(f"Images directory {images_dir} does not exist. Please run the data collection script first.")
 
+  MAX_IMAGES = 10000
+
   # Open the HDF5 file in read mode
   print("Loading HDF5 file...")
   hdf5_file_path = os.path.join(scene_dir, "camera_data.hdf5")
@@ -21,12 +23,12 @@ def hdf5_to_colmap(scene_dir):
       raise FileNotFoundError(f"HDF5 file {hdf5_file_path} does not exist. Please run the data collection script first.")
   with h5py.File(hdf5_file_path, 'r') as f:
       # Load each dataset into memory
-      rgb_data = f['rgb'][:]                 # shape: (TOTAL_IMAGES, HEIGHT, WIDTH, 4), dtype: uint8
-      depth_data = f['depth'][:]             # shape: (TOTAL_IMAGES, HEIGHT, WIDTH), dtype: float32
-      segmentation_data = f['segmentation'][:]  # shape: (TOTAL_IMAGES, HEIGHT, WIDTH), dtype: int32
+      rgb_data = f['rgb'][:MAX_IMAGES]                 # shape: (TOTAL_IMAGES, HEIGHT, WIDTH, 4), dtype: uint8
+      depth_data = f['depth'][:MAX_IMAGES]             # shape: (TOTAL_IMAGES, HEIGHT, WIDTH), dtype: float32
+      segmentation_data = f['segmentation'][:MAX_IMAGES]  # shape: (TOTAL_IMAGES, HEIGHT, WIDTH), dtype: int32
       
-      camera_pose_data = f['camera_pose'][:]       # shape: (TOTAL_IMAGES, 4, 4), dtype: float32
-      camera_intrinsics_data = f['camera_intrinsics'][:]  # shape: (TOTAL_IMAGES, 3, 3), dtype: float32
+      camera_pose_data = f['camera_pose'][:MAX_IMAGES]       # shape: (TOTAL_IMAGES, 4, 4), dtype: float32
+      camera_intrinsics_data = f['camera_intrinsics'][:MAX_IMAGES]  # shape: (TOTAL_IMAGES, 3, 3), dtype: float32
 
       # Load the segmentation labels (stored as a JSON string in the file attributes)
       segmentation_labels = json.loads(f.attrs['segmentation_labels'])
@@ -57,32 +59,34 @@ def hdf5_to_colmap(scene_dir):
     points = points.reshape((rgb.shape[1], rgb.shape[0], 3)).permute(
         1, 0, 2
     )
-    # cam_orn_opengl = ilutils.convert_camera_frame_orientation_convention(cam_orn, "opengl", "world")
-    cam_orn_opengl = cam_orn
-    extrinsics.append(T.pose2mat((cam_pos, cam_orn_opengl)))
+    flip_yz = np.diag([1, -1, -1, 1])  # 4x4 matrix
+    cam_extrinsic_opencv = flip_yz @ cam_pose
+    extrinsics.append(cam_extrinsic_opencv)
     colors = rgb[:, :, :3]
     points_rgb.append(colors)
     points_3d.append(points.numpy())
     image_paths.append(image_files_by_idx[i])
 
+  print("Stacking points and colors...")
   points_3d = np.stack(points_3d, axis=0)
   points_rgb = np.stack(points_rgb, axis=0)
   points_xyf = create_pixel_coordinate_grid(rgb_data.shape[0], rgb_data.shape[1], rgb_data.shape[2])
   extrinsics = np.stack(extrinsics, axis=0)
 
+  print("Reducing point cloud size...")
   # First find all the finite points in the point cloud
   finite_mask = np.isfinite(points_3d).all(axis=-1)
   points_3d = points_3d[finite_mask]
   points_rgb = points_rgb[finite_mask]
   points_xyf = points_xyf[finite_mask]
 
-  # Limit the number of points to 100,000
-  indices = np.random.choice(points_3d.shape[0], 100000, replace=False)
+  # Limit the number of points to 1,000,000
+  indices = np.random.choice(points_3d.shape[0], 1000000, replace=False)
   points_3d = points_3d[indices]
   points_rgb = points_rgb[indices]
   points_xyf = points_xyf[indices]
 
-  assert points_3d.shape == points_rgb.shape == points_xyf.shape == (100000, 3), \
+  assert points_3d.shape == points_rgb.shape == points_xyf.shape == (1000000, 3), \
       "Mismatch in number of points, colors, and pixel coordinates."
 
   print("Creating COLMAP reconstruction...")
@@ -98,17 +102,6 @@ def hdf5_to_colmap(scene_dir):
       shared_camera=False,
       camera_type="PINHOLE",
   )
-
-  # reconstruction_resolution = vggt_fixed_resolution
-
-  # reconstruction = rename_colmap_recons_and_rescale_camera(
-  #     reconstruction,
-  #     base_image_path_list,
-  #     original_coords.cpu().numpy(),
-  #     img_size=reconstruction_resolution,
-  #     shift_point2d_to_original_res=True,
-  #     shared_camera=shared_camera,
-  # )
 
   print(f"Saving reconstruction to {scene_dir}/sparse")
   sparse_reconstruction_dir = os.path.join(scene_dir, "sparse")
