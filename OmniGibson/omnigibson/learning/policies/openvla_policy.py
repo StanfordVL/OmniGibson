a866751ba72a4c5bd1845c08fb697a4c0c437350
Evans Han <21998509+evansh666@users.noreply.github.com>
2025-07-12 14:04:25 -0700

    Update OpenVLA policy

--- OmniGibson/omnigibson/learning/policies/openvla_policy.py
+++ OmniGibson/omnigibson/learning/policies/openvla_policy.py
@@ -1,36 +1,38 @@
 import torch
-import tensorflow as tf
 from typing import Union, Tuple
 import copy
 import numpy as np
+import requests
+import json_numpy
 from collections import deque
 from PIL import Image
 from omnigibson.learning.policies.policy_base import BasePolicy
+from omnigibson.learning.utils.eval_utils import PROPRIOCEPTION_INDICES, ACTION_QPOS_INDICES
+json_numpy.patch()
 
 RESIZE_SIZE = 224
+ACTION_DIM = 23
+LEFT_GRIPPER_IDX = ACTION_QPOS_INDICES["R1Pro"]["left_gripper"]
+RIGHT_GRIPPER_IDX = ACTION_QPOS_INDICES["R1Pro"]["right_gripper"]
 
 def generate_prop_state(proprio_data):
-    base_qpos = proprio_data[:,246:249] # 3
-    trunk_qpos = proprio_data[:,238:242] # 4
-    arm_left_qpos = proprio_data[:,158:165] #  7
-    arm_right_qpos = proprio_data[:,198:205] #  7
-    left_gripper_width = proprio_data[:,194:196].sum(axis=-1)[:,None] # 1
-    right_gripper_width = proprio_data[:,234:236].sum(axis=-1)[:,None] # 1
+    return np.concatenate([
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["base_qpos"]], # 3
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["trunk_qpos"]], # 4
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["arm_left_qpos"]], #  7
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["arm_right_qpos"]], #  7
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["gripper_left_qpos"]].sum(axis=-1)[:,None], # 1
+        proprio_data[:,PROPRIOCEPTION_INDICES["R1Pro"]["gripper_right_qpos"]].sum(axis=-1)[:,None] # 1
+    ], axis=-1) # (:,23)
     
-    prop_state = np.concatenate((base_qpos, trunk_qpos, arm_left_qpos, arm_right_qpos, left_gripper_width, right_gripper_width), axis=-1) # 23
-    return prop_state
-
-def resize_image_for_openvla(img: np.ndarray, resize_size: Union[int, Tuple[int, int]]) -> np.ndarray:
+def resize_image_for_openvla(img: np.ndarray, size: Union[int, Tuple[int, int]]) -> np.ndarray:
     """
     Resize an image to match openvla-oft policy's expected input size.
     """
-    assert isinstance(resize_size, int) or isinstance(resize_size, tuple)
-    if isinstance(resize_size, int):
-        resize_size = (resize_size, resize_size)
+    if isinstance(size, int):
+        size = (size, size)
+    return np.array(Image.fromarray(img).resize(size, resample=Image.LANCZOS))
 
-    return np.array(
-        Image.fromarray(img).resize(resize_size, resample=Image.LANCZOS)
-    )
 
 class OpenVLA(BasePolicy):
     """
@@ -47,15 +49,14 @@ class OpenVLA(BasePolicy):
         **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
-        self._resize_obs = resize_image_for_openvla
-        
         # server endpoint for action generation from OpenVLA server
         self.policy_endpoint = f"http://{host}:{port}/act"
+        self._resize_obs = resize_image_for_openvla
 
         self.text_prompt = text_prompt
         self.control_mode = control_mode
         self.action_queue = deque([],maxlen=10)
-        self.last_action = np.zeros((10, 21), dtype=np.float64)
+        self.last_action = np.zeros((10, ACTION_DIM), dtype=np.float64)
 
         self.replan_interval = 10             # K: replan every 10 steps
         self.max_len = 50                     # how long the policy sequences are
@@ -64,10 +65,10 @@ class OpenVLA(BasePolicy):
 
     def reset(self) -> None:
         self.action_queue = deque([],maxlen=10)
-        self.last_action = np.zeros((10, 23), dtype=np.float64)
+        self.last_action = np.zeros((10, ACTION_DIM), dtype=np.float64)
         self.step_counter = 0
 
-    def process_obs(self, obs: dict) -> dict:
+    def process_obs(self, obs: dict, control_mode: str) -> dict:
         """
         Prepare observation for policy input.
         """
@@ -76,26 +77,47 @@ class OpenVLA(BasePolicy):
             obs["robot_r1::robot_r1:zed_link:Camera:0::rgb"][None,...,:3],
             obs["robot_r1::robot_r1:left_realsense_link:Camera:0::rgb"][None,...,:3],
             obs["robot_r1::robot_r1:right_realsense_link:Camera:0::rgb"][None,...,:3],
-        ], axis=1)
+        ], axis=1) # (1, 3, H, W, C)
+        
+        # Only use last observation and proprio if not receeding_temporal
+        if control_mode != "receeding_temporal":
+            img_obs = img_obs[:, -1] #(B, T, num_cameras, H, W, C) -> (B, num_cameras, H, W, C)
+            prop_state = prop_state[:, -1] # (B, T, 23) -> (B, 23)
+
+        if img_obs.shape[-1] != 3: # Channel last
+            img_obs = np.transpose(img_obs, (0, 1, 3, 4, 2))
 
         processed_obs = {
-            "observation": img_obs,  # Shape: (1, 3, H, W, C)
-            "proprio": prop_state,
-            "prompt": self.text_prompt,
+            "full_image": self._resize_obs(img_obs[0, 0].numpy(), RESIZE_SIZE),
+            "left_wrist_image": self._resize_obs(img_obs[0, 1].numpy(), RESIZE_SIZE),
+            "right_wrist_image": self._resize_obs(img_obs[0, 2].numpy(), RESIZE_SIZE),
+            "state": prop_state[0],
+            "instruction": self.text_prompt,
         }
+
         return processed_obs
 
     def get_action_from_server(self, obs: dict) -> torch.Tensor:
-        import requests
-        response = requests.post(
-            self.policy_endpoint,
-            json=obs,
-        )
-        return response.json()
-
+        """
+        Get action from OpenVLA server, update self.last_action if successful
+        """
+        try:
+            response = requests.post(
+                self.policy_endpoint,
+                json=obs,
+            )
+            action = response.json() # list of np.ndarray
+            if action != "error":
+                self.last_action = action
+            else:
+                print(f"Error in action prediction, using last action")
+        except:
+            print(f"Error in action prediction, using last action")
+        return self.last_action
+    
     def forward(self, obs: dict, *args, **kwargs) -> torch.Tensor:
         """
-        Model input expected: 
+        Model input expected: (finetuned on R1Pro)
             📌 Key: full_image
             Type: ndarray
             Dtype: uint8
@@ -126,7 +148,7 @@ class OpenVLA(BasePolicy):
             Dtype: float64
             Shape: (10, 23)
         """
-        input_obs = self.process_obs(obs)
+        input_obs = self.process_obs(obs, self.control_mode)
 
         if self.control_mode == 'receeding_temporal':
             return self._act_receeding_temporal(input_obs)
@@ -135,32 +157,11 @@ class OpenVLA(BasePolicy):
             if len(self.action_queue) > 0:
                 # pop the first action in the queue
                 final_action = self.action_queue.popleft()[None]
-                return final_action[...,:23]
+                return final_action[...,:ACTION_DIM]
 
-        nbatch = copy.deepcopy(input_obs)
-
-        # (B, T, num_cameras, H, W, C) -> (B, num_cameras, H, W, C), only use last observation 
-        nbatch["observation"] = nbatch["observation"][:, -1] 
-        # (B, NUM_CAM, H, W, C) --> (B, NUM_CAM, C, H, W), permute if pytorch
-        if nbatch["observation"].shape[-1] != 3: 
-            nbatch["observation"] = np.transpose(nbatch["observation"], (0, 1, 3, 4, 2))
-
-        # nbatch["proprio"], (B, T, 16), where B=1
-        joint_positions = nbatch["proprio"][0, -1]
-        batch = {
-                "full_image": self._resize_obs(nbatch["observation"][0, 0].numpy(), RESIZE_SIZE),
-                "left_wrist_image": self._resize_obs(nbatch["observation"][0, 1].numpy(), RESIZE_SIZE),
-                "right_wrist_image": self._resize_obs(nbatch["observation"][0, 2].numpy(), RESIZE_SIZE),
-                "state": joint_positions,
-                "instruction": self.text_prompt,
-        }
-
-        try:
-            action = self.get_action_from_server(batch)
-            self.last_action = action
-        except:
-            action = self.last_action
-            print("Error in action prediction, using last action")
+        # Get action from OpenVLA server
+        batch = copy.deepcopy(input_obs)
+        action = self.get_action_from_server(batch)
         
         # action shape: (10, 23), joint_positions shape: (23,)
         target_joint_positions = action.copy()
@@ -184,39 +185,22 @@ class OpenVLA(BasePolicy):
             exp_weights = exp_weights / exp_weights.sum()
 
             final_action = (actions_current_timestep * exp_weights[:, None]).sum(axis=0)
-            final_action[-8] = target_joint_positions[0,-8]
-            final_action[-1] = target_joint_positions[0,-1]
+            final_action[LEFT_GRIPPER_IDX] = target_joint_positions[0,LEFT_GRIPPER_IDX]
+            final_action[RIGHT_GRIPPER_IDX] = target_joint_positions[0,RIGHT_GRIPPER_IDX]
             final_action = final_action[None]
         else:
             final_action = target_joint_positions
 
-        return final_action[...,:23]  # return only the first 23 joints, which are the robot's joints (base, trunk, arms, grippers)
+        return final_action[...,:ACTION_DIM]  # return only the first 23 joints, which are the robot's joints (base, trunk, arms, grippers)
 
     def _act_receeding_temporal(self, input_obs):
         # Step 1: check if we should re-run policy
         if self.step_counter % self.replan_interval == 0:
             # Run policy every K steps
-            nbatch = copy.deepcopy(input_obs)
-            if nbatch["observation"].shape[-1] != 3:
-                nbatch["observation"] = np.transpose(nbatch["observation"], (0, 1, 3, 4, 2))
+            batch = copy.deepcopy(input_obs)
 
-            joint_positions = nbatch["proprio"][0]
-            batch = {
-                "full_image": self._resize_obs(nbatch["observation"][0, 0].numpy(), RESIZE_SIZE),
-                "left_wrist_image": self._resize_obs(nbatch["observation"][0, 1].numpy(), RESIZE_SIZE),
-                "right_wrist_image": self._resize_obs(nbatch["observation"][0, 2].numpy(), RESIZE_SIZE),
-                "state": joint_positions,
-                "instruction": self.text_prompt,
-            }
-
-            try:
-                action = self.get_action_from_server(batch)
-                print(f"action: {action.shape}")
-                self.last_action = action
-            except:
-                action = self.last_action
-                print("Error in action prediction, using last action")
-
+            # Get action from OpenVLA server
+            action = self.get_action_from_server(batch)
             target_joint_positions = action.copy()
 
             # Add this sequence to action queue
@@ -247,11 +231,11 @@ class OpenVLA(BasePolicy):
         final_action = (actions_current_timestep * exp_weights[:, None]).sum(axis=0)
 
         # Preserve grippers from most recent rollout
-        final_action[-8] = actions_current_timestep[0, -8]
-        final_action[-1] = actions_current_timestep[0, -1]
+        final_action[LEFT_GRIPPER_IDX] = actions_current_timestep[0, LEFT_GRIPPER_IDX]
+        final_action[RIGHT_GRIPPER_IDX] = actions_current_timestep[0, RIGHT_GRIPPER_IDX]
         final_action = final_action[None]
 
         self.step_counter += 1
 
-        return final_action[...,:23]
+        return final_action[...,:ACTION_DIM]
 
