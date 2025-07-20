Implement normalization & fix torch bug for eval

OmniGibson/omnigibson/learning/policies/va_policy.py
@@ -1,11 +1,13 @@
+import cv2
 import logging
 import torch as th
+import torch.nn.functional as F
 from collections import deque
 from omnigibson.learning.policies.policy_base import BasePolicy
 from omnigibson.learning.utils.array_tensor_utils import any_concat, any_slice
 from omnigibson.learning.utils.eval_utils import PROPRIOCEPTION_INDICES, PROPRIO_QPOS_INDICES, JOINT_RANGE
 from omnigibson.learning.utils.network_utils import WebsocketClientPolicy
-from typing import Optional
+from typing import Optional, Tuple
 
 
 class VisionActionILPolicy(BasePolicy):
@@ -22,6 +24,7 @@ class VisionActionILPolicy(BasePolicy):
         port: Optional[int] = None,
         action_prediction_horizon: int,
         obs_window_size: int = 1,
+        obs_output_size: Tuple[int, int] = (128, 128),
         # ====== other args for base class ======
         **kwargs,
     ) -> None:
@@ -34,10 +37,12 @@ class VisionActionILPolicy(BasePolicy):
             logging.info(f"Skipped creating websocket client policy")
         self.action_prediction_horizon = action_prediction_horizon
         self.obs_window_size = obs_window_size
+        self.obs_output_size = obs_output_size
         self._obs_history = deque(maxlen=obs_window_size)
         self._action_traj_pred = None
         self._action_idx = 0
         self.robot_type = "R1Pro"
+        self.joint_range = JOINT_RANGE[self.robot_type]
         self.device = th.device("cuda" if th.cuda.is_available() else "cpu")
 
     def forward(self, obs: dict, *args, **kwargs) -> th.Tensor:
@@ -65,19 +70,21 @@ class VisionActionILPolicy(BasePolicy):
 
     def process_obs(self, obs: dict) -> dict:
         # Expand twice to get B and T_A dimensions
-        proprio = obs["robot_r1::proprio"].unsqueeze(0).unsqueeze(0).to(self.device)
+        proprio = obs["robot_r1::proprio"].unsqueeze(0).unsqueeze(0)
         processed_obs = {
             "qpos": {
-                key: (proprio[..., PROPRIO_QPOS_INDICES[self.robot_type][key]] - JOINT_RANGE[self.robot_type][key][0]) / 
-                (JOINT_RANGE[self.robot_type][key][1] - JOINT_RANGE[self.robot_type][key][0])
+                key: ((proprio[..., PROPRIO_QPOS_INDICES[self.robot_type][key]] - self.joint_range[key][0]) / 
+                (self.joint_range[key][1] - self.joint_range[key][0])).to(self.device)
                 for key in PROPRIO_QPOS_INDICES[self.robot_type]
             },
             "odom": {
-                "base_velocity": (proprio[..., PROPRIOCEPTION_INDICES[self.robot_type]["base_qvel"]] - JOINT_RANGE[self.robot_type]["base"][0]) / 
-                (JOINT_RANGE[self.robot_type]["base"][1] - JOINT_RANGE[self.robot_type]["base"][0])
+                "base_velocity": ((proprio[..., PROPRIOCEPTION_INDICES[self.robot_type]["base_qvel"]] - self.joint_range["base"][0]) / 
+                (self.joint_range["base"][1] - self.joint_range["base"][0])).to(self.device)
             },
         }
         processed_obs.update({
-            k: v[..., :3].unsqueeze(0).unsqueeze(0).to(self.device) for k, v in obs.items() if "rgb" in k
+            k: F.interpolate(
+                v[..., :3].unsqueeze(0).movedim(-1, -3).to(th.float32) / 255.0, self.obs_output_size, mode="area"
+            ).unsqueeze(0).to(self.device) for k, v in obs.items() if "rgb" in k
         })
         return processed_obs

OmniGibson/omnigibson/learning/utils/eval_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch as th
 from collections import OrderedDict
 
 
@@ -96,38 +97,38 @@ PROPRIO_QPOS_INDICES = {
 JOINT_RANGE = {
     "R1Pro": {
         "base": (
-            np.array([-0.75, -0.75, -1.0], dtype=np.float32),
-            np.array([0.75, 0.75, 1.0], dtype=np.float32)
+            th.tensor([-0.75, -0.75, -1.0], dtype=th.float32),
+            th.tensor([0.75, 0.75, 1.0], dtype=th.float32)
         ),
         "torso": (
-            np.array([-1.1345, -2.7925, -1.8326, -3.0543], dtype=np.float32),
-            np.array([1.8326, 2.5307, 1.5708, 3.0543], dtype=np.float32)
+            th.tensor([-1.1345, -2.7925, -1.8326, -3.0543], dtype=th.float32),
+            th.tensor([1.8326, 2.5307, 1.5708, 3.0543], dtype=th.float32)
         ),
         "left_arm": (
-            np.array([-4.4506, -0.1745, -2.3562, -2.0944, -2.3562, -1.0472, -1.5708], dtype=np.float32),
-            np.array([1.3090, 3.1416, 2.3562, 0.3491, 2.3562, 1.0472, 1.5708], dtype=np.float32)
+            th.tensor([-4.4506, -0.1745, -2.3562, -2.0944, -2.3562, -1.0472, -1.5708], dtype=th.float32),
+            th.tensor([1.3090, 3.1416, 2.3562, 0.3491, 2.3562, 1.0472, 1.5708], dtype=th.float32)
         ),
         "left_gripper": (
-            np.array([-1], dtype=np.float32),
-            np.array([1], dtype=np.float32)
+            th.tensor([-1], dtype=th.float32),
+            th.tensor([1], dtype=th.float32)
         ),
         "right_arm": (
-            np.array([-4.4506, -3.1416, -2.3562, -2.0944, -2.3562, -1.0472, -1.5708], dtype=np.float32),
-            np.array([1.3090, 0.1745, 2.3562, 0.3491, 2.3562, 1.0472, 1.5708], dtype=np.float32)
+            th.tensor([-4.4506, -3.1416, -2.3562, -2.0944, -2.3562, -1.0472, -1.5708], dtype=th.float32),
+            th.tensor([1.3090, 0.1745, 2.3562, 0.3491, 2.3562, 1.0472, 1.5708], dtype=th.float32)
         ),
         "right_gripper": (
-            np.array([-1], dtype=np.float32),
-            np.array([1], dtype=np.float32)
+            th.tensor([-1], dtype=th.float32),
+            th.tensor([1], dtype=th.float32)
         ),
     }
 }
 JOINT_RANGE_ARRAY = {
     robot_name: (
-        np.concatenate([
+        th.cat([
             JOINT_RANGE[robot_name][part][0]
             for part in ACTION_QPOS_INDICES[robot_name]
         ]), 
-        np.concatenate([
+        th.cat([
             JOINT_RANGE[robot_name][part][1]
             for part in ACTION_QPOS_INDICES[robot_name]
         ])

OmniGibson/omnigibson/learning/utils/obs_utils.py
@@ -50,29 +50,29 @@ def quantize_depth(
 
 
 def dequantize_depth(
-    quantized_depth: th.Tensor, 
+    quantized_depth: np.ndarray, 
     min_depth: float = MIN_DEPTH, 
     max_depth: float = MAX_DEPTH, 
     shift: float=DEPTH_SHIFT
-) -> th.Tensor:
+) -> np.ndarray:
     """
     Dequantizes a 14-bit depth tensor back to the original depth values.
     
     Args:
-        quantized_depth (th.Tensor): Quantized depth tensor.
+        quantized_depth (np.ndarray): Quantized depth tensor.
         min_depth (float): Minimum depth value.
         max_depth (float): Maximum depth value.
         shift (float): Small value to shift depth to avoid log(0).
     Returns:
-        th.Tensor: Dequantized depth tensor.
+        np.ndarray: Dequantized depth tensor.
     """
     qmax = (1 << 14) - 1
-    log_min = th.log(th.tensor(min_depth + shift, dtype=th.float32, device=quantized_depth.device))
-    log_max = th.log(th.tensor(max_depth + shift, dtype=th.float32, device=quantized_depth.device))
+    log_min = np.log(min_depth + shift)
+    log_max = np.log(max_depth + shift)
 
-    log_norm = quantized_depth.to(th.float32) / qmax
+    log_norm = quantized_depth / qmax
     log_depth = log_norm * (log_max - log_min) + log_min
-    depth = th.clamp(th.exp(log_depth) - shift, min=min_depth, max=max_depth)
+    depth = np.clip(np.exp(log_depth) - shift, min_depth, max_depth)
 
     return depth
 
@@ -180,6 +180,7 @@ class VideoLoader:
         path: str,
         batch_size: Optional[int]=None, 
         stride: int=1, 
+        output_size: Tuple[int, int]=(128, 128),
         *args, 
         **kwargs
     ):
@@ -201,7 +202,8 @@ class VideoLoader:
         self.stride = stride
         self._frame_iter = None
         self._done = False
-
+        self.output_size = output_size
+        
     def __iter__(self) -> Generator[th.Tensor, None, None]:
         self.container.seek(0)
         self._frame_iter = self.container.decode(self.stream)
@@ -268,8 +270,9 @@ class RGBVideoLoader(VideoLoader):
         )
 
     def _process_single_frame(self, frame: av.VideoFrame) -> th.Tensor:
-        rgb = frame.to_ndarray(format="rgb24")  # (H, W, 3), dtype=uint8
-        return th.from_numpy(rgb).unsqueeze(0)  # (1, H, W, 3)
+        rgb = frame.to_ndarray(format="rgb24").astype(np.float32) / 255.0  # (H, W, 3), dtype=float32
+        rgb = cv2.resize(rgb, self.output_size, interpolation=cv2.INTER_AREA)
+        return th.from_numpy(rgb).unsqueeze(0).to(th.float32)  # (1, H, W, 3)
 
 
 class DepthVideoLoader(VideoLoader):
@@ -293,15 +296,17 @@ class DepthVideoLoader(VideoLoader):
 
     def _process_single_frame(self, frame: av.VideoFrame) -> th.Tensor:
         # Decode Y (luma) channel only; YUV420 → grayscale image
-        frame_gray16 = frame.reformat(format='gray16le').to_ndarray()
-        frame_gray16 = th.from_numpy(frame_gray16).unsqueeze(0)  # (1, H, W)
+        frame_gray16 = frame.reformat(format='gray16le').to_ndarray()  # (H, W)
         depth = dequantize_depth(
             frame_gray16,
             min_depth=self.min_depth,
             max_depth=self.max_depth,
             shift=self.shift
         )
-        return depth
+        depth = cv2.resize(depth, self.output_size, interpolation=cv2.INTER_AREA).astype(np.float32)
+        # normalize depth to [0, 1]
+        depth = (depth - self.min_depth) / (self.max_depth - self.min_depth)
+        return th.from_numpy(depth).unsqueeze(0).to(th.float32)  # (1, H, W)
         
 
 class SegVideoLoader(VideoLoader):
@@ -331,7 +336,8 @@ class SegVideoLoader(VideoLoader):
         distances = th.cdist(rgb_flat[None, :, :], self.palette[None, :, :], p=2)[0]  # (H*W, N_ids)
         ids = th.argmin(distances, dim=-1)  # (H*W,)
         ids = self.id_list[ids].reshape((rgb.shape[0], rgb.shape[1]))  # (H, W)
-        return ids.unsqueeze(0).cpu()  # (1, H, W)
+        ids = cv2.resize(ids.numpy(), self.output_size, interpolation=cv2.INTER_NEAREST)
+        return th.from_numpy(ids).unsqueeze(0).cpu().to(th.long)  # (1, H, W)
 
 
 OBS_LOADER_MAP = {