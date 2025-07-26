283e41b5f7120b9114d0bbfb3b87f89ea780a63f
Wensi Ai <wsai@stanford.edu>
2025-07-25 19:44:17 -0700

    Update datasets

--- OmniGibson/omnigibson/learning/datas/lerobot_dataset.py
+++ OmniGibson/omnigibson/learning/datas/lerobot_dataset.py
@@ -1,10 +1,60 @@
 import argparse
 import json
 import os
-from typing import Tuple
+import packaging.version
 import numpy as np
-from lerobot.datasets.lerobot_dataset import LeRobotDataset
-from omnigibson.learning.utils.eval_utils import TASK_INDICES_TO_NAMES
+import torch as th
+from collections.abc import Callable
+from omnigibson.learning.utils.eval_utils import TASK_NAMES_TO_INDICES, ROBOT_CAMERA_NAMES
+from pathlib import Path
+from torch.utils.data import Dataset
+from typing import Iterable, Tuple
+
+from lerobot.constants import HF_LEROBOT_HOME
+from lerobot.datasets.compute_stats import aggregate_stats
+from lerobot.datasets.lerobot_dataset import LeRobotDataset, LeRobotDatasetMetadata, CODEBASE_VERSION
+from lerobot.datasets.utils import (
+    EPISODES_PATH,
+    check_delta_timestamps,
+    check_timestamps_sync,
+    get_delta_indices,
+    get_episode_data_index,
+    get_safe_version,
+    load_jsonlines,
+)
+from lerobot.datasets.video_utils import get_safe_default_codec
+
+
+class BehaviorLerobotDatasetMetadata(LeRobotDatasetMetadata):
+    """
+    LerobotDatasetMetadata with the following customizations:
+        1. Custom task names mapping to indices.
+    """
+    def __init__(
+        self,
+        *args,
+        modalities: Iterable[str] = None,
+        cameras: Iterable[str] = None,
+        **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.modalities = set(modalities)
+        self.camera_names = set(cameras)
+        assert self.modalities.issubset({"rgb", "depth", "seg_instance_id"}), \
+            f"Modalities must be a subset of ['rgb', 'depth', 'seg_instance_id'], but got {self.modalities}"
+        assert self.camera_names.issubset(ROBOT_CAMERA_NAMES), \
+            f"Camera names must be a subset of {ROBOT_CAMERA_NAMES}, but got {self.camera_names}"
+
+    @property
+    def features(self) -> dict[str, dict]:
+        """All features contained in the dataset."""
+        features = dict()
+        # pop not required features
+        for name in self.info["features"].keys():
+            if name.startswith("observation.images.") \
+                and name.split(".")[-1] in self.camera_names and name.split(".")[-2] in self.modalities:
+                features[name] = self.info["features"][name]
+        return features
 
 
 class BehaviorLeRobotDataset(LeRobotDataset):
@@ -14,21 +64,87 @@ class BehaviorLeRobotDataset(LeRobotDataset):
     """
     def __init__(
         self,
-        *args,
-        tasks: list[str] = None,
-        video_backend: str = "pyav",
-        **kwargs,
+        repo_id: str,
+        root: str | Path | None = None,
+        episodes: list[int] | None = None,
+        image_transforms: Callable | None = None,
+        delta_timestamps: dict[list[float]] | None = None,
+        tolerance_s: float = 1e-4,
+        revision: str | None = None,
+        force_cache_sync: bool = False,
+        download_videos: bool = True,
+        video_backend: str | None = "pyav",
+
+        tasks: Iterable[str] = None,
+        modalities: Iterable[str] = None,
+        cameras: Iterable[str] = None,
     ):
         """
-        Args:
-            tasks: list of tasks to load. If None, all tasks will be loaded.
+        Custom args:
+            tasks (List[str]): list of task names to load. If None, all tasks will be loaded.
+            modalities (List[str]): list of modality names to load. If None, all modalities will be loaded.
+                must be a subset of ["rgb", "depth", "seg_instance_id"]
+            cameras (List[str]): list of camera names to load. If None, all cameras will be loaded.
+                must be a subset of ["left_wrist", "right_wrist", "head"]
             video_backend: video backend to use for decoding videos.
         """
-        super().__init__(
-            *args,
-            video_backend=video_backend,
-            **kwargs,
+        Dataset.__init__(self)
+        self.repo_id = repo_id
+        self.root = Path(root) if root else HF_LEROBOT_HOME / repo_id
+        self.image_transforms = image_transforms
+        self.delta_timestamps = delta_timestamps
+        self.episodes = episodes
+        self.tolerance_s = tolerance_s
+        self.revision = revision if revision else CODEBASE_VERSION
+        self.video_backend = video_backend if video_backend else get_safe_default_codec()
+        self.delta_indices = None
+
+        # Unused attributes
+        self.image_writer = None
+        self.episode_buffer = None
+
+        self.root.mkdir(exist_ok=True, parents=True)
+
+        # ========== Customizations ==========
+        # Load metadata
+        self.meta = BehaviorLerobotDatasetMetadata(
+            repo_id=self.repo_id, root=self.root, revision=self.revision, force_cache_sync=force_cache_sync,
+            modalities=modalities, cameras=cameras
         )
+        self.tasks_names = set(tasks) if tasks is not None else set(TASK_NAMES_TO_INDICES.keys())
+        # overwrite episode based on task
+        if episodes is None:
+            episodes = load_jsonlines(self.root / EPISODES_PATH)
+            self.episodes = sorted([item["episode_index"] for item in episodes if item["tasks"][0] in self.tasks_names])
+        # ====================================
+
+        if self.episodes is not None and self.meta._version >= packaging.version.parse("v2.1"):
+            episodes_stats = [self.meta.episodes_stats[ep_idx] for ep_idx in self.episodes]
+            self.stats = aggregate_stats(episodes_stats)
+
+        # Load actual data
+        try:
+            if force_cache_sync:
+                raise FileNotFoundError
+            assert all((self.root / fpath).is_file() for fpath in self.get_episodes_file_paths())
+            self.hf_dataset = self.load_hf_dataset()
+        except (AssertionError, FileNotFoundError, NotADirectoryError):
+            self.revision = get_safe_version(self.repo_id, self.revision)
+            self.download_episodes(download_videos)
+            self.hf_dataset = self.load_hf_dataset()
+
+        self.episode_data_index = get_episode_data_index(self.meta.episodes, self.episodes)
+
+        # Check timestamps
+        timestamps = th.stack(self.hf_dataset["timestamp"]).numpy()
+        episode_indices = th.stack(self.hf_dataset["episode_index"]).numpy()
+        ep_data_index_np = {k: t.numpy() for k, t in self.episode_data_index.items()}
+        check_timestamps_sync(timestamps, episode_indices, ep_data_index_np, self.fps, self.tolerance_s)
+
+        # Setup delta_indices
+        if self.delta_timestamps is not None:
+            check_delta_timestamps(self.delta_timestamps, self.fps, self.tolerance_s)
+            self.delta_indices = get_delta_indices(self.delta_timestamps, self.fps)
 
     def get_episodes_file_paths(self) -> list[str]:
         """
@@ -48,10 +164,10 @@ class BehaviorLeRobotDataset(LeRobotDataset):
 
 
 def generate_task_json(data_dir: str) -> int:
-    num_tasks = len(TASK_INDICES_TO_NAMES)
+    num_tasks = len(TASK_NAMES_TO_INDICES)
     
     with open(f"{data_dir}/meta/tasks.jsonl", "w") as f:
-        for task_index, task_name in TASK_INDICES_TO_NAMES.items():
+        for task_name, task_index in TASK_NAMES_TO_INDICES.items():
             json.dump({"task_index": task_index, "task": task_name}, f)
             f.write("\n")
     return num_tasks
@@ -351,6 +467,13 @@ def generate_info_json(
                 "dtype": "int64",
                 "shape": [
                     1
+                ],
+                "names": None
+            },
+            "observation.cam_rel_poses": {
+                "dtype": "float32",
+                "shape": [
+                    21
                 ],
                 "names": None
             },
