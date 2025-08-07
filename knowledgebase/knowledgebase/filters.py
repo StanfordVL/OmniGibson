19c3cd3b3619391a85f2d7c911f0b9be3eb0f31a
Cem Gökmen <cgokmen@stanford.edu>
2025-08-07 00:19:51 -0700

    Remove flask version

--- knowledgebase/knowledgebase/filters.py
+++ knowledgebase/knowledgebase/filters.py
@@ -1,9 +1,5 @@
-from flask import Blueprint
 from bddl.knowledge_base import SynsetState
 
-bp = Blueprint('filters', __name__)
-
-@bp.app_template_filter('tocolor')
 def tocolor_filter(state):
     """Convert a SynsetState to a Bootstrap color class."""
     color_map = {
