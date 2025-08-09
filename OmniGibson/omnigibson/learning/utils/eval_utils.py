7de9d16fef87734330806e1f71db00c914212c9f
Wensi Ai <wsai@stanford.edu>
2025-08-09 14:23:08 -0700

    Update task list

--- OmniGibson/omnigibson/learning/utils/eval_utils.py
+++ OmniGibson/omnigibson/learning/utils/eval_utils.py
@@ -179,9 +179,39 @@ EEF_POSITION_RANGE = {
 
 
 TASK_NAMES_TO_INDICES = {
+    # B10
     "turning_on_radio": 0,
     "picking_up_trash": 1,
-    "can_meat": 2,
+    "putting_away_halloween_decorations": 2,
+    "cleaning_up_plates_and_food": 3,
+    "can_meat": 4,
+    "setting_mousetraps": 5,
+    "hiding_easter_eggs": 6,
+    "picking_up_toys": 7,
+    "rearranging_kitchen_furniture": 8,
+    "putting_up_christmas_decorations": 9,
+    # B20
+    "set_up_a_coffee_station_in_your_kitchen": 10,
+    "putting_dishes_away_after_cleaning": 11,
+    "preparing_lunch_box": 12,
+    "loading_the_car": 13,
+    "carrying_in_groceries": 14,
+    "bringing_in_wood": 15,
+    "moving_boxes_to_storage": 16,
+    "bringing_water": 17,
+    "tidying_bedroom": 18,
+    "outfit_a_basic_toolbox": 19,
+    # B30
+    "sorting_vegetables": 20,
+    "collecting_childrens_toys": 21,
+    "putting_shoes_on_rack": 22,
+    "boxing_books_up_for_storage": 23,
+    "storing_food": 24,
+    "clearing_food_from_table_into_fridge": 25,
+    "assembling_gift_baskets": 26,
+    "sorting_household_items": 27,
+    "getting_organized_for_work": 28,
+    "clean_up_your_desk": 29
 }
 TASK_INDICES_TO_NAMES = {v: k for k, v in TASK_NAMES_TO_INDICES.items()}
 
