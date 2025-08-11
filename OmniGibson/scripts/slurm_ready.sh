ee9eb120af61755523b5f58553995a4c00b9c84c
Wensi Ai <wsai@stanford.edu>
2025-08-11 15:31:26 -0700

    Fix slurm ready script bug

--- OmniGibson/scripts/slurm_ready.sh
+++ OmniGibson/scripts/slurm_ready.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # create texture cache on scr-ssd
-mkdir -p /scr-ssd/$(whoami)/texturecache
-rm -rf ~/.cache/ov/texturecache
-ln -s /scr-ssd/$(whoami)/texturecache ~/.cache/ov/texturecache
+target="/scr-ssd/$(whoami)/texturecache"
+mkdir -p "$target"
+if [ "$(readlink -f ~/.cache/ov/texturecache 2>/dev/null)" != "$(realpath "$target")" ]; then
+    ln -sfn "$target" ~/.cache/ov/texturecache
+fi
